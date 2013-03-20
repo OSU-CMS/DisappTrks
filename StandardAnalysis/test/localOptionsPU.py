cdf9068d6b58fa7f9ca1a899a5953f04ab93f5f8
wulsin <>
2013-03-20 15:21:42 +0000

    options for PU

---
+++ StandardAnalysis/test/localOptionsPU.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+config_file = "pu_cfg.py"
+
+#intLumi = 30000.
+
+datasets = [
+###Samples commented out here have already been done####
+
+#    'AMSB_mGrav50K_0p5ns_Reco',
+#    'AMSB_mGrav50K_1ns_Reco',
+#    'AMSB_mGrav50K_5ns_Reco',
+#    'DYJetsToLL_Reco',
+#    'WJetsToLNu_Reco',
+#    'QCD_Reco',
+#    'QCD_0to5_Reco',
+#    'QCD_5to15_Reco',
+#    'QCD_30to50_Reco',
+#    'QCD_80to120_Reco',
+#    'QCD_120to170_Reco',
+#    'QCD_170to300_Reco',
+#    'QCD_300to470_Reco',
+#    'QCD_470to600_Reco',
+#    'QCD_600to800_Reco',
+#    'QCD_800to1000_Reco',
+#    'QCD_1000to1400_Reco',
+#    'QCD_1400to1800_Reco',
+#    'QCD_1800_Reco',   
+###Add any new samples for pileup reweighting here####
+    
+
+]
+
+
