afade0986dc7ad2609a28f5b67316159733e5667
wulsin <>
2013-03-20 15:16:47 +0000

    trackAnalyzer_cfg.py

--- StandardAnalysis/test/trackAnalyzer_cfg.py
+++ StandardAnalysis/test/trackAnalyzer_cfg.py
@@ -6,11 +6,14 @@ from OSUT3Analysis.AnaTools.osuAnalysis_cfi import *
 
 #uncomment this line to add a single file to be processed
 #process.source.fileNames.append('file:'+os.environ['CMSSW_BASE']+'/src/OSUT3Analysis/AnaTools/test/stop600toBottom_100mm.root')
-#process.source.fileNames.append('file:/data/se/store/user/jbrinson/disappTrksNew_V2/2012A/pat2bean_53x_10_1_38g.root')
-#process.source.fileNames.append('file:/data/se/store/user/jbrinson/disappTrksNew_V2/2012A_V2/pat2bean_53x_9_3_wHZ.root')
-#process.source.fileNames.append('file:/data/se/store/user/wulsin/disappTrksBkgdMC_V6/WJetsToLNu//pat2bean_53x_19_1_n6n.root')
+#process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/DYJetsToLL_V6/pat2bean_53x_363_1_5wf.root')
+process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/WJetsToLNu/pat2bean_53x_10_1_3IZ.root')
 #process.source.fileNames.append('file:/afs/cern.ch/user/j/jbrinson/public/disappTrks/analysisFilesFromOSU/dataFiletoTestTemplate.root')
 process.source.fileNames.append('file:/afs/cern.ch/work/j/jbrinson/public/analysisFilesFromOSU/SigFiletoTestTemp.root')
+
+
+                                
+
 
 #uncomment these 3 lines to add all files in a given directory to be processed
 #dir = "/data/se/store/user/abrinke1/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1_BEAN_53xOn53x_V02_CV01/894bf83260076a22df0c97ce24c6bb58/"
@@ -28,9 +31,13 @@ process.maxEvents.input = 1000
 #False is recommended under normal circumstances
 process.OSUAnalysis.plotAllObjectsInPassingEvents = False
 
-#overwrite the default collections
+
+#overwrite the default inputs in osuAnalysis_cfi
 process.OSUAnalysis.muons     = cms.InputTag('BNproducer', 'selectedPatMuons')
 process.OSUAnalysis.electrons = cms.InputTag('BNproducer', 'selectedPatElectrons')
+
+process.OSUAnalysis.doPileupReweighting = cms.bool(False)
+process.OSUAnalysis.puFile = cms.string (os.environ['CMSSW_BASE']+'/src/OSUT3Analysis/Configuration/data/pu_disappTrks.root')
 
 
 
@@ -65,6 +72,10 @@ process.OSUAnalysis.channels.append(SigRegWithMaxCalo)
 
 # Other channels used to make plots; exact composition of bkgds may not be correct.   
 process.OSUAnalysis.channels.append(PreSelection)
+process.OSUAnalysis.channels.append(PreSelectionPMissing)
+process.OSUAnalysis.channels.append(PreSelectionPEiso)
+process.OSUAnalysis.channels.append(PreSelectionElectronId)
+process.OSUAnalysis.channels.append(PreSelectionPionId)
 process.OSUAnalysis.channels.append(PreSelectionWithTrigJetMet)
 process.OSUAnalysis.channels.append(CtrlReg)
 #process.OSUAnalysis.channels.append(CtrlRegWithTrigJetMet)
