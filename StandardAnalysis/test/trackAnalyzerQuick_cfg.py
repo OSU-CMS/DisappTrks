c2ad5ce31a308c5ae2095fa243bbf6a8d67cdbf7
wulsin <>
2013-03-21 11:44:53 +0000

    config file for quick studies

---
+++ StandardAnalysis/test/trackAnalyzerQuick_cfg.py
@@ -0,0 +1,19 @@
+# Use to run a quick condor job, e.g., to get the number of events.  
+#
+# Usage:
+# > cmsRun trackAnalyzerQuick_cfg.py 2>&1 | tee trackAnalyzerQuick_cfg.log 
+
+import FWCore.ParameterSet.Config as cms
+import trackAnalyzer_cfg as pset
+
+##########################################################
+##### Add the Desired Channels to the List to be Run #####
+##########################################################
+from DisappTrksT3ANTemp.StandardAnalysis.MyEventSelections_disappTrks import *
+pset.process.OSUAnalysis.channels = cms.VPSet()  
+pset.process.OSUAnalysis.channels.append(PreSelection)
+
+
+# Run the job.
+process = pset.process
+                 
