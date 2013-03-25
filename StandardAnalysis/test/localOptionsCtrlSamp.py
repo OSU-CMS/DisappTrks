9890a5abd9948f5dcdbe885dcd12d28a590d6c93
jbrinson <>
2013-03-25 12:58:25 +0000

    Changed/added the new config files

--- StandardAnalysis/test/localOptionsCtrlSamp.py
+++ StandardAnalysis/test/localOptionsCtrlSamp.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python
 
-config_file = "trackAnalyzerStandard_cfg.py"
-#config_file = "trackAnalyzerQuick_cfg.py"
+config_file = "trackAnalyzerCtrlSamp_cfg.py"
 
 intLumi = 19500.
 
 datasets = [
-
-    'MET_data_Reco',
-    
-    'AMSB_mGrav50K_0p5ns_Reco',
-    'AMSB_mGrav50K_1ns_Reco',
-    'AMSB_mGrav50K_5ns_Reco',
-    
     'DYJetsToLL_Reco',
-    'WJetsToLNu_Reco',
-    'QCD_Reco',
+#    'WJetsToLNu_Reco',
     
     ]
 
--- StandardAnalysis/test/localOptionsSigData.py
+++ StandardAnalysis/test/localOptionsSigData.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-config_file = "trackAnalyzer_cfg.py"
+config_file = "trackAnalyzerStandard_cfg.py"
+#config_file = "trackAnalyzerQuick_cfg.py"
 
 intLumi = 19500.
 
