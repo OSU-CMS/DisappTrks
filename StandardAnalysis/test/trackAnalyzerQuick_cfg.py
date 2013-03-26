9b642214b06cf396bd4d7367117524c510ebb741
wulsin <>
2013-03-26 13:28:03 +0000

    set up for quick studies on Tier 3

--- StandardAnalysis/test/trackAnalyzerQuick_cfg.py
+++ StandardAnalysis/test/trackAnalyzerQuick_cfg.py
@@ -1,18 +1,27 @@
 # Use to run a quick condor job, e.g., to get the number of events.  
 #
 # Usage:
-# > cmsRun trackAnalyzerQuick_cfg.py 2>&1 | tee trackAnalyzerQuick_cfg.log 
+# > cmsRun trackAnalyzerQuick_cfg.py |& tee trackAnalyzerQuick_cfg.log 
+
+
 
 import FWCore.ParameterSet.Config as cms
 from DisappTrksT3ANTemp.StandardAnalysis.trackAnalyzerCondor_cfg import *
+
+#uncomment this line to add a single file to be processed
+#process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/DYJetsToLL_V6/pat2bean_53x_363_1_5wf.root')
+process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/WJetsToLNu/pat2bean_53x_10_1_3IZ.root')
+
+
+
 ##########################################################
 ##### Add the Desired Channels to the List to be Run #####
 ##########################################################
 from DisappTrksT3ANTemp.StandardAnalysis.MyEventSelections_disappTrks import *
-pset.process.OSUAnalysis.channels = cms.VPSet()  
-pset.process.OSUAnalysis.channels.append(PreSelection)
+process.OSUAnalysis.channels = cms.VPSet()  
+process.OSUAnalysis.channels.append(PreSelection)
 
 
-# Run the job.
-process = pset.process
+
+
                  
