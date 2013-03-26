2b1e22d33991b420b9f58f76eda2edda0cb4d43b
wulsin <>
2013-03-26 10:16:12 +0000

    update to use trackAnalyzerCondor_cfg.py

--- StandardAnalysis/test/trackAnalyzerLxplus_cfg.py
+++ StandardAnalysis/test/trackAnalyzerLxplus_cfg.py
@@ -7,7 +7,8 @@ import FWCore.ParameterSet.Config as cms
 from DisappTrksT3ANTemp.StandardAnalysis.trackAnalyzerCondor_cfg import *
 
 process.source.fileNames = cms.untracked.vstring ()
-process.source.fileNames.append('file:/afs/cern.ch/work/j/jbrinson/public/analysisFilesFromOSU/SigFiletoTestTemp.root')
+#process.source.fileNames.append('file:/afs/cern.ch/work/j/jbrinson/public/analysisFilesFromOSU/SigFiletoTestTemp.root')  
+process.source.fileNames.append('file:/afs/cern.ch/user/w/wulsin/workspace/public/disappTrk/dataCopyFromOSU/WJetsToLNu_TuneZ2Star_8TeV/pat2bean_53x_10_1_80n.root')  
 
 
 ##########################################################
@@ -16,8 +17,10 @@ process.source.fileNames.append('file:/afs/cern.ch/work/j/jbrinson/public/analys
 from DisappTrksT3ANTemp.StandardAnalysis.MyEventSelections_disappTrks import *
 process.OSUAnalysis.channels = cms.VPSet()  
 process.OSUAnalysis.channels.append(PreSelection)
+process.OSUAnalysis.channels.append(PreSelectionPMissing)
+process.OSUAnalysis.channels.append(PreSelectionPMissingElectronId)
+process.OSUAnalysis.channels.append(PreSelectionPMissingPionId)
+process.OSUAnalysis.channels.append(PreSelectionPMissingNotGenMatched)
+#process.OSUAnalysis.channels.append(ZtoMuMu)
 
 
-# Run the job.
-process = pset.process
-                 
