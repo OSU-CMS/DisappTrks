277e6603067a765e44f4380e3dbebd17b4ad7567
wulsin <>
2013-03-27 12:53:27 +0000

    more testing

--- StandardAnalysis/test/trackAnalyzerLxplus_cfg.py
+++ StandardAnalysis/test/trackAnalyzerLxplus_cfg.py
@@ -7,8 +7,8 @@ import FWCore.ParameterSet.Config as cms
 from DisappTrksT3ANTemp.StandardAnalysis.trackAnalyzerCondor_cfg import *
 
 process.source.fileNames = cms.untracked.vstring ()
-#process.source.fileNames.append('file:/afs/cern.ch/work/j/jbrinson/public/analysisFilesFromOSU/SigFiletoTestTemp.root')  
-process.source.fileNames.append('file:/afs/cern.ch/user/w/wulsin/workspace/public/disappTrk/dataCopyFromOSU/WJetsToLNu_TuneZ2Star_8TeV/pat2bean_53x_10_1_80n.root')  
+process.source.fileNames.append('file:/afs/cern.ch/work/j/jbrinson/public/analysisFilesFromOSU/SigFiletoTestTemp.root')  
+#process.source.fileNames.append('file:/afs/cern.ch/user/w/wulsin/workspace/public/disappTrk/dataCopyFromOSU/WJetsToLNu_TuneZ2Star_8TeV/pat2bean_53x_10_1_80n.root')  
 
 
 ##########################################################
@@ -22,5 +22,23 @@ process.OSUAnalysis.channels.append(PreSelectionPMissingElectronId)
 process.OSUAnalysis.channels.append(PreSelectionPMissingPionId)
 process.OSUAnalysis.channels.append(PreSelectionPMissingNotGenMatched)
 #process.OSUAnalysis.channels.append(ZtoMuMu)
+
+
+#import the desired sets of histograms from the standard python file which defines them
+
+#import user-defined histograms
+from DisappTrksT3ANTemp.StandardAnalysis.MyHistogramDefinitions_disappTrks import *  
+from OSUT3Analysis.Configuration.histogramDefinitions import *
+process.OSUAnalysis.histogramSets = cms.VPSet()  
+process.OSUAnalysis.histogramSets.append(ShortTrackHistograms)
+process.OSUAnalysis.histogramSets.append(TestTrackHistograms)
+process.OSUAnalysis.histogramSets.append(TestEventHistograms)
+
+
+
+# Change other settings as desired
+#process.OSUAnalysis.printEventInfo = cms.bool(True)  
+
+
 
 
