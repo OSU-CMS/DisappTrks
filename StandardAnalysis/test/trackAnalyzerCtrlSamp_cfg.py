e70b9616b730b057ffa07d1f8b9c042049cfa824
jbrinson <>
2013-03-28 09:14:27 +0000

    Added different channels for electrona and pion control samples

--- StandardAnalysis/test/trackAnalyzerCtrlSamp_cfg.py
+++ StandardAnalysis/test/trackAnalyzerCtrlSamp_cfg.py
@@ -4,11 +4,9 @@
 from OSUT3Analysis.AnaTools.osuAnalysis_cfi import *
 from DisappTrksT3ANTemp.StandardAnalysis.trackAnalyzerCondor_cfg import *
 
-#uncomment this line to add a single file to be processed
-#pset.process.source.fileNames.append('file:/afs/cern.ch/work/j/jbrinson/public/analysisFilesFromOSU/SigFiletoTestTemp.root')  
-process.source.fileNames.append('file:/afs/cern.ch/user/w/wulsin/workspace/public/disappTrk/dataCopyFromOSU/WJetsToLNu_TuneZ2Star_8TeV/pat2bean_53x_10_1_80n.root')  
-#process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/DYJetsToLL_V6/pat2bean_53x_363_1_5wf.root')
 
+process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/DYJetsToLL_V6/pat2bean_53x_363_1_5wf.root')
+process.maxEvents.input = 1000
 
 ########################################################################
 ##### Import the information about all the histograms to be filled #####
@@ -17,6 +15,11 @@ process.source.fileNames.append('file:/afs/cern.ch/user/w/wulsin/workspace/publi
 from DisappTrksT3ANTemp.StandardAnalysis.MyHistogramDefinitions_disappTrks import *  
 process.OSUAnalysis.histogramSets.append(ExtraMuonHistograms)
 process.OSUAnalysis.histogramSets.append(ExtraElectronHistograms)
+process.OSUAnalysis.histogramSets.append(ExtraElectronTrackHistograms)
+#For pions
+process.OSUAnalysis.histogramSets.append(ExtraTauHistograms)
+process.OSUAnalysis.histogramSets.append(ExtraMuonTauHistograms)
+process.OSUAnalysis.histogramSets.append(ExtraMuonTrackHistograms)
 
 ##########################################################
 ##### Add the Desired Channels to the List to be Run #####
@@ -25,16 +28,26 @@ process.OSUAnalysis.histogramSets.append(ExtraElectronHistograms)
 from DisappTrksT3ANTemp.StandardAnalysis.MyEventSelections_disappTrks import *
 
 # First two channels needed to estimate yields
-process.OSUAnalysis.channels.append(SigReg)
-process.OSUAnalysis.channels.append(SigRegWithTrigJetMet)
-process.OSUAnalysis.channels.append(SigRegWithMaxCalo)
+#process.OSUAnalysis.channels.append(SigReg)
+#process.OSUAnalysis.channels.append(SigRegWithTrigJetMet)
+#process.OSUAnalysis.channels.append(SigRegWithMaxCalo)
 
 # Other channels used to make plots; exact composition of bkgds may not be correct.   
-process.OSUAnalysis.channels.append(PreSelection)
-process.OSUAnalysis.channels.append(PreSelectionElectronId)
-process.OSUAnalysis.channels.append(PreSelectionWithTrigJetMet)
-process.OSUAnalysis.channels.append(FitRegWithMaxCalo)
+#process.OSUAnalysis.channels.append(PreSelection)
+#process.OSUAnalysis.channels.append(PreSelectionElectronId)
+#process.OSUAnalysis.channels.append(PreSelectionWithTrigJetMet)
+#process.OSUAnalysis.channels.append(FitRegWithMaxCalo)
+
+from DisappTrksT3ANTemp.StandardAnalysis.MyCtrlSampleSelections_disappTrks import *
 process.OSUAnalysis.channels.append(ZtoMuMu)
 process.OSUAnalysis.channels.append(ZtoEE)  
+process.OSUAnalysis.channels.append(ZtoEEPreSel)  
+process.OSUAnalysis.channels.append(ZtoETrackPreSel)  
+process.OSUAnalysis.channels.append(ZtoETrackFullPreSel)  
+##For pions
+process.OSUAnalysis.channels.append(ZtoTauTau)
+process.OSUAnalysis.channels.append(ZtoMuTau)
+process.OSUAnalysis.channels.append(ZtoMuTrackPreSel)
+process.OSUAnalysis.channels.append(ZtoMuTrackFullPreSel)
 
 
