cf1a64cdac6115fb65199831ca760ffc7ee77ad2
jbrinson <>
2013-03-29 13:41:30 +0000

    Added data control samples

---
+++ StandardAnalysis/test/localOptionsCtrlSample.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+config_file = "trackAnalyzerCtrlSamp_cfg.py"
+
+intLumi = 19500.
+
+datasets = [
+    'SingleElec_data_Reco',
+#    'SingleMu_data_Reco',
+
+    'DYJetsToLL_Reco',
+#    'WJetsToLNu_Reco',
+    
+    ]
+
+
+#maxEvents = {
+#'DYJetsToLL'     : 100,
+#'WJetsToLNu'     : 100,
+#'QCD_0to5'       : 100,
+#'QCD_5to15'      : 100,
+#'QCD_15to30'     : 100,
+#'QCD_30to50'     : 100,
+#'QCD_80to120'    : 100,
+#'QCD_120to170'   : 100,
+#'QCD_170to300'   : 100,
+#'QCD_470to600'   : 100,
+#'QCD_600to800'   : 100,
+#'QCD_800to1000'  : 100,
+#'QCD_1000to1400' : 100,
+#'QCD_1400to1800' : 100,
+#'QCD_1800'       : 100,
+#}
+
