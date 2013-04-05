46a6158243568bf4e9415018c6c1abfb779b6b30
wulsin <>
2013-04-05 10:53:46 +0000

    AOD datasets for testing

---
+++ StandardAnalysis/test/localOptionsAODSamples.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+config_file = "trackAnalyzerStandard_cfg.py"
+
+intLumi = 19500.
+
+datasets = [    
+    'MET_data_Reco',
+
+    'TTbar',
+    'Diboson',
+    'DY',
+    'Wjets',
+    'QCD',
+       
+    ]
