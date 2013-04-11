954f5b1e39255174e09803fe43a51eb8fbf48f95
jbrinson <>
2013-04-11 15:43:45 +0000

    *** empty log message ***

--- StandardAnalysis/test/trackAnalyzerCtrlSamp_cfg.py
+++ StandardAnalysis/test/trackAnalyzerCtrlSamp_cfg.py
@@ -5,7 +5,9 @@ from OSUT3Analysis.AnaTools.osuAnalysis_cfi import *
 from DisappTrksT3ANTemp.StandardAnalysis.trackAnalyzerCondor_cfg import *
 
 
-process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/DYJetsToLL_V6/pat2bean_53x_363_1_5wf.root')
+#process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/DYJetsToLL_V6/pat2bean_53x_363_1_5wf.root')
+process.source.fileNames.append('file:/mnt/hadoop/mc/DYToTauTau_M-20_CT10_TuneZ2star_v2_8TeV-powheg-tauola-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_DISPLACED_LEPTON-v3/bean_989_1_ACv.root')
+#process.source.fileNames.append('file:/mnt/hadoop/mc/DYToMuMu_M-20_CT10_TuneZ2star_v2_8TeV-powheg-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_DISPLACED_LEPTON-v3/bean_4514_1_Jov.root')
 process.maxEvents.input = 1000
 
 ########################################################################
@@ -27,27 +29,22 @@ process.OSUAnalysis.histogramSets.append(ExtraMuonTrackHistograms)
 
 from DisappTrksT3ANTemp.StandardAnalysis.MyEventSelections_disappTrks import *
 
-# First two channels needed to estimate yields
-#process.OSUAnalysis.channels.append(SigReg)
-#process.OSUAnalysis.channels.append(SigRegWithTrigJetMet)
-#process.OSUAnalysis.channels.append(SigRegWithMaxCalo)
+#from DisappTrksT3ANTemp.StandardAnalysis.MyCtrlSampleSelections_disappTrks import *
+from DisappTrksT3ANTemp.StandardAnalysis.MyElectronCtrlSampleSelections_disappTrks import *
+#process.OSUAnalysis.channels.append(ZtoEE)  
+#process.OSUAnalysis.channels.append(ZtoETrackPreSel)  
+process.OSUAnalysis.channels.append(ZtoETrackFullPreSel)
 
-# Other channels used to make plots; exact composition of bkgds may not be correct.   
-#process.OSUAnalysis.channels.append(PreSelection)
-#process.OSUAnalysis.channels.append(PreSelectionElectronId)
-#process.OSUAnalysis.channels.append(PreSelectionWithTrigJetMet)
-#process.OSUAnalysis.channels.append(FitRegWithMaxCalo)
+from DisappTrksT3ANTemp.StandardAnalysis.MyMuonCtrlSampleSelections_disappTrks import *
+#process.OSUAnalysis.channels.append(ZtoMuMu)
+#process.OSUAnalysis.channels.append(ZtoMuTrackPreSel)
+process.OSUAnalysis.channels.append(ZtoMuTrackFullPreSel)
 
-from DisappTrksT3ANTemp.StandardAnalysis.MyCtrlSampleSelections_disappTrks import *
-process.OSUAnalysis.channels.append(ZtoMuMu)
-process.OSUAnalysis.channels.append(ZtoEE)  
-process.OSUAnalysis.channels.append(ZtoEEPreSel)  
-process.OSUAnalysis.channels.append(ZtoETrackPreSel)  
-process.OSUAnalysis.channels.append(ZtoETrackFullPreSel)  
-##For pions
+from DisappTrksT3ANTemp.StandardAnalysis.MyTauCtrlSampleSelections_disappTrks import *
 process.OSUAnalysis.channels.append(ZtoTauTau)
 process.OSUAnalysis.channels.append(ZtoMuTau)
-process.OSUAnalysis.channels.append(ZtoMuTrackPreSel)
-process.OSUAnalysis.channels.append(ZtoMuTrackFullPreSel)
+#process.OSUAnalysis.channels.append(ZtoTauTrackPreSel)
+process.OSUAnalysis.channels.append(ZtoTauTrackFullPreSel)
+
 
 
