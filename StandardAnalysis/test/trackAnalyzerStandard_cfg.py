a1e4690d39a8b87bc65d8ae36ed93aad68d7c6b4
jbrinson <>
2013-04-23 13:01:22 +0000

    some channel names changed

--- StandardAnalysis/test/trackAnalyzerStandard_cfg.py
+++ StandardAnalysis/test/trackAnalyzerStandard_cfg.py
@@ -5,11 +5,15 @@ from DisappTrksT3ANTemp.StandardAnalysis.trackAnalyzerCondor_cfg import *
 ###############################################################
 
 #uncomment this line to add a single file to be processed
-process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/DYJetsToLL_V6/pat2bean_53x_363_1_5wf.root')
+#process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/DYJetsToLL_V6/pat2bean_53x_363_1_5wf.root')
 #process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/WJetsToLNu/pat2bean_53x_10_1_3IZ.root')
+process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/SigMC_LL01_mGrav150K_5ns/pat2bean_53x_5.root')
 #process.source.fileNames.append('file:/afs/cern.ch/user/j/jbrinson/public/disappTrks/analysisFilesFromOSU/dataFiletoTestTemplate.root')
 #process.source.fileNames.append('file:/afs/cern.ch/work/j/jbrinson/public/analysisFilesFromOSU/SigFiletoTestTemp.root')
-
+process.maxEvents.input = 1000
+#process.OSUAnalysis.dataset = cms.string ('DYJetsToLL_Reco')
+#process.OSUAnalysis.dataset = cms.string ('WJetsToLNu_Reco')
+process.OSUAnalysis.doPileupReweighting = cms.bool(False)
 #output file name when running interactively
 process.TFileService.fileName = 'hist.root'
 
@@ -25,29 +29,46 @@ process.TFileService.fileName = 'hist.root'
 
 from DisappTrksT3ANTemp.StandardAnalysis.MyEventSelections_disappTrks import *
 
-# First two channels needed to estimate yields
+## Signal Region Channels ##
+
 process.OSUAnalysis.channels.append(SigRegWithMaxCaloPUCorr)
 process.OSUAnalysis.channels.append(SigRegWithMaxCaloLoose)
-process.OSUAnalysis.channels.append(SigRegWithTrigJetMetPUCorr)
-process.OSUAnalysis.channels.append(SigRegWithTrigJetMet)
+process.OSUAnalysis.channels.append(SigRegWithMaxCaloPUCorrWithTrigJetMet)
+process.OSUAnalysis.channels.append(SigRegWithMaxCaloLooseWithTrigJetMet)
 
 # Other channels used to make plots; exact composition of bkgds may not be correct.   
-process.OSUAnalysis.channels.append(PreSelection)
-process.OSUAnalysis.channels.append(PreSelectionPMissing)
+## Preselection Channels ##
+
+#process.OSUAnalysis.channels.append(PreSelection)
 
 process.OSUAnalysis.channels.append(PreSelectionWithTrigJetMet)
-process.OSUAnalysis.channels.append(PreSelectionIsoTrkOnlyWithTrigJetMet)  
-process.OSUAnalysis.channels.append(PreSelectionMuonVetoOnlyWithTrigJetMet)  
+#process.OSUAnalysis.channels.append(PreSelectionIsoTrkOnlyWithTrigJetMet)  
+#process.OSUAnalysis.channels.append(PreSelectionIsoTrkOnlyNoD0WithTrigJetMet)  
+#process.OSUAnalysis.channels.append(PreSelectionIsoTrkOnlyNoDzWithTrigJetMet)  
+#process.OSUAnalysis.channels.append(PreSelectionMuonVetoOnlyWithTrigJetMet)  
+#process.OSUAnalysis.channels.append(PreSelectionPMissingWithTrigJetMet)
 
-process.OSUAnalysis.channels.append(PreSelectionWithTrigJetMetNoHitCut)
-process.OSUAnalysis.channels.append(PreSelectionIsoTrkOnlyWithTrigJetMetNoHitCut)  
-process.OSUAnalysis.channels.append(PreSelectionMuonVetoOnlyWithTrigJetMetNoHitCut)  
+## For AOD Samples ##
 
-#Other available channels
-#process.OSUAnalysis.channels.append(PreSelectionPMissing)
-#process.OSUAnalysis.channels.append(PreSelectionPEiso)
+#process.OSUAnalysis.channels.append(PreSelectionNoHitCutWithTrigJetMet)
+#process.OSUAnalysis.channels.append(PreSelectionIsoTrkOnlyNoHitCutWithTrigJetMet)  
+#process.OSUAnalysis.channels.append(PreSelectionMuonVetoOnlyNoHitCutWithTrigJetMet)  
+
+## d0 and dz sidebands ##
+
+#process.OSUAnalysis.channels.append(PreSelectionIsoTrkOnlyDzSideWithTrigJetMet)  
+#process.OSUAnalysis.channels.append(PreSelectionIsoTrkOnlyD0SideWithTrigJetMet)  
+
+## GenMatched Channels ##
+
+#process.OSUAnalysis.channels.append(PreSelectionPMissingNotGenMatched)
+#process.OSUAnalysis.channels.append(PreSelectionPMissingPionId)
+#process.OSUAnalysis.channels.append(PreSelectionPMissingElectronId)
 #process.OSUAnalysis.channels.append(PreSelectionElectronId)
 #process.OSUAnalysis.channels.append(PreSelectionPionId)
+
+## Other Channels ##
+
 #process.OSUAnalysis.channels.append(CtrlReg)
 #process.OSUAnalysis.channels.append(CtrlRegWithTrigJetMet)
 #process.OSUAnalysis.channels.append(PreSelectionPt20)
