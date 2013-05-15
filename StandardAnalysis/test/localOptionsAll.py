9592ea8514dd87737a0053d6dca4f5bed1f41793
wulsin <>
2013-05-15 21:25:34 +0000

    rearrange import of configurationOptions.py to prevent error on submitToCondor.py

--- StandardAnalysis/test/localOptionsAll.py
+++ StandardAnalysis/test/localOptionsAll.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+from OSUT3Analysis.Configuration.configurationOptions import *  # Needed if you want to modify (not replace) one of the parameters.  
+
 config_file = "trackAnalyzerStandard_cfg.py"
 #config_file = "trackAnalyzerQuick_cfg.py"
 
-from OSUT3Analysis.Configuration.configurationOptions import *  # Needed if you want to modify (not replace) one of the parameters.  
 
 #intLumi = 19500.
 intLumi = 18046.  # MET 2012 data registered as of 2013-05-07
@@ -12,19 +13,18 @@ intLumi = 18046.  # MET 2012 data registered as of 2013-05-07
 datasets = [
 
     'MET_data_Reco',
-    
+  
     'AMSB_mGrav50K_0p5ns_Reco',
     'AMSB_mGrav50K_1ns_Reco',
     'AMSB_mGrav50K_5ns_Reco',
-    
+
     'WJetsToLNu_Reco',
+    'ZJetsToNuNu', 
     'TTbar_Reco',
     'QCD_Reco',
     'DYJetsToLL_Reco',
-    'ZJetsToNuNu', 
-    'TTbar_Reco',
 
-    'Background', 
+#    'Background', 
 
     ]
 
