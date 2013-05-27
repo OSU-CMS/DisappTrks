40edf4600fab3b8929add73db8b0254d24da21eb
wulsin <>
2013-05-27 11:04:43 +0000

    new file for single-lepton trigger control samples

---
+++ StandardAnalysis/test/localOptionsAODCtrlElec.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from OSUT3Analysis.Configuration.configurationOptions import *  # Needed if you want to modify (not replace) one of the parameters.  
+
+config_file = "trackAnalyzerCtrlElec_cfg.py"
+
+intLumi = 19200. # SingleElectron dataset = 19183.  
+
+
+datasets = [    
+
+       'Diboson',
+       'QCD_BCtoE',
+       'QCD_EMEnriched',
+       'TTbar',
+       'DY',
+       'Wjets',                                                                                                                                                                                           
+       'SingleElectron',    
+
+    ]
+
---
+++ StandardAnalysis/test/localOptionsAODCtrlMuon.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from OSUT3Analysis.Configuration.configurationOptions import *  # Needed if you want to modify (not replace) one of the parameters.  
+
+config_file = "trackAnalyzerCtrlMuon_cfg.py"
+
+intLumi = 19500.
+
+datasets = [    
+
+       'Diboson',
+       'QCD_MuEnriched',
+       'TTbar',
+       'DY',
+       'Wjets',                                                                                                                                                                                           
+       'SingleMu',        
+
+    ]
+
