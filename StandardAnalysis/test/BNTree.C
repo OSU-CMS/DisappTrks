e7aa986a75e831e348838d0d68c51c68a1f028f4
wulsin <>
2013-06-13 12:22:43 +0000

    add MET phi reweighting histograms

--- StandardAnalysis/test/BNTree.C
+++ StandardAnalysis/test/BNTree.C
@@ -41,16 +41,31 @@ void BNTree::Loop(TString outFile)
 
   if (fChain == 0) return;
 
+  TString dir = "";  
+  dir = fChain->GetName();
+  dir = dir(0, dir.Last('/'));  
 
+  // Get histograms for reweighting 
+  TFile* fData  = new TFile("condor/condor_2013_06_04_AODCtrlMuonNewTrig/SingleMu.root", "READ");  
+  TFile* fWjets = new TFile("condor/condor_2013_06_04_AODCtrlMuonNewTrig/Wjets.root",    "READ");  
+
+  TH1* hMetPhiData  = 0;
+  TH1* hMetPhiWjets = 0;
+  fData ->GetObject(dir+"/mets_phi", hMetPhiData);  
+  fWjets->GetObject(dir+"/mets_phi", hMetPhiWjets);  
+  hMetPhiData  ->Scale(1.0 * hMetPhiData ->GetNbinsX() / hMetPhiData ->Integral());  // normalize to area 1.0 * nbins 
+  hMetPhiWjets ->Scale(1.0 * hMetPhiWjets->GetNbinsX() / hMetPhiWjets->Integral());  // normalize to area 1.0 * nbins
+  TH1F* hMetPhiDataOverMC = (TH1F*) hMetPhiData->Clone("hMetPhiDataOverMC");  
+  hMetPhiDataOverMC->Sumw2();  
+  hMetPhiDataOverMC->Divide(hMetPhiWjets);  
+
+  bool isData = outFile.Contains("SingleMu") || outFile.Contains("SingleElec");  // FIXME:  need a better way to do this  
 
   // Open output root file.  
   TFile* fOut = 0;
-  TString dir = "";  
   if (outFile=="") fOut = new TFile("newBNTreePlot.root", "RECREATE");  
   else {
     fOut = new TFile(outFile,              "UPDATE");  
-    dir = fChain->GetName();
-    dir = dir(0, dir.Last('/'));  
     cout << "Debug:  name of dir is: " << dir << endl;  
     fOut->cd(dir);  
   }
@@ -63,11 +78,28 @@ void BNTree::Loop(TString outFile)
     cout << "Could not find directory " << dir << endl;  
   } else {
     tDir->Delete("BNTree_Met;*");  
+    tDir->Delete("BNTree_MetWithPhiRewtToData;*");  
+    tDir->Delete("BNTree_MetWithPhiRewtFlat;*");  
+    tDir->Delete("BNTree_MetPhi;*");  
+    tDir->Delete("BNTree_MetPhiWithPhiRewtToData;*");  
+    tDir->Delete("BNTree_MetPhiWithPhiRewtFlat;*");  
+    tDir->Delete("BNTree_MetPhiCutPhiLo;*");  
+    tDir->Delete("BNTree_MetPhiCutPhiMed;*");  
+    tDir->Delete("BNTree_MetPhiCutPhiHi;*");  
+
   }
 
   // Declare desired histograms.  
-  TH1F* hMet = new TH1F("BNTree_Met", ";MET (GeV)", 100, 0, 500);  
-
+  TH1F* hMet                     = new TH1F("BNTree_Met",                     ";MET (GeV)", 100, 0, 500);  
+  TH1F* hMetWithPhiRewtToData    = new TH1F("BNTree_MetWithPhiRewtToData",    ";MET (GeV)", 100, 0, 500);  
+  TH1F* hMetWithPhiRewtFlat      = new TH1F("BNTree_MetWithPhiRewtFlat",      ";MET (GeV)", 100, 0, 500);  
+  TH1F* hMetPhi                  = new TH1F("BNTree_MetPhi",                  ";MET #phi",  100, -3.15, 3.15);  
+  TH1F* hMetPhiWithPhiRewtToData = new TH1F("BNTree_MetPhiWithPhiRewtToData", ";MET #phi",  100, -3.15, 3.15);  
+  TH1F* hMetPhiWithPhiRewtFlat   = new TH1F("BNTree_MetPhiWithPhiRewtFlat",   ";MET #phi",  100, -3.15, 3.15);  
+  TH1F* hMetPhiCutPhiLo          = new TH1F("BNTree_MetPhiCutPhiLo",          ";MET #phi",  100, -3.15, 3.15);  
+  TH1F* hMetPhiCutPhiMed         = new TH1F("BNTree_MetPhiCutPhiMed",         ";MET #phi",  100, -3.15, 3.15);  
+  TH1F* hMetPhiCutPhiHi          = new TH1F("BNTree_MetPhiCutPhiHi",          ";MET #phi",  100, -3.15, 3.15);  
+  
 
   Long64_t nentries = fChain->GetEntries();
 
@@ -97,23 +129,43 @@ void BNTree::Loop(TString outFile)
       events_muonScaleFactor    ->at(0) * 
       events_electronScaleFactor->at(0);  
 
-    hMet->Fill(mets_pt->at(0), BNTreeWt);  
-    // cout << "Debug:  met = " << mets_pt->at(0) << std::endl;  
-    // cout << "Debug:  met size = " << mets_pt->size() << std::endl;  
-    //      std::cout << "Debug:  events_weight = " << events_weight << std::endl;  
-    //      cout << "Debug:  events_weight size = " << events_weight->size() << std::endl;  
+    // Calculate reweighting.
+    double wtToData = hMetPhiDataOverMC->GetBinContent(hMetPhiDataOverMC->FindBin(mets_phi->at(0)));  
+    if (isData) wtToData = 1.0;  
+    double      wtToFlat = 1.0 / hMetPhiWjets->GetBinContent(hMetPhiWjets->FindBin(mets_phi->at(0)));  
+    if (isData) wtToFlat = 1.0 / hMetPhiData ->GetBinContent(hMetPhiData ->FindBin(mets_phi->at(0)));  
 
-    // if (Cut(ientry) < 0) continue;
-  }
+    hMet                     ->Fill(mets_pt->at(0), BNTreeWt);  
+    hMetWithPhiRewtToData    ->Fill(mets_pt->at(0), BNTreeWt * wtToData);  
+    hMetWithPhiRewtFlat      ->Fill(mets_pt->at(0), BNTreeWt * wtToFlat);  
+
+    hMetPhi                  ->Fill(mets_phi->at(0), BNTreeWt);  
+    hMetPhiWithPhiRewtToData ->Fill(mets_phi->at(0), BNTreeWt * wtToData);  
+    hMetPhiWithPhiRewtFlat   ->Fill(mets_phi->at(0), BNTreeWt * wtToFlat);  
+    hMetPhiCutPhiLo          ->Fill(mets_phi->at(0), BNTreeWt * (mets_pt->at(0) < 100));  
+    hMetPhiCutPhiMed         ->Fill(mets_phi->at(0), BNTreeWt * (mets_pt->at(0) > 100 && mets_pt->at(0) < 200));  
+    hMetPhiCutPhiHi          ->Fill(mets_phi->at(0), BNTreeWt * (mets_pt->at(0) > 200));  
+
+
+  }  // end   for (Long64_t jentry=0; jentry<nentries;jentry++) {
 
   cout << "Debug:  mean of hMet = " << hMet->GetMean() << endl;  
 
-  // close output root file 
-  hMet->Write();
+  hMet                     ->Write();  
+  hMetWithPhiRewtToData    ->Write();  
+  hMetWithPhiRewtFlat      ->Write();  
+  hMetPhi                  ->Write();  
+  hMetPhiWithPhiRewtToData ->Write();  
+  hMetPhiWithPhiRewtFlat   ->Write();  
+  hMetPhiCutPhiLo          ->Write();  
+  hMetPhiCutPhiMed         ->Write();  
+  hMetPhiCutPhiHi          ->Write();  
+  hMetPhiDataOverMC        ->Write();  
+
   fOut->Close();  
 
   cout << "Finished BNTree::Loop() successfully." << endl;
-  cout << "Total time:  " << flush;
+  cout << "Total time to run BNTree::Loop():  " << flush;
   timer.Print();  
 
 
