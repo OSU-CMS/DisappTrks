76ca0ebe0ea3f5702050eae537ff7aac9c9e8804
wulsin <>
2013-06-19 13:57:29 +0000

    Add monojet selection

--- StandardAnalysis/test/BNTreeMonojet.C
+++ StandardAnalysis/test/BNTreeMonojet.C
@@ -74,6 +74,19 @@ void BNTree::Loop(TString outFile)
   TH1::SetDefaultSumw2();
 
   TH1F* hNJets                   = new TH1F("BNHist_NJets",                 ";Jet mulitiplicity", 10, 0, 10);  
+  TH1F* hNElecs                  = new TH1F("BNHist_NElecs",                ";Elec mulitiplicity", 11, -0.5, 10.5);   
+  TH1F* hNMuons                  = new TH1F("BNHist_NMuons",                ";Muon mulitiplicity", 11, -0.5, 10.5);   
+  TH1F* hNTaus                   = new TH1F("BNHist_NTaus",                 ";Tau  mulitiplicity", 11, -0.5, 10.5);   
+  TH1F* hNElecsNoCut             = new TH1F("BNHist_NElecsNoCut",           ";Elec mulitiplicity (no elec cut)", 11, -0.5, 10.5);   
+  TH1F* hNMuonsNoCut             = new TH1F("BNHist_NMuonsNoCut",           ";Muon mulitiplicity (no muon cut)", 11, -0.5, 10.5);   
+  TH1F* hNTausNoCut              = new TH1F("BNHist_NTausNoCut",            ";Tau  mulitiplicity (no tau cut)",  11, -0.5, 10.5);   
+  TH1F* hElecMVA                 = new TH1F("BNHist_ElecMVA",               ";Elec MVA",           100, -2, 2);
+  TH1F* hNMets                   = new TH1F("BNHist_NMets",                 ";Met mulitiplicity",  11, -0.5, 10.5);  
+  TH1F* hNMetsCut0               = new TH1F("BNHist_NMetsCut0",             ";Met mulitiplicity (cut 0)",  11, -0.5, 10.5);  
+  TH1F* hNMetsCut1               = new TH1F("BNHist_NMetsCut1",             ";Met mulitiplicity (cut 1)",  11, -0.5, 10.5);  
+  TH1F* hNMetsCut2               = new TH1F("BNHist_NMetsCut2",             ";Met mulitiplicity (cut 2)",  11, -0.5, 10.5);  
+  TH1F* hNMetsCut3               = new TH1F("BNHist_NMetsCut3",             ";Met mulitiplicity (cut 3)",  11, -0.5, 10.5);  
+  TH1F* hNMetsCut4               = new TH1F("BNHist_NMetsCut4",             ";Met mulitiplicity (cut 4)",  11, -0.5, 10.5);  
 
     
   Long64_t nentries = fChain->GetEntries();
@@ -101,11 +114,95 @@ void BNTree::Loop(TString outFile)
       events_muonScaleFactor    ->at(0) * 
       events_electronScaleFactor->at(0);  
 
-    hNJets->Fill(jets_pt->size(),    BNTreeWt);  	
+    // Count other physics objects, according to AN-2012-421-v6.  
+    int numJets  = 0;
+    int numMuons = 0;
+    int numElecs = 0;
+    int numTaus  = 0; 
+
+    double DiJetDeltaPhi = 0;  // default is 0, so event passes if only one jet found  
+    vector<uint> jetPassedIdx;  
+    for (uint ijet = 0; ijet<jets_pt->size(); ijet++) {
+      // count number of jets that have pT>30 and |eta|<4.5
+      if (!(jets_pt      ->at(ijet)  > 30))  continue;
+      if (!(fabs(jets_eta->at(ijet)) < 4.5)) continue;
+      jetPassedIdx.push_back(ijet);  
+      numJets++;  
+    } 
+    if (numJets>1) {
+      uint idx0 = jetPassedIdx.at(0);
+      uint idx1 = jetPassedIdx.at(1);
+      DiJetDeltaPhi = fabs(fabs(fabs(jets_phi->at(idx0) - jets_phi->at(idx1)) - 3.14159) - 3.14159);  
+    }
+
+    for (uint imuon = 0; imuon<muons_pt->size(); imuon++) {
+      if (!(muons_pt                      ->at(imuon)  > 10))   continue;  
+      if (!(muons_isGlobalMuon ==1 || 
+	    muons_isTrackerMuon==1                         ))   continue;  
+
+      // Selection below is for positive muon ID, not muon veto!
+      // if (!(muons_pt                      ->at(imuon)  > 20))   continue;
+      // if (!(fabs(muons_eta                ->at(imuon)) < 2.1))  continue;
+      // if (!(muons_isGlobalMuon            ->at(imuon) == 1))    continue;
+      // if (!(fabs(muons_correctedD0Vertex  ->at(imuon)) < 0.02)) continue;
+      // // FIXME:  no R<0.2 cut applied 
+      // if (!(muons_numberOfValidPixelHits  ->at(imuon) >= 1))    continue;
+      // if (!(muons_numberOfValidTrackerHits->at(imuon) >= 10)) 	continue;
+      // if (!(muons_numberOfMatchedStations ->at(imuon) >= 2))  	continue;
+      // if (!(muons_numberOfValidMuonHits   ->at(imuon) >= 1))  	continue;
+      numMuons++;  
+    }
+
+    for (uint ielec = 0; ielec<electrons_pt->size(); ielec++) {
+      hElecMVA->Fill(electrons_mvaNonTrigV0->at(ielec), BNTreeWt);  
+      if (!(electrons_pt                     ->at(ielec)  > 10))   continue;  
+      if (!(electrons_mvaNonTrigV0           ->at(ielec)  >  0))   continue;  // FIXME:  May need to tune this cut value!  
+      numElecs++;  
+    }
+
+    for (uint itau = 0; itau<taus_pt->size(); itau++) {
+      if (!(taus_pt                                           ->at(itau)  > 10))  continue;  
+      if (!(fabs(taus_pt                		      ->at(itau)) < 2.3)) continue;  
+      if (!(fabs(taus_HPSbyLooseCombinedIsolationDeltaBetaCorr->at(itau)) == 1))  continue;  
+      if (!(fabs(taus_HPSdecayModeFinding                     ->at(itau)) == 1))  continue;  					 
+      if (!(fabs(taus_HPSagainstElectronLoose		      ->at(itau)) == 1))  continue;  					 
+      if (!(fabs(taus_HPSagainstMuonTight    		      ->at(itau)) == 1))  continue;    // monojet uses AgainstMuonTight2  
+      numTaus++;  
+    }
+
+    // Apply other selection requirements, corresponding to https://twiki.cern.ch/twiki/bin/viewauth/CMS/MonojetDataFinalStandard2012v2#Cutflow
+    hNMetsCut0->Fill(mets_pt->size(),    BNTreeWt);  	
+    if (!(numJets  <= 2))     continue;     hNMetsCut1->Fill(mets_pt->size(),    BNTreeWt);  	
+    if (!(DiJetDeltaPhi < 2)) continue;     hNMetsCut2->Fill(mets_pt->size(),    BNTreeWt);  	
+    if (!(numMuons == 0))     continue;     hNMetsCut3->Fill(mets_pt->size(),    BNTreeWt);  	
+    if (!(numElecs == 0))     continue;     hNMetsCut4->Fill(mets_pt->size(),    BNTreeWt);  	
+    if (!(numTaus  == 0))     continue; 
+
+    hNJets            ->Fill(numJets,                   BNTreeWt);  	
+    hNMuons           ->Fill(numMuons,                  BNTreeWt);  	
+    hNElecs           ->Fill(numElecs,                  BNTreeWt);  	
+    hNTaus            ->Fill(numTaus,                   BNTreeWt);  	
+    hNMuonsNoCut      ->Fill(muons_pt->size(),          BNTreeWt);  	
+    hNElecsNoCut      ->Fill(electrons_pt->size(),      BNTreeWt);  	
+    hNTausNoCut       ->Fill(taus_pt->size(),           BNTreeWt);  	
+    hNMets            ->Fill(mets_pt->size(),           BNTreeWt);  	
 
   }  // end   for (Long64_t jentry=0; jentry<nentries;jentry++) {
   
   hNJets                   ->Write();  
+  hElecMVA                 ->Write();  
+  hNElecs                  ->Write();  
+  hNMuons                  ->Write();  
+  hNTaus                   ->Write();  
+  hNElecsNoCut             ->Write();  
+  hNMuonsNoCut             ->Write();  
+  hNTausNoCut              ->Write();  
+  hNMets                   ->Write();  
+  hNMetsCut0               ->Write();  
+  hNMetsCut1               ->Write();  
+  hNMetsCut2               ->Write();  
+  hNMetsCut3               ->Write();  
+  hNMetsCut4               ->Write();  
 
   fOut->Close();  
 
