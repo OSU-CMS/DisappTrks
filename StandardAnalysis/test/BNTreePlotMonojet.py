b9cd84fe7cb3900f20d468ee69c058e50293254c
wulsin <>
2013-06-19 12:02:45 +0000

    config for monojet studies

---
+++ StandardAnalysis/test/BNTreePlotMonojet.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#from localOptionsAll import *
+from localOptionsAll import *
+
+
+## datasets = [
+     
+#   'MET',
+#    'MET_data_Reco',                                                                                                                                                                                    
+
+##     'AMSB_mGrav50K_0p5ns_Reco',                                                                                                                                                                       
+##     'AMSB_mGrav50K_1ns_Reco',                                                                                                                                                                         
+##     'AMSB_mGrav50K_5ns_Reco',                                                                                                                                                                         
+
+##     'Wjets',                                                                                                                                                                                          
+##     'ZJetsToNuNu',                                                                                                                                                                                    
+##     'TTbar',                                                                                                                                                                                          
+##     'QCD',                                                                                                                                                                                            
+##     'DY',                                                                                                                                                                                             
+##     'Diboson',                                                                                                                                                                                         
+    
+##     ]           
+
+BNTreeUseScript = True
+BNTreeScript = 'BNTreeMonojetRun.C'  
+
+BNTreeChannel = 'Monojet'  
+BNTreeWt = 'events_puScaleFactor * events_muonScaleFactor * events_electronScaleFactor'   # excludes lumi weight, which is included automatically 
+
+input_histograms = [
+
+
+    ]
+
+
