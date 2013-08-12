a0a0fa6dc8eec0098d48d807b593bb9c8619a761
wulsin <>
2013-08-12 20:36:58 +0000

    add 2012A dataset

--- StandardAnalysis/test/localOptionsCtrlElec.py
+++ StandardAnalysis/test/localOptionsCtrlElec.py
@@ -4,23 +4,25 @@ from OSUT3Analysis.Configuration.configurationOptions import *  # Needed if you 
 
 config_file = "trackAnalyzerCtrlElec_cfg.py"  
 
-intLumi = 2240.  # For SingleElec_data_Reco dataset 
+# intLumi = 2240.  # For SingleElec_data_Reco dataset 
+intLumi = 883.273  # For /SingleElectron/Run2012A-22Jan2013-v1/AOD 
 
 datasets = [
     
-'SingleElec_data_Reco', 
+'SingleElectron_2012A',
+# 'SingleElec_data_Reco', 
 
-'AMSB_mGrav50K_0p5ns_Reco',
-'AMSB_mGrav50K_1ns_Reco',
-'AMSB_mGrav50K_5ns_Reco',
+## 'AMSB_mGrav50K_0p5ns_Reco',
+## 'AMSB_mGrav50K_1ns_Reco',
+## 'AMSB_mGrav50K_5ns_Reco',
 
 #    'WjetsHighPt',
 'Wjets',  
 'TTbar',
+'SingleTop',
 #    'DY_PtZ100',    
 'DY',
 'Diboson',
-'TTbar',
 'ZJetsToNuNu', 
 'QCD',
 
