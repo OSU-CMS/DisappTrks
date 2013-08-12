b06a46dc477a98f26f1895aefe761defb687583a
wulsin <>
2013-08-12 20:38:46 +0000

    add skim option

--- StandardAnalysis/test/trackAnalyzerCtrlElec_cfg.py
+++ StandardAnalysis/test/trackAnalyzerCtrlElec_cfg.py
@@ -8,9 +8,14 @@ from DisappTrksT3ANTemp.StandardAnalysis.trackAnalyzerCondor_cfg import *
 #process.source.fileNames.append('file:/mnt/hadoop/mc/DYToTauTau_M-20_CT10_TuneZ2star_v2_8TeV-powheg-tauola-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM_DISPLACED_LEPTON-v3/bean_989_1_ACv.root')
 #process.source.fileNames.append('file:/mnt/hadoop/mc/DYToMuMu_M-20_CT10_TuneZ2star_v2_8TeV-powheg-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_DISPLACED_LEPTON-v3/bean_4514_1_Jov.root')
 #process.source.fileNames.append('file:/afs/cern.ch/user/w/wulsin/workspace/public/disappTrk/dataCopyFromOSU/WJetsToLNu_TuneZ2Star_8TeV/pat2bean_53x_154_2_DeK.root')
-process.source.fileNames.append('file:/afs/cern.ch/user/w/wulsin/workspace/public/disappTrk/dataCopyFromOSU/DYJetsToLL/pat2bean_53x_100_1_2KD.root')
+#process.source.fileNames.append('file:/afs/cern.ch/user/w/wulsin/workspace/public/disappTrk/dataCopyFromOSU/DYJetsToLL/pat2bean_53x_100_1_2KD.root')
 #process.source.fileNames.append('file:/mnt/hadoop/se/store/user/abrinke1/SingleElectron/SingleElectron_Run2012A-13Jul2012-v1_BEAN_53xOn53x_V02_CV01/4da6952f3c4fba5e66dcec676ef9c024//ttH_pat2bean_53x_110_1_kpZ.root')
 #process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/SingleElec_Reco_P1//pat2bean_53x_102_1_CCH.root')
+#process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/SingleMu/Run2012D-PromptReco-v1_BEAN2012-v4/ad2797bf6dcd13ca01c5e72b5465df6c/ttH_pat2bean_53x_240_23_ZnR.root')
+#process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/ZJetsToNuNu_100_HT_200_TuneZ2Star_8TeV_madgraph/BEAN2012-v4/0ff8045eb3a4a7ce9562dd332df0072c/ttH_pat2bean_53x_100_1_hBP.root')
+process.source.fileNames.append('file:/mnt/hadoop/se/store/user/jbrinson/SingleElectron/BEAN2012-v4/ad2797bf6dcd13ca01c5e72b5465df6c/ttH_pat2bean_53x_100_2_92I.root')
+
+                                                                                                                                                                                                        
 
 #process.OSUAnalysis.useTrackCaloRhoCorr = cms.bool(False)
 #process.OSUAnalysis.applyLeptonSF       = cms.bool(True)
@@ -18,16 +23,20 @@ process.source.fileNames.append('file:/afs/cern.ch/user/w/wulsin/workspace/publi
 
 process.OSUAnalysis.histogramSets.append(ElectronHistograms)
 process.OSUAnalysis.histogramSets.append(ElectronTrackHistograms)
+process.OSUAnalysis.verbose = cms.int32(2)
 
 ##########################################################
 ##### Add the Desired Channels to the List to be Run #####
 ##########################################################
 from DisappTrksT3ANTemp.StandardAnalysis.MyElectronCtrlSampleSelections_disappTrks import *
 
-process.OSUAnalysis.channels.append(ZtoETrk)
+add_channels (process, [SingleElecTrig], ["keep *", "drop BNtriggers_BNproducer_L1Talgo_BEANs"])
 
+## process.OSUAnalysis.channels.append(ZtoETrk)
+# process.OSUAnalysis.channels.append(SingleElecTrig)
 ## process.OSUAnalysis.channels.append(PreSelElecMatchTrigElecV1)
-## process.OSUAnalysis.channels.append(PreSelElecMatchTrigElecV2)
+
+#process.OSUAnalysis.channels.append(PreSelElecMatchTrigElecV2)
 #process.OSUAnalysis.channels.append(PreSelElecMatchTrigElecV3)
 #process.OSUAnalysis.channels.append(WToENuSimple)
 ## process.OSUAnalysis.channels.append(PreSelElecMatchTrigElecV2NJet1)  
