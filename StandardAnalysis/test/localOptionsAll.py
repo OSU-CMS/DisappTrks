112111e6d37aa4a27b5e707ea590513889a57a79
jbrinson <>
2013-08-20 09:58:51 +0000

    Changed dataset to MET and updated lumi

--- StandardAnalysis/test/localOptionsAll.py
+++ StandardAnalysis/test/localOptionsAll.py
@@ -5,26 +5,40 @@ from OSUT3Analysis.Configuration.configurationOptions import *  # Needed if you 
 config_file = "trackAnalyzerStandard_cfg.py"
 #config_file = "trackAnalyzerQuick_cfg.py"
 
-intLumi = 18046.  # MET 2012 data registered as of 2013-05-07
+intLumi = 19783.
 
 datasets = [
 
-    'MET_data_Reco',
-
+#    'MET_data_Reco',
+    'MET',
+    
     'AMSB_mGrav50K_0p5ns_Reco',
     'AMSB_mGrav50K_1ns_Reco',
     'AMSB_mGrav50K_5ns_Reco',
-
+    
     'WjetsHighPt',
-    'TTbar',
-    'SingleTop',
+    'TTbar_Inclusive',
+    
+    #    'TTbar',
+    #  'SingleTop',
     'DY_PtZ100',    
     #    'DY',
     'Diboson',
     #    'Wjets',  
     'ZJetsToNuNu', 
     'QCD',
+    #   'Background',
     
+    ]
+
+composite_dataset_definitions['Background'] = [
+    'DY_PtZ100',
+    'WjetsHighPt',
+    'SingleTop',
+    'QCD',
+    'TTbar_Inclusive',
+    'ZJetsToNuNu',
+    'Diboson',
     ]
 
 histsToBlind = [
