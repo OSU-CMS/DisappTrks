28ed5e99a1c8e96ab6615f013b55b91dc9bfc0c5
Jessica Brinson <jbrinson@interactive-0-0.local>
2013-12-16 09:15:48 -0500

    fix errors

--- StandardAnalysis/test/localOptionsBkgd.py
+++ StandardAnalysis/test/localOptionsBkgd.py
@@ -4,13 +4,13 @@ from localOptionsAll import *
 
 datasets = [
     # put bkgd datasets in roughly ascending order of size of contribution after preselection
-#    'QCD',
+    'QCD',
     'SingleTop',
-#    'TTbar',
-#    'ZJetsToNuNu',
-#    'DY_PtZ100',
- #   'Diboson',
- #   'WjetsHighPt',
+    'TTbar',
+    'ZJetsToNuNu',
+    'DY_PtZ100',
+    'Diboson',
+    'WjetsHighPt',
     ]
 
 
