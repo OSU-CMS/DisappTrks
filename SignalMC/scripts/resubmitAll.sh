6ac5146435ab37e3cf2d8c9a2483453bd0201b0a
Wells Wulsin <wulsin.1@osu.edu>
2014-02-28 12:47:42 +0100

    Update to fix problem with writing out log file

--- SignalMC/scripts/resubmitAll.sh
+++ SignalMC/scripts/resubmitAll.sh
@@ -1,62 +1,50 @@
 #!/usr/bin/env bash
 
-#for dir in \
-#CharginoAMSB_LL01_1ns  \
-#CharginoAMSB_LL01_0p5ns  \
-#CharginoAMSB_LL01_5ns  \
-#CharginoAMSB_LL01_mGrav50K_1ns  \
-#CharginoAMSB_LL01_mGrav50K_0p5ns  \
-#CharginoAMSB_LL01_mGrav50K_5ns  \
-#CharginoAMSB_LL01_mGrav75K_1ns  \
-#CharginoAMSB_LL01_mGrav75K_0p5ns  \
-#CharginoAMSB_LL01_mGrav75K_5ns  \
-#CharginoAMSB_LL01_mGrav100K_1ns  \
-#CharginoAMSB_LL01_mGrav100K_0p5ns  \
-#CharginoAMSB_LL01_mGrav100K_5ns  \
-#CharginoAMSB_LL01_mGrav125K_1ns  \
-#CharginoAMSB_LL01_mGrav125K_0p5ns  \
-#CharginoAMSB_LL01_mGrav125K_5ns  \
-#CharginoAMSB_LL01_mGrav150K_1ns  \
-#CharginoAMSB_LL01_mGrav150K_0p5ns  \
-#CharginoAMSB_LL01_mGrav150K_5ns  \
+# Usage:
+# /afs/cern.ch/work/w/wulsin/public/disappTrk/signalMCGenV2/CMSSW_5_3_11/src/DisappTrks/SignalMC> 
+# > scripts/resubmitAll.sh  
+
+# Take directories from multicrabAuto.cfg  
+
 
 for dir in \
-CharginoAMSB_LL01_1ns  \
-CharginoAMSB_LL01_0p5ns  \
-CharginoAMSB_LL01_5ns  \
-CharginoAMSB_LL01_mGrav50K_1ns  \
-CharginoAMSB_LL01_mGrav50K_0p5ns  \
-CharginoAMSB_LL01_mGrav50K_5ns  \
-CharginoAMSB_LL01_mGrav75K_1ns  \
-CharginoAMSB_LL01_mGrav75K_0p5ns  \
-CharginoAMSB_LL01_mGrav75K_5ns  \
-CharginoAMSB_LL01_mGrav100K_1ns  \
-CharginoAMSB_LL01_mGrav100K_0p5ns  \
-CharginoAMSB_LL01_mGrav100K_5ns  \
-CharginoAMSB_LL01_mGrav125K_1ns  \
-CharginoAMSB_LL01_mGrav125K_0p5ns  \
-CharginoAMSB_LL01_mGrav125K_5ns  \
-CharginoAMSB_LL01_mGrav150K_1ns  \
-CharginoAMSB_LL01_mGrav150K_0p5ns  \
-CharginoAMSB_LL01_mGrav150K_5ns  \
-
+AMSBChargino_SystISRDn_mGrav150K_0p5ns     \
+AMSBChargino_SystISRDn_mGrav150K_1ns     \
+AMSBChargino_SystISRDn_mGrav150K_5ns     \
+AMSBChargino_SystISRDn_mGrav75K_0p5ns     \
+AMSBChargino_SystISRDn_mGrav75K_1ns     \
+AMSBChargino_SystISRDn_mGrav75K_5ns     \
+AMSBChargino_SystISRUp_mGrav150K_0p5ns     \
+AMSBChargino_SystISRUp_mGrav150K_1ns     \
+AMSBChargino_SystISRUp_mGrav150K_5ns     \
+AMSBChargino_SystISRUp_mGrav75K_0p5ns     \
+AMSBChargino_SystISRUp_mGrav75K_1ns     \
+AMSBChargino_SystISRUp_mGrav75K_5ns     \
+AMSBChargino_mGrav150K_0p5ns     \
+AMSBChargino_mGrav150K_1ns     \
+AMSBChargino_mGrav150K_5ns     \
+AMSBChargino_mGrav75K_0p5ns     \
+AMSBChargino_mGrav75K_1ns     \
+AMSBChargino_mGrav75K_5ns       
 do
     echo "Doing dir: $dir" 
-#    crab -status -c $dir | tee status$dir.log 
-#    crab -getoutput -c $dir 
-#    crab -status -c $dir | tee status$dir.log 
+    crab -status -c $dir > $dir/status.log 
+    crab -getoutput -c $dir 
+#    crab -status -c $dir  >  $dir/status.log 
 #    crab -getoutput -c $dir   # Do twice, since sometimes the first time doesn't work.  
-#    crab -status -c $dir | tee status$dir.log 
-#   cat status.log | ./resubmitStuckJobs.pl > resubmitOne.sh
-#    cat status$dir.log | ./resubmitJobs.pl > resubmitOne$dir.sh
-#   chmod +x resubmitOne$dir.sh
-#    ./resubmitOne$dir.sh
+    crab -status -c $dir  >  $dir/status.log 
+    cat $dir/status.log # So user can see it 
+    cat $dir/status.log | scripts/resubmitJobs.pl > resubmitOne.sh     # can use resubmitStuckJobs.pl or resubmitStillbornJobs.pl instead
+    cp resubmitOne.sh $dir/ 
+    chmod +x resubmitOne.sh
+    ./resubmitOne.sh  >  $dir/resubmitOne.log 
+    cat $dir/resubmitOne.log   # so user can see it
 
 #######
 # When finished, report and publish:
 ######
-    crab -report -c $dir
-    crab -publishNoInp -USER.dbs_url_for_publication=https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet -c $dir 
+#    crab -report -c $dir
+#    crab -publishNoInp -USER.dbs_url_for_publication=https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet -c $dir 
 
 done  
 
