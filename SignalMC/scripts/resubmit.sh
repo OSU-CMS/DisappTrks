7445678ca08468e310251a8cfc470bfcf914dba1
Wells Wulsin <wulsin.1@osu.edu>
2014-02-28 11:14:57 +0100

    Remove unneeded scripts

--- SignalMC/scripts/resubmit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-crab -kill 1-41,43-75,77-128,130-184,186-200 -c /afs/cern.ch/work/w/wulsin/public/disappTrk/signalMCGenNew1/CMSSW_5_3_3/src/CharginoAMSB_LL01_1ns/
-sleep 60
-crab -forceResubmit 1-41,43-75,77-128,130-184,186-200 -GRID.ce_white_list= -c /afs/cern.ch/work/w/wulsin/public/disappTrk/signalMCGenNew1/CMSSW_5_3_3/src/CharginoAMSB_LL01_1ns/
