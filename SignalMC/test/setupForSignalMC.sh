e747c7ba6dbaa0f8aa9c22a489c08845665f72cb
Wells Wulsin <wulsin.1@osu.edu>
2014-03-27 16:58:06 +0100

    Streamline setup

--- SignalMC/test/setupForSignalMC.sh
+++ SignalMC/test/setupForSignalMC.sh
@@ -11,20 +11,17 @@
 #CMSSW_5_3_11/src> git cms-addpkg SimG4Core/Application
 #CMSSW_5_3_11/src> git clone git@github.com:wulsin/DisappTrks.git
 #CMSSW_5_3_11/src> DisappTrks/SignalMC/test/setupForSignalMC.sh  
-cp DisappTrks/SignalMC/StackingAction.cc  SimG4Core/Application/src/  
-cp DisappTrks/SignalMC/TrackingAction.cc  SimG4Core/Application/src/  
-cp DisappTrks/SignalMC/CustomPDGParser.h SimG4Core/CustomPhysics/interface/  
-cp DisappTrks/SignalMC/CustomPDGParser.cc SimG4Core/CustomPhysics/src/  
+cp DisappTrks/SignalMC/StackingAction.cc        SimG4Core/Application/src/  
+cp DisappTrks/SignalMC/TrackingAction.cc        SimG4Core/Application/src/  
+cp DisappTrks/SignalMC/CustomPDGParser.h        SimG4Core/CustomPhysics/interface/  
+cp DisappTrks/SignalMC/CustomPDGParser.cc       SimG4Core/CustomPhysics/src/  
 cp DisappTrks/SignalMC/CustomParticleFactory.cc SimG4Core/CustomPhysics/src/  
-cp DisappTrks/SignalMC/python/Exotica_HSCP_SIM_cfi.py SimG4Core/CustomPhysics/python/  
-cp DisappTrks/SignalMC/data/*.*  Configuration/Generator/data/  
 echo "Finished copying over relevant files.  Now checking for differences.  Any difference found indicates a problem in copying."  
-diff DisappTrks/SignalMC/StackingAction.cc  SimG4Core/Application/src/StackingAction.cc  
-diff DisappTrks/SignalMC/TrackingAction.cc  SimG4Core/Application/src/TrackingAction.cc  
-diff DisappTrks/SignalMC/CustomPDGParser.h SimG4Core/CustomPhysics/interface/CustomPDGParser.h  
-diff DisappTrks/SignalMC/CustomPDGParser.cc SimG4Core/CustomPhysics/src/CustomPDGParser.cc  
+diff DisappTrks/SignalMC/StackingAction.cc        SimG4Core/Application/src/StackingAction.cc  
+diff DisappTrks/SignalMC/TrackingAction.cc        SimG4Core/Application/src/TrackingAction.cc  
+diff DisappTrks/SignalMC/CustomPDGParser.h        SimG4Core/CustomPhysics/interface/CustomPDGParser.h  
+diff DisappTrks/SignalMC/CustomPDGParser.cc       SimG4Core/CustomPhysics/src/CustomPDGParser.cc  
 diff DisappTrks/SignalMC/CustomParticleFactory.cc SimG4Core/CustomPhysics/src/CustomParticleFactory.cc  
-diff DisappTrks/SignalMC/python/Exotica_HSCP_SIM_cfi.py SimG4Core/CustomPhysics/python/Exotica_HSCP_SIM_cfi.py  
 echo "Finished checking differences.  If none were found, all is OK."  
 echo "Now you are ready to scram b."  
 
