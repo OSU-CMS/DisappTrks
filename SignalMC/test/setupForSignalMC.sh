b3fa6b82f92c96d4e6fa4110b7035385c3a202ed
Wells Wulsin <wulsin.1@osu.edu>
2014-03-27 11:38:37 +0100

    Copy TrackingAction.cc version

--- SignalMC/test/setupForSignalMC.sh
+++ SignalMC/test/setupForSignalMC.sh
@@ -8,10 +8,11 @@
 #CMSSW_5_3_11/src> cmsenv
 #CMSSW_5_3_11/src> git clone git@github.com:wulsin/DisappTrks.git
 #CMSSW_5_3_11/src> DisappTrks/SignalMC/test/setupForSignalMC.sh  
-git cms-addpkg Configuration/Generator
-git cms-addpkg SimG4Core/CustomPhysics 
-git cms-addpkg SimG4Core/Application
+#git cms-addpkg Configuration/Generator
+#git cms-addpkg SimG4Core/CustomPhysics 
+#git cms-addpkg SimG4Core/Application
 cp DisappTrks/SignalMC/StackingAction.cc  SimG4Core/Application/src/  
+cp DisappTrks/SignalMC/TrackingAction.cc  SimG4Core/Application/src/  
 cp DisappTrks/SignalMC/CustomPDGParser.h SimG4Core/CustomPhysics/interface/  
 cp DisappTrks/SignalMC/CustomPDGParser.cc SimG4Core/CustomPhysics/src/  
 cp DisappTrks/SignalMC/CustomParticleFactory.cc SimG4Core/CustomPhysics/src/  
@@ -19,6 +20,7 @@ cp DisappTrks/SignalMC/python/Exotica_HSCP_SIM_cfi.py SimG4Core/CustomPhysics/py
 cp DisappTrks/SignalMC/data/*.*  Configuration/Generator/data/  
 echo "Finished copying over relevant files.  Now checking for differences.  Any difference found indicates a problem in copying."  
 diff DisappTrks/SignalMC/StackingAction.cc  SimG4Core/Application/src/StackingAction.cc  
+diff DisappTrks/SignalMC/TrackingAction.cc  SimG4Core/Application/src/TrackingAction.cc  
 diff DisappTrks/SignalMC/CustomPDGParser.h SimG4Core/CustomPhysics/interface/CustomPDGParser.h  
 diff DisappTrks/SignalMC/CustomPDGParser.cc SimG4Core/CustomPhysics/src/CustomPDGParser.cc  
 diff DisappTrks/SignalMC/CustomParticleFactory.cc SimG4Core/CustomPhysics/src/CustomParticleFactory.cc  
