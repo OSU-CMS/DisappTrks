347fa77a119637a65f968a6ceede2674ed3de5c1
Wells Wulsin <wulsin.1@osu.edu>
2014-04-24 14:34:43 +0200

    Add options; create multicrab file for getting filter efficiency

--- SignalMC/scripts/makeCfgFilesCharginoMassCTau.py
+++ SignalMC/scripts/makeCfgFilesCharginoMassCTau.py
@@ -4,8 +4,8 @@
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/DisappearingTracksSearch2012Recipes#Set_up_signal_MC_generation
 #
 # Usage:
-# DisappTrks/SignalMC/test > makeCfgFilesCharginoMassCTau.py 
-# DisappTrks/SignalMC/test > multicrab -create -cfg multicrabAuto.cfg 2>&1 | tee multicrabCreate.log
+# DisappTrks/SignalMC/test > ../scripts/makeCfgFilesCharginoMassCTau.py -o multicrabNew.cfg -c 
+# DisappTrks/SignalMC/test > multicrab -create -cfg multicrabNew.cfg 2>&1 | tee multicrabCreate.log
 
 from optparse import OptionParser
 
@@ -13,7 +13,10 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-o", "--outputName", dest="outputName", default="multicrab.cfg", 
                   help="name of output multicrab.cfg file")
-
+parser.add_option("-c", "--createConfigs", action="store_true", dest="createConfigs", default=False,
+                  help="create config files; default is to create multicrab files only")
+parser.add_option("-n", "--noFilterOnly", action="store_true", dest="noFilterOnly", default=False,
+                  help="only create the NoFilter jobs; default is to create Filter and NoFilter jobs")
 (arguments, args) = parser.parse_args()
 
 
@@ -31,12 +34,12 @@ fin.close()
 
 
 mass = [
-    ## 100,
+    100,
     200,
-    ## 300,
-    ## 400,
-    ## 500,
-    ## 600
+    300,
+    400,
+    500,
+    600
 ]
 
 ctau = [
@@ -57,38 +60,47 @@ massSystISR = [
     ]
     
 
+multicrabFilterEffName = arguments.outputName.replace(".cfg", "_FilterEff.cfg")  
 fMult = open(arguments.outputName, "w")  
+fMEff = open(multicrabFilterEffName, "w")  
 fMult.write("[MULTICRAB]\n")  
+fMEff.write("[MULTICRAB]\n")  
 fMult.write("cfg=crab_AMSB_chargino.cfg\n\n")  
+fMEff.write("cfg=crab_FilterEff.cfg\n\n")  
 fMult.write("[COMMON]\n\n")  
 fMult.write("\n")  
+fMEff.write("# Get dataset names from DAS search; add them here by hand \n\n")  
     
 cfgDir = "../python/"
 version = "_8TeV_pythia6_V1"
 
-def makeOneConfig(newCfg, cfgTemplate, fMult, m, t):
-    fnew = open(newCfg, "w")
-    configNew = cfgTemplate
-    configNew = configNew.replace("-999", str(m))
-    configNew = configNew.replace("MASSPOINT", str(m))
-    configNew = configNew.replace("LIFETIME", str(t))        
-    fnew.write(configNew)
-    fnew.close()  
+def makeOneConfig(newCfg, cfgTemplate, fMult, fMEff, m, t):
+    if arguments.createConfigs: 
+        fnew = open(newCfg, "w")
+        configNew = cfgTemplate
+        configNew = configNew.replace("-999", str(m))
+        configNew = configNew.replace("MASSPOINT", str(m))
+        configNew = configNew.replace("LIFETIME", str(t))        
+        fnew.write(configNew)
+        fnew.close()  
+        print "Created new config file:  " + newCfg  
     name = newCfg.replace("_GEN-SIM-RECO.py", "") 
     name = name.replace("../python/", "")  
     fMult.write("[" + name + "]\n")  
+    fMEff.write("[" + name + "__FilterEff]\n")  
     fMult.write("CMSSW.pset = " + newCfg + "\n")  
+    fMEff.write("CMSSW.datasetpath =   \n\n")  
     fMult.write("USER.publish_data_name = " + name + version + "\n\n")
-    print "Created new config file:  " + newCfg  
     
 
 for m in mass:
     for t in ctau:
-        newCfg = cfgDir + "AMSB_chargino_" + str(m) + "GeV_ctau" + str(t) + "cm_FilterSumPt50_GEN-SIM-RECO.py"
-        makeOneConfig(newCfg, configTemplateFilter, fMult, m, t)  
+        if not arguments.noFilterOnly: 
+            newCfg = cfgDir + "AMSB_chargino_" + str(m) + "GeV_ctau" + str(t) + "cm_FilterSumPt50_GEN-SIM-RECO.py"
+            makeOneConfig(newCfg, configTemplateFilter, fMult, fMEff, m, t)  
         if m in massNoFilter:
             newCfg = cfgDir + "AMSB_chargino_" + str(m) + "GeV_ctau" + str(t) + "cm_NoFilter_GEN-SIM-RECO.py"
-            makeOneConfig(newCfg, configTemplateNoFilter, fMult, m, t)  
+            makeOneConfig(newCfg, configTemplateNoFilter, fMult, fMEff, m, t)  
         
         if m in massSystISR:
             newCfgSystISRUp = newCfg.replace("Chargino", "Chargino_SystISRUp")
@@ -111,7 +123,8 @@ for m in mass:
             fMult.write("USER.publish_data_name = AMSBChargino_SystISRDn_mGrav" + m + "_" + t + "ns" + version + "\n\n")
 
 fMult.close()  
-print "Finished writing multicrab config file: " + arguments.outputName
+fMEff.close()
+print "Finished writing multicrab config file: " + arguments.outputName + " and filter efficiency multicrab config: " + multicrabFilterEffName  
 
 
 
