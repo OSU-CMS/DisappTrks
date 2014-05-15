3295f7c1626b9c782d41bf946286db48fd5b141f
Jessica Brinson <jbrinson@cern.ch>
2014-05-15 05:11:18 -0400

    add stuff for unblind limits

--- StandardAnalysis/test/amsbLimitConfig.py
+++ StandardAnalysis/test/amsbLimitConfig.py
@@ -124,12 +124,12 @@ signal_channel = 'FullSelection'
 #######################
 
 #this just sets the observed number of events equal to the total background expectation
-run_blind_limits = True
+run_blind_limits = False
 
-data_dataset = "" 
+data_dataset = "MET" 
 
 #condor directory in which to find data root file
-data_condor_dir = '' 
+data_condor_dir = WellsCondorDir + 'condor_2014_04_29_FullSelectionUnBlinded' 
 
 #name of event selection from which to take observed events
 data_channel = 'FullSelection'
