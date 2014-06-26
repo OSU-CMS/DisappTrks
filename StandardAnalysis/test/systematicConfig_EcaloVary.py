c761ef76f6508759ce3f210a9e4172605c868ecc
Jessica Brinson <jbrinson@cern.ch>
2014-06-26 08:00:25 -0400

    updated with new signal samples

--- StandardAnalysis/test/systematicConfig_EcaloVary.py
+++ StandardAnalysis/test/systematicConfig_EcaloVary.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python
 
+#makeRewtdPlot = True
 makeRewtdPlot = False
+
 systematic_name = "EcaloRewt"
-condor_dir = 'condor_2014_03_18_ZtoMuMuFakeTrkNHits4NoEcalo'  
+#condor_dir = 'condor_2014_03_18_ZtoMuMuFakeTrkNHits4NoEcalo'  
+condor_dir = 'ztoMuMuFakeTrkNHits4NoEcalo'  
 channel =  'ZtoMuMuFakeTrkNHits4NoEcalo'   
 
+from localOptionsAll import *
+
 datasets = [
-    'AMSB_mGrav32K_0p5ns',
-    'AMSB_mGrav32K_1ns',
-    'AMSB_mGrav32K_5ns',
-
-    'AMSB_mGrav50K_0p5ns',
- 'AMSB_mGrav50K_1ns',
-    'AMSB_mGrav50K_5ns',
-
-#    'AMSB_mGrav61K_0p2ns',
-    'AMSB_mGrav61K_0p5ns',
-    'AMSB_mGrav61K_1ns',
-    'AMSB_mGrav61K_5ns',
-
-    'AMSB_mGrav75K_0p5ns',
-    'AMSB_mGrav75K_1ns',
-    'AMSB_mGrav75K_5ns',
-
-    'AMSB_mGrav100K_0p5ns',
-    'AMSB_mGrav100K_1ns',
-    'AMSB_mGrav100K_5ns',
-
-    'AMSB_mGrav125K_0p5ns',
-    'AMSB_mGrav125K_1ns',
-    'AMSB_mGrav125K_5ns',
-
-    'AMSB_mGrav150K_0p5ns',
-    'AMSB_mGrav150K_1ns',
-    'AMSB_mGrav150K_5ns',
-
     ]
 
+options = {}
+options['datasets'] = datasets
+options['composite_dataset_definitions'] = composite_dataset_definitions
+options['dataset_names'] = dataset_names
+options['nJobs'] = nJobs
+options['maxEvents'] = maxEvents
+options['types'] = types
+options['labels'] = labels
 
 
+
+def add_charginos (options, masses, ctaus):
+    for mass in masses:
+        for ctau in ctaus:
+            datasetName       = 'AMSB_chargino_' + str (mass) + "GeV_RewtCtau" + str (ctau)  + "cm"
+            sourceDatasetName = 'AMSB_chargino_' + str (mass) + "GeV_ctau" + str(math.floor(source_chargino_ctau(ctau))).rstrip('0').rstrip('.') + "cm"
+            options['datasets'].append (datasetName)
+            options['dataset_names'][datasetName] = options['dataset_names'][sourceDatasetName]
+            options['nJobs']        [datasetName] =  5
+            options['maxEvents']    [datasetName] = -1
+            options['types']        [datasetName] = "signalMC"
+            options['labels']       [datasetName] = str (mass) + " GeV #chi^{#pm} (#LTc#tau#GT = " + str (ctau) + " cm)"
+            print "Adding dataset:  " + datasetName + "; sourceDatasetName=" + sourceDatasetName + "; dataset_name[sourceDatasetName]=" + options['dataset_names'][sourceDatasetName]
+
+add_charginos (options, [100,200,300,400,500,600], [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000])
+
+## mass point used for plot in AN
+#add_charginos (options, [400], [30])
+            
