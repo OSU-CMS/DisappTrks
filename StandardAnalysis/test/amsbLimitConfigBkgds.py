90666389f24ebc3e471a0588d1bde49558e5b3d5
Wells Wulsin <wulsin.1@osu.edu>
2014-06-26 07:12:21 -0400

    Update bkgd estimates

--- StandardAnalysis/test/amsbLimitConfigBkgds.py
+++ StandardAnalysis/test/amsbLimitConfigBkgds.py
@@ -4,15 +4,15 @@
 backgrounds = { 
 'ElecWjets' : {    
     'N' : '0',    
-    'alpha' : '0.367',    
+    'alpha' : '0.4894',    
         },    
 'Muon' : {    
     'N' : '1',    
-    'alpha' : '0.6452',    
+    'alpha' : '0.6524',    
         },    
 'Tau' : {    
     'N' : '0',    
-    'alpha' : '0.3764',    
+    'alpha' : '0.5128',    
         },    
 'Fake' : {    
     'N' : '2',    
