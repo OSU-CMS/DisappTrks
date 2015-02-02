8dc12a0ccaf525010758cdd2aaee55275f2f19e4
Wells Wulsin <wulsin.1@osu.edu>
2015-02-02 10:55:40 -0500

    Update

--- SignalMC/test/addToConfig.py
+++ SignalMC/test/addToConfig.py
@@ -8,12 +8,21 @@ process.g4SimHits.StackingAction.SavePrimaryDecayProductsAndConversionsInTracker
 process.g4SimHits.StackingAction.SavePrimaryDecayProductsAndConversionsInCalo    = cms.untracked.bool(True)
 process.g4SimHits.StackingAction.SavePrimaryDecayProductsAndConversionsInMuon    = cms.untracked.bool(True)
 
-process.g4SimHits.SteppingAction.MaxTrackTimes = cms.vdouble(2000.0, 500.0, 500.0)
-process.g4SimHits.StackingAction.MaxTrackTimes = cms.vdouble(2000.0, 500.0, 500.0)
-process.common_maximum_time.MaxTrackTimes      = cms.vdouble(2000.0, 500.0, 500.0)
+process.g4SimHits.SteppingAction.MaxTrackTimes = cms.vdouble(2000.0)
+process.g4SimHits.StackingAction.MaxTrackTimes = cms.vdouble(2000.0)
+process.common_maximum_time.MaxTrackTimes      = cms.vdouble(2000.0)
 
-## Dump python config if wished
-outfile = open('dumpedConfig.py','w'); print >> outfile,process.dumpPython(); outfile.close()
+process.g4SimHits.Physics.GflashEcal = cms.bool(False)
+process.g4SimHits.Physics.GflashHcal = cms.bool(False)
+process.g4SimHits.Physics.ElectronStepLimit = cms.bool(False)
+process.g4SimHits.Physics.ElectronRangeTest = cms.bool(False)
+process.g4SimHits.Physics.PositronStepLimit = cms.bool(False)
+process.g4SimHits.Physics.MinStepLimit = cms.double(1.0)
+process.g4SimHits.Physics.RusRoElectronEnergyLimit = cms.double(0.0)
+process.g4SimHits.Physics.FlagFluo = cms.bool(False)
+
+process.g4SimHits.StackingAction.KillGamma = cms.bool(True)
+process.g4SimHits.StackingAction.GammaThreshold = cms.double(0.0001)
 
 
 
