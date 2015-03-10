20b14554c067d3b4b8a4bb72c81dace868cbe3ab
Wells Wulsin <wulsin.1@osu.edu>
2015-03-10 06:38:44 -0400

    Do only DIGI,L1,DIGI2RAW

--- SignalMC/test/AMSB_chargino500GeV_ctau100cm_step1b.py
+++ SignalMC/test/AMSB_chargino500GeV_ctau100cm_step1b.py
@@ -2,23 +2,24 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: step1 --filein file:AMSB_chargino500GeV_ctau100cm_step1.root --fileout file:AMSB_chargino500GeV_ctau100cm_step1b.root --pileup_input dbs:/MinBias_TuneA2MB_13TeV-pythia8/Fall13-POSTLS162_V1-v1/GEN-SIM --mc --eventcontent RAWSIM --pileup AVE_20_BX_25ns --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions MCRUN2_71_V1::All --step DIGI,L1,DIGI2RAW,HLT:GRun --magField 38T_PostLS1 --python_filename AMSB_chargino500GeV_ctau100cm_step1b.py --no_exec -n 5
+# with command line options: step1 --filein file:AMSB_chargino500GeV_ctau100cm_step1.root --fileout file:AMSB_chargino500GeV_ctau100cm_step1b.root --mc --eventcontent RAWSIM --pileup AVE_20_BX_25ns --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions MCRUN2_71_V1::All --step DIGI,L1,DIGI2RAW --magField 38T_PostLS1 --python_filename AMSB_chargino500GeV_ctau100cm_step1b.py --no_exec -n 5
 
 # Notes from Wells:
-# Start with the cmsDriver.py commands from the Phys14 campaign, e.g.:  
+# Start with the cmsDriver.py commands from the Phys14 campaign, e.g.:
 # https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_setup/EXO-Phys14DR-00030
 # Note that the GEN-SIM step that was the input was produced with:
 # https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_setup/EXO-Fall13-00237
-# 
+#
 # Then make the following modifications:
 # Remove --step GEN:fixGenInfo
 # Remove --inputEventContent REGEN
-# Use:  --conditions MCRUN2_71_V1::All
-# Add:  process.RAWSIMoutput.outputCommands.extend( [ "keep *_genParticlePlusGeant_*_*", ] ) 
+# Use: --conditions MCRUN2_71_V1::All
+# Add: process.RAWSIMoutput.outputCommands.extend( [ "keep *_genParticlePlusGeant_*_*", ] )
+# Add: memory debugging information at the end.  
 
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process('HLT')
+process = cms.Process('DIGI2RAW')
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -31,7 +32,6 @@ process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.StandardSequences.Digi_cff')
 process.load('Configuration.StandardSequences.SimL1Emulator_cff')
 process.load('Configuration.StandardSequences.DigiToRaw_cff')
-process.load('HLTrigger.Configuration.HLT_GRun_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
@@ -68,9 +68,7 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
         dataTier = cms.untracked.string('GEN-SIM-RAW')
     )
 )
-
-process.RAWSIMoutput.outputCommands.extend( [ "keep *_genParticlePlusGeant_*_*", ] ) 
-
+process.RAWSIMoutput.outputCommands.extend( [ "keep *_genParticlePlusGeant_*_*", ] )
 
 # Additional output definition
 
@@ -79,7 +77,6 @@ process.mix.input.nbPileupEvents.averageNumber = cms.double(20.000000)
 process.mix.bunchspace = cms.int32(25)
 process.mix.minBunch = cms.int32(-12)
 process.mix.maxBunch = cms.int32(3)
-process.mix.input.fileNames = cms.untracked.vstring(['/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/001CB469-A91E-E311-9BFE-0025907FD24A.root', '/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/009CB248-A81C-E311-ACD8-00259073E4F0.root', '/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/009F81D5-B21C-E311-966C-BCAEC50971D0.root', '/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/00B5BB8C-A91E-E311-816A-782BCB1F5E6B.root', '/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/00B8F676-BA1C-E311-BA87-0019B9CABFB6.root', '/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/00DD7446-B51D-E311-B714-001E6739CEB1.root', '/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/021E1B53-101D-E311-886F-00145EDD7569.root', '/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/022A782D-A51C-E311-9856-80000048FE80.root', '/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/026FE678-BA1C-E311-BEF5-00D0680BF90A.root', '/store/mc/Fall13/MinBias_TuneA2MB_13TeV-pythia8/GEN-SIM/POSTLS162_V1-v1/10000/02A10BDE-B21C-E311-AB59-00266CF327C0.root'])
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_71_V1::All', '')
 
@@ -91,17 +88,9 @@ process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.digitisation_step,process.L1simulation_step,process.digi2raw_step)
-process.schedule.extend(process.HLTSchedule)
-process.schedule.extend([process.endjob_step,process.RAWSIMoutput_step])
+process.schedule = cms.Schedule(process.digitisation_step,process.L1simulation_step,process.digi2raw_step,process.endjob_step,process.RAWSIMoutput_step)
 
 # customisation of the process.
-
-# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
-from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC 
-
-#call to customisation function customizeHLTforMC imported from HLTrigger.Configuration.customizeHLTforMC
-process = customizeHLTforMC(process)
 
 # Automatic addition of the customisation function from Configuration.DataProcessing.Utils
 from Configuration.DataProcessing.Utils import addMonitoring 
@@ -116,3 +105,10 @@ from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1
 process = customisePostLS1(process)
 
 # End of customisation functions
+
+
+# Memory information:  
+process.SimpleMemoryCheck.jobReportOutputOnly  = cms.untracked.bool(False)
+process.SimpleMemoryCheck.monitorPssAndPrivate = cms.untracked.bool(True)
+process.SimpleMemoryCheck.moduleMemorySummary  = cms.untracked.bool(True)
+
