1c10ceba4192d7f0a27cd286e6aa382079be6898
Wells Wulsin <wulsin.1@osu.edu>
2015-03-11 08:03:07 -0400

    Clean up and add comments

--- SignalMC/test/checkResources.py
+++ SignalMC/test/checkResources.py
@@ -1,82 +1,92 @@
 #!/usr/bin/python
-import os
 
-#import numpy as np
-#import matplotlib.pyplot as plt
+# This script makes plots of the resource usage of condor jobs.
+# It was designed for CMSSW simulation jobs, but could be adapted for other 
+# types of jobs, as long as the information is included in the stdout 
+# or stderr files.  
+#
+# For CMSSW jobs, to get the memory usage, add the following to the config file:
+process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck")  
+# See for reference:  https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideEDMTimingAndMemory  
+
+
+import os
 from ROOT import gROOT, TGraphErrors, TCanvas, TH1F, TStyle, gStyle, TColor 
 
 gROOT.SetBatch() # Do not open TCanvas
 gStyle.SetOptStat(0)
 
-def getMemoryUsage(step, y, yerr):
-#    print "Checking memory usage..."
-    #os.system('grep "Peak virtual size" condor/AMSB_chargino500GeV_ctau100cm_step2/condor_0.err >! ') 
-    cmd = "grep 'Peak virtual size' condor/AMSB_chargino500GeV_ctau100cm_" + step + "/condor_*.err | awk '{print $5}'"
-    #mem = os.popen(cmd).read().split("\n")[4] 
-    mem = os.popen(cmd).read().split("\n")
-    mem = sorted(mem)  
-#    print "mem = "
-    idxMedian = int(len(mem) / 2)
-    idx16     = int(0.16 * len(mem)) 
-    idx84     = int(0.84 * len(mem)) 
-    memMedian = float(mem[idxMedian])
-    mem16     = float(mem[idx16])
-    mem84     = float(mem[idx84])  
-    memErr = ((mem84 - memMedian) + (memMedian - mem16)) / 2 
-#    print mem
-    print "len(mem) = ", str(len(mem) )  
-    print "cmd = ", cmd  
-    print "indices: median: ", idxMedian, ", idx16=", idx16, "idx84=", idx84  
-#    print "Memory usage for ", step, ":  median=", memMedian, ", 68% interval=[", mem16, " - ", mem84, "]"  
-    print "Memory usage for ", step, ":  ", memMedian, " +- ", memErr
-    print "float(memMedian) = ", float(memMedian) 
-    y.append(memMedian)
-    yerr.append(memErr)  
+def getUsage(cmd, step, y, yerr):
+    fullcmd = cmd[0] + step + cmd[1]   # cmd is list of 2 strings 
+    print "fullcmd = ", fullcmd  
+    usage = os.popen(fullcmd).read().split("\n")
+    usage = sorted(usage)  
+    idxMedian = int(len(usage) / 2)
+    idx16     = int(0.16 * len(usage)) 
+    idx84     = int(0.84 * len(usage)) 
+    usageMedian = float(usage[idxMedian])
+    usage16     = float(usage[idx16])
+    usage84     = float(usage[idx84])  
+    usageErr = ((usage84 - usageMedian) + (usageMedian - usage16)) / 2 
+    if "Time" in fullcmd:
+        print "Time ",
+    else:
+        print "Memory ", 
+    print "usage for ", step, ":  ", usageMedian, " +- ", usageErr
+    y.append(usageMedian)
+    yerr.append(usageErr)  
 
-#getMemoryUsage("step1")
-n = 3
-x    = [1.0, 2.0, 3.0] 
+condorDir = "AMSB_chargino500GeV_ctau100cm_" # excluding 
+
+labels = ["step1b", "step1c", "step2"]
+n = len(labels)  
 y    = [] 
 yerr = [] 
-labels = ["step1b", "step1c", "step2"]
-# getMemoryUsage(labels[0], y, yerr) 
-# getMemoryUsage(labels[1], y, yerr) 
-# getMemoryUsage(labels[2],  y, yerr) 
-#x = range(1,n+1)
-xerr = [1] * n
-
-print "Debug: x = ", x
-print "Debug: y = ", y 
-print "Debug: xerr = ", xerr 
-print "Debug: yerr = ", yerr 
-
-#gr = TGraphErrors()
+cmdMem = ["grep 'Peak virtual size' condor/AMSB_chargino500GeV_ctau100cm_", 
+          "/condor_*.err | awk '{print $5}'"]  
+# Print out will be of the form:
+# MemoryReport> Peak virtual size 1228.38 Mbytes  
 h = TH1F("h", "Memory usage;;Peak virtual memory (MB)", n, 0, n)
 for i in range(n):
-    # gr.SetPoint(i, i, y[i])
-    # gr.SetPointError(i, 0.5, yerr[i])
-    getMemoryUsage(labels[i],  y, yerr) 
+    getUsage(cmdMem, labels[i],  y, yerr) 
     h.SetBinContent(i+1, y   [i])
     h.SetBinError  (i+1, yerr[i])
     h.GetXaxis().SetBinLabel(i+1, labels[i])  
-
 h.SetMinimum(0)
 h.SetMarkerStyle(21)  
-h.SetLineColor(0)  
+h.SetLineColor(1)  
 can = TCanvas()
-
-#gr.Draw()
 h.Draw("pe")
+can.SaveAs("usageMem.pdf")  
 
-can.SaveAs("plot1.pdf")  
 
-# # example data
-# x = np.arange(1, 4, 1)
-# #y = np.exp(-x)
 
-# plt.errorbar(x, y, xerr, yerr)
-# #plt.show()
-# plt.savefig('plot1.png')
+labels = ["step1", "step1b", "step1c", "step2"]
+n = len(labels)  
+y    = [] 
+yerr = [] 
+cmdTime = ["grep -A 5 'Time report complete' condor/AMSB_chargino500GeV_ctau100cm_", 
+           "/condor_*.err | grep 'Avg event' | awk '{print $5}'"]  
+# Print outs will be of the form:
+# TimeReport> Time report complete in 1476.87 seconds
+#  Time Summary: 
+#  - Min event:   6.90868
+#  - Max event:   44.1434
+#  - Avg event:   14.7687
+#  - Total job:   1476.87
+h = TH1F("h", "Processing time;;Average processing time per event (seconds)", n, 0, n)
+for i in range(n):
+    getUsage(cmdTime, labels[i],  y, yerr) 
+    h.SetBinContent(i+1, y   [i])
+    h.SetBinError  (i+1, yerr[i])
+    h.GetXaxis().SetBinLabel(i+1, labels[i])  
+h.SetMinimum(0)
+h.SetMarkerStyle(21)  
+h.SetLineColor(1)  
+can = TCanvas()
+h.Draw("pe")
+can.SaveAs("usageTime.pdf")  
+
 
 print "done"
 
