b7f0abe4b52ae6a619fc08d42fe09e5b71448f4b
Andrew Hart <ahart@cern.ch>
2015-09-02 16:42:32 -0400

    Config fragment for the ISRAnalyzer.

---
+++ SignalMC/test/isrAnalyzer_cfg.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process ("ISR")
+
+process.load ("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet ( input = cms.untracked.int32 (-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+
+process.source = cms.Source ("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring (
+        '/store/user/ahart/AMSB_chargino500GeV_ctau100cm_step0.root',
+    )
+)
+
+process.source.duplicateCheckMode = cms.untracked.string ('noDuplicateCheck')
+
+process.ISRAnalyzer = cms.EDAnalyzer ('ISRAnalyzer',
+    genParticles = cms.InputTag ("genParticles", ""),
+)
+
+
+process.TFileService = cms.Service ("TFileService",
+    fileName = cms.string ('hist.root')
+)
+
+process.p = cms.Path (process.ISRAnalyzer)
