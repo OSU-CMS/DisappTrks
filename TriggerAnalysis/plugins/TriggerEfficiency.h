202e539c7fab1493d2866007c645b1bfcf7b71d2
Andrew Hart <ahart@cern.ch>
2015-09-14 20:01:27 -0400

    Synced with TriggerEfficiencyWithTracks.

--- TriggerAnalysis/plugins/TriggerEfficiency.h
+++ TriggerAnalysis/plugins/TriggerEfficiency.h
@@ -9,9 +9,12 @@
 
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/METReco/interface/CaloMET.h"
 #include "DataFormats/PatCandidates/interface/MET.h"
 #include "DataFormats/PatCandidates/interface/Muon.h"
 #include "DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h"
+#include "DataFormats/TrackReco/interface/Track.h"
 
 #include "FWCore/Common/interface/TriggerNames.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
@@ -21,6 +24,7 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 
 #include "TH1D.h"
+#include "TH2D.h"
 
 using namespace std;
 
@@ -35,20 +39,28 @@ class TriggerEfficiency : public edm::EDAnalyzer
   private:
     void logSpace (const unsigned, const double, const double, vector<double> &) const;
     void linSpace (const unsigned, const double, const double, vector<double> &) const;
-    void fillHistograms (const vector<pat::MET> &, const vector<pat::Muon> &, const string &) const;
-    void fillHistograms (const vector<pat::MET> &, const pat::Muon &, const string &) const;
+    void fillHistograms (const vector<pat::MET> &, const vector<reco::CaloMET> &, const pat::TriggerObjectStandAlone &, const pat::TriggerObjectStandAlone &, const vector<pat::Muon> &, const string &, const string & = "NoTrigger") const;
+    void fillHistograms (const vector<pat::MET> &, const vector<reco::CaloMET> &, const pat::TriggerObjectStandAlone &, const pat::TriggerObjectStandAlone &, const pat::Muon &, const string &, const string & = "NoTrigger") const;
+    const pat::TriggerObjectStandAlone &getHLTMET (const edm::TriggerNames &, const vector<pat::TriggerObjectStandAlone> &, const string &) const;
     bool passesTriggerFilter (const edm::TriggerNames &, const vector<pat::TriggerObjectStandAlone> &, const string &) const;
     bool passesTrigger (const edm::TriggerNames &, const edm::TriggerResults &, const string &) const;
+    bool genMatched (const pat::Muon &, const vector<reco::GenParticle> &, const int, const int, const double) const;
 
     edm::InputTag  mets_;
+    edm::InputTag  caloMets_;
     edm::InputTag  muons_;
     edm::InputTag  electrons_;
     edm::InputTag  triggerBits_;
     edm::InputTag  triggerObjs_;
     edm::InputTag  vertices_;
+    edm::InputTag  genParticles_;
+
+    vector<vector<string> > metTriggersList_;
+    vector<string> metTriggerNames_;
 
     edm::Service<TFileService> fs_;
-    map<string, TH1D *> oneDHists_;
+    map<string, map<string, TH1D *> > oneDHists_;
+    map<string, map<string, TH2D *> > twoDHists_;
 };
 
 #endif
