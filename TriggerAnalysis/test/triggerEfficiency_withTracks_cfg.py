ac9f12e2f48cfe65301264fad97f319397bf6b82
Andrew Hart <ahart@cern.ch>
2015-09-16 01:07:00 -0400

    Added a second command line argument for creating a skim of high-MET events which fail the L1 seed. If the argument is "True", then the skim is created.

--- TriggerAnalysis/test/triggerEfficiency_withTracks_cfg.py
+++ TriggerAnalysis/test/triggerEfficiency_withTracks_cfg.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 import sys
 
 prefix = sys.argv[2]
+doSkim = True if sys.argv[3] == "True" else False
 
 ###########################################################
 ##### Set up process #####
@@ -74,7 +75,7 @@ collections.MiniAOD = cms.PSet (
   trigobjs        =  cms.InputTag  ("selectedPatTrigger",             ""),
 )
 
-process.TriggerEfficiency = cms.EDAnalyzer ("TriggerEfficiencyWithTracks",
+process.TriggerEfficiency = cms.EDFilter ("TriggerEfficiencyWithTracks",
   mets         =  collections.MiniAOD.mets,
   caloMets     =  collections.MiniAOD.caloMets,
   tracks       =  collections.MiniAOD.tracks,
@@ -85,3 +86,25 @@ process.TriggerEfficiency = cms.EDAnalyzer ("TriggerEfficiencyWithTracks",
 )
 
 process.myPath = cms.Path (process.TriggerEfficiency)
+
+if doSkim:
+  print "creating skim..."
+  process.load('Configuration.EventContent.EventContent_cff')
+  process.MINIAODSIMoutput = cms.OutputModule("PoolOutputModule",
+      compressionAlgorithm = cms.untracked.string('LZMA'),
+      compressionLevel = cms.untracked.int32(4),
+      dataset = cms.untracked.PSet(
+          dataTier = cms.untracked.string('MINIAODSIM'),
+          filterName = cms.untracked.string('')
+      ),
+      dropMetaData = cms.untracked.string('ALL'),
+      eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+      fastCloning = cms.untracked.bool(False),
+      fileName = cms.untracked.string('file:' + prefix + '_skim.root'),
+      outputCommands = process.MINIAODSIMEventContent.outputCommands,
+      overrideInputFileSplitLevels = cms.untracked.bool(True),
+      SelectEvents = cms.untracked.PSet (
+        SelectEvents = cms.vstring ("myPath")
+      )
+  )
+  process.myEndPath = cms.EndPath (process.MINIAODSIMoutput)
