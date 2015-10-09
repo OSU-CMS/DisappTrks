19e4678a1f2602ffea3e46c2e7f80283b97a5fe6
Andrew Hart <ahart@cern.ch>
2015-10-09 15:48:45 -0400

    Added an output command to keep the candidateDisappearingTrack collection.

--- SignalMC/python/recoCollectionsToKeep.py
+++ SignalMC/python/recoCollectionsToKeep.py
@@ -4,6 +4,7 @@ def customize (process):
     b = a + "output"
     if hasattr (process, b):
       getattr (process, b).outputCommands.append ("keep *_generalTracks_*_*")
+      getattr (process, b).outputCommands.append ("keep *_candidateDisappearingTracks_*_*")
       getattr (process, b).outputCommands.append ("keep recoCaloMETs_*_*_RECO")
       getattr (process, b).outputCommands.append ("keep recoMETs_*_*_RECO")
       getattr (process, b).outputCommands.append ("keep recoPFMETs_*_*_RECO")
