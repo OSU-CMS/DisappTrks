1cf3dbbec1d01667b2aab8a096f00792e810c02b
Andrew Hart <ahart@cern.ch>
2015-10-14 12:12:59 -0400

    Example of a localConfig file for use with osusub.py.

---
+++ StandardAnalysis/test/localConfig.py
@@ -0,0 +1,11 @@
+from OSUT3Analysis.Configuration.configurationOptions_13TeV import *
+
+config_file = "protoConfig_cfg.py"
+
+intLumi = 500
+
+datasets = [
+  'SingleTop_MiniAOD',
+]
+
+InputCondorArguments = {}
