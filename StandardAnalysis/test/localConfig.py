6cc020c6674494a38ea2f88022914cca64c25b5f
Andrew Hart <ahart@cern.ch>
2015-10-15 14:47:41 -0400

    Added more MC samples and the MET 2015D dataset.

--- StandardAnalysis/test/localConfig.py
+++ StandardAnalysis/test/localConfig.py
@@ -2,10 +2,14 @@ from OSUT3Analysis.Configuration.configurationOptions_13TeV import *
 
 config_file = "protoConfig_cfg.py"
 
-intLumi = 500
+intLumi = 628.76
 
 datasets = [
   'SingleTop_MiniAOD',
+  'TTJets_Lept_MiniAOD',
+  'WJetsToLNu_MiniAOD',
+
+  'MET_2015D_05Oct2015',
 ]
 
-InputCondorArguments = {}
+labels["WJetsToLNu_MiniAOD"] = "W+jets"
