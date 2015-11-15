4f06bccafbfc76703c04f88fa3b9b7aa368f6cd8
Andrew Hart <ahart@cern.ch>
2015-11-15 06:44:22 -0500

    Explicity start with the MiniAOD data format and add our tracks collection. This way, we do not have to update this file every time the MiniAOD data format is modified.

--- StandardAnalysis/interface/CustomDataFormat.h
+++ StandardAnalysis/interface/CustomDataFormat.h
@@ -1,56 +1,15 @@
-// Based on MINI_AOD data format specified in OSUT3Analysis/AnaTools/interface/DataFormat.h 
-// Add definition/include for track collection. 
+// Based on MINI_AOD data format specified in OSUT3Analysis/AnaTools/interface/DataFormatMiniAOD.h
+// Add definition/include for track collection.
 
 #ifndef CUSTOM_DATA_FORMAT
 
-  #define  basicjets_TYPE         pat::Jet
-  #define  beamspots_TYPE         reco::BeamSpot
-  #define  bjets_TYPE             INVALID_TYPE
-  #define  bxlumis_TYPE           INVALID_TYPE
-  #define  candeles_TYPE          INVALID_TYPE
-  #define  candjets_TYPE          INVALID_TYPE
-  #define  electrons_TYPE         pat::Electron
-  #define  events_TYPE            INVALID_TYPE
-  #define  eventvariables_TYPE    EventVariableProducerPayload
-  #define  genjets_TYPE           reco::GenJet
-  #define  jets_TYPE              pat::Jet
-  #define  mcparticles_TYPE       pat::PackedGenParticle
-  #define  mets_TYPE              pat::MET
-  #define  muons_TYPE             pat::Muon
-  #define  photons_TYPE           pat::Photon
-  #define  primaryvertexs_TYPE    reco::Vertex
-  #define  superclusters_TYPE     reco::SuperCluster
-  #define  taus_TYPE              pat::Tau
-  #define  tracks_TYPE            CandidateTrack
-  #define  trigobjs_TYPE          pat::TriggerObjectStandAlone
-  #define  uservariables_TYPE     VariableProducerPayload
+  #include "OSUT3Analysis/AnaTools/interface/DataFormatMiniAOD.h"
 
-  #define  triggers_TYPE          edm::TriggerResults
-  #define  prescales_TYPE         pat::PackedTriggerPrescales  
-  #define  generatorweights_TYPE  GenEventInfoProduct
+  #undef  tracks_TYPE
+  #undef  tracks_INVALID
 
-  #define  bjets_INVALID
-  #define  bxlumis_INVALID
-  #define  candeles_INVALID
-  #define  candjets_INVALID
-  #define  events_INVALID
+  #define tracks_TYPE  CandidateTrack
 
-  #include "DataFormats/BeamSpot/interface/BeamSpot.h"
-  #include "DataFormats/Common/interface/TriggerResults.h"
-  #include "DataFormats/EgammaReco/interface/SuperCluster.h"
-  #include "DataFormats/JetReco/interface/BasicJet.h"
-  #include "DataFormats/JetReco/interface/GenJet.h"
-  #include "DataFormats/PatCandidates/interface/Electron.h"
-  #include "DataFormats/PatCandidates/interface/Jet.h"
-  #include "DataFormats/PatCandidates/interface/MET.h"
-  #include "DataFormats/PatCandidates/interface/Muon.h"
-  #include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
-  #include "DataFormats/PatCandidates/interface/PackedTriggerPrescales.h"
-  #include "DataFormats/PatCandidates/interface/Photon.h"
-  #include "DataFormats/PatCandidates/interface/Tau.h"
-  #include "DisappTrks/CandidateTrackProducer/interface/CandidateTrack.h" 
-  #include "DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h"
-  #include "DataFormats/VertexReco/interface/Vertex.h"
-  #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+  #include "DisappTrks/CandidateTrackProducer/interface/CandidateTrack.h"
 
 #endif
