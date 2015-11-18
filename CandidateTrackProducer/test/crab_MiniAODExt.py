0639da622348892304b0665388bb4fb75bacb4de
Wells Wulsin <wulsin.1@osu.edu>
2015-11-18 16:26:47 -0500

    Update to silver json for all 2015 pp data

--- CandidateTrackProducer/test/crab_MiniAODExt.py
+++ CandidateTrackProducer/test/crab_MiniAODExt.py
@@ -19,9 +19,11 @@ config.Data.unitsPerJob = 1
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = True
 config.Data.publishDataName = 'RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-DisappearingTracks-v1'
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-258159_13TeV_PromptReco_Collisions15_25ns_JSON.txt'  
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_Silver.txt'
+# 2.46 /fb from https://hypernews.cern.ch/HyperNews/CMS/get/physics-validation/2544.html
 
-config.Site.storageSite = 'T3_US_OSU'
+#config.Site.storageSite = 'T3_US_OSU'
+config.Site.storageSite = 'T2_US_Purdue'
 
 if __name__ == '__main__':
 
@@ -44,112 +46,112 @@ if __name__ == '__main__':
     ## Do MC first  
     config.JobType.pyCfgParams = ['runOnMC=1']  
 
-    config.General.requestName = 'candidateTrackProducer_DYToLL'
-    config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_DYToLL'
+    # config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_DYToNuNu'
-    config.Data.inputDataset = '/DYJetsToNuNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_DYToNuNu'
+    # config.Data.inputDataset = '/DYJetsToNuNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_WToLNu'
-    config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_WToLNu'
+    # config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_WWToLNuQQ'
-    config.Data.inputDataset = '/WWTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_WWToLNuQQ'
+    # config.Data.inputDataset = '/WWTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_WWToLNuLNu'
-    config.Data.inputDataset = '/WWTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_WWToLNuLNu'
+    # config.Data.inputDataset = '/WWTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_WZToLNuQQ'
-    config.Data.inputDataset = '/WZTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_WZToLNuQQ'
+    # config.Data.inputDataset = '/WZTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_WZToLLQQ'
-    config.Data.inputDataset = '/WZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_WZToLLQQ'
+    # config.Data.inputDataset = '/WZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_WZToLNuNuNu'
-    config.Data.inputDataset = '/WZTo1L3Nu_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_WZToLNuNuNu'
+    # config.Data.inputDataset = '/WZTo1L3Nu_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_WZToLLLNu'
-    config.Data.inputDataset = '/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_WZToLLLNu'
+    # config.Data.inputDataset = '/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_ZZToNuNuQQ'
-    config.Data.inputDataset = '/ZZTo2Q2Nu_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_ZZToNuNuQQ'
+    # config.Data.inputDataset = '/ZZTo2Q2Nu_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_ZZToLLQQ'
-    config.Data.inputDataset = '/ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_ZZToLLQQ'
+    # config.Data.inputDataset = '/ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_ZZToLLNuNu'
-    config.Data.inputDataset = '/ZZTo2L2Nu_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_ZZToLLNuNu'
+    # config.Data.inputDataset = '/ZZTo2L2Nu_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_ZZToLLLL'
-    config.Data.inputDataset = '/ZZTo4L_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_ZZToLLLL'
+    # config.Data.inputDataset = '/ZZTo4L_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_WG'
-    config.Data.inputDataset = '/WGToLNuG_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_WG'
+    # config.Data.inputDataset = '/WGToLNuG_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_ZG'
-    config.Data.inputDataset = '/ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_ZG'
+    # config.Data.inputDataset = '/ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_TTbarToLFromT'
-    config.Data.inputDataset = '/TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_TTbarToLFromT'
+    # config.Data.inputDataset = '/TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_TTbarToLFromTbar'
-    config.Data.inputDataset = '/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_TTbarToLFromTbar'
+    # config.Data.inputDataset = '/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_TTbarToLL'
-    config.Data.inputDataset = '/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_TTbarToLL'
+    # config.Data.inputDataset = '/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_TW'
-    config.Data.inputDataset = '/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_TW'
+    # config.Data.inputDataset = '/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_TbarW'
-    config.Data.inputDataset = '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_TbarW'
+    # config.Data.inputDataset = '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleT_s-channel'
-    config.Data.inputDataset = '/ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_SingleT_s-channel'
+    # config.Data.inputDataset = '/ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleT_t-channel'
-    config.Data.inputDataset = '/ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_SingleT_t-channel'
+    # config.Data.inputDataset = '/ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
+    # submit(config)
 
 
     ## Now do data 
     ## Warning:  chaning pyCfgParams may trigger an error.  See for documentation:
     ## https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3FAQ#Multiple_submission_fails_with_a  
     config.JobType.pyCfgParams = ['runOnMC=0']  
-    config.Data.publishDataName = 'Run2015D-05Oct2015-v1/'  
+    config.Data.publishDataName = 'Run2015D-05Oct2015-v1'  
 
-    config.General.requestName = 'candidateTrackProducer_MET_2015D_v2'  
+    config.General.requestName = 'candidateTrackProducer_MET_2015D_v4'  
     config.Data.inputDataset = '/MET/Run2015D-05Oct2015-v1/MINIAOD' 
     submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleMuon_2015D_v2'  
-    config.Data.inputDataset = '/SingleMuon/Run2015D-05Oct2015-v1/MINIAOD' 
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_SingleMuon_2015D_v3'  
+    # config.Data.inputDataset = '/SingleMuon/Run2015D-05Oct2015-v1/MINIAOD' 
+    # submit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleElectron_2015D_v2'  
-    config.Data.inputDataset = '/SingleElectron/Run2015D-05Oct2015-v1/MINIAOD'  
-    submit(config)
+    # config.General.requestName = 'candidateTrackProducer_SingleElectron_2015D_v2'  
+    # config.Data.inputDataset = '/SingleElectron/Run2015D-05Oct2015-v1/MINIAOD'  
+    # submit(config)
 
 
 
