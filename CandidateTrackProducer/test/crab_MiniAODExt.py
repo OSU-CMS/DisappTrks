c25d0250293ac48a09bf4a73484d97faccb6c7fc
Wells Wulsin <wulsin.1@osu.edu>
2015-12-11 10:19:00 -0500

    Use forkAndSubmit instead of submit for MC and data tasks

--- CandidateTrackProducer/test/crab_MiniAODExt.py
+++ CandidateTrackProducer/test/crab_MiniAODExt.py
@@ -14,8 +14,7 @@ config.JobType.psetName = 'candidateTrackProducer_cfg.py'
 config.Data.inputDataset = ''
 config.Data.useParent = True
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 1
+config.Data.splitting = 'LumiBased' # for both MC and data
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = True
 config.Data.outputDatasetTag = 'RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-DisappearingTracks-v1'
@@ -28,6 +27,7 @@ if __name__ == '__main__':
     from CRABAPI.RawCommand import crabCommand
     from CRABClient.ClientExceptions import ClientException
     from httplib import HTTPException
+    from multiprocessing import Process
 
     def submit(config):
         try:
@@ -37,6 +37,12 @@ if __name__ == '__main__':
         except ClientException as cle:
             print "Failed submitting task: %s" % (cle)
 
+    def forkAndSubmit(config): 
+        p = Process(target=submit, args=(config,))
+        p.start()
+        p.join()
+
+
     #############################################################################################
     ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
     #############################################################################################
@@ -44,113 +50,116 @@ if __name__ == '__main__':
     ## Do MC first  
     config.JobType.pyCfgParams = ['runOnMC=1']  
 
+    config.Data.unitsPerJob = 10  # MC samples usually have ~150 events per lumi section
+    # This parameter may need to be modified to avoid the 10K jobs limit.  
+
+
     # config.General.requestName = 'candidateTrackProducer_DYToLL'
     # config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_DYToNuNu'
     # config.Data.inputDataset = '/DYJetsToNuNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WToLNu'
     # config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WToLNu_HT100To200'
     # config.Data.inputDataset = '/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM' 
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WToLNu_HT200To400'
     # config.Data.inputDataset = '/WJetsToLNu_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM' 
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WToLNu_HT400To600'
     # config.Data.inputDataset = '/WJetsToLNu_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WToLNu_HT600ToInf'
     # config.Data.inputDataset = '/WJetsToLNu_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'  
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WWToLNuQQ'
     # config.Data.inputDataset = '/WWTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WWToLNuLNu'
     # config.Data.inputDataset = '/WWTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WZToLNuQQ'
     # config.Data.inputDataset = '/WZTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WZToLLQQ'
     # config.Data.inputDataset = '/WZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WZToLNuNuNu'
     # config.Data.inputDataset = '/WZTo1L3Nu_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WZToLLLNu'
     # config.Data.inputDataset = '/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_ZZToNuNuQQ'
     # config.Data.inputDataset = '/ZZTo2Q2Nu_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_ZZToLLQQ'
     # config.Data.inputDataset = '/ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_ZZToLLNuNu'
     # config.Data.inputDataset = '/ZZTo2L2Nu_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_ZZToLLLL'
     # config.Data.inputDataset = '/ZZTo4L_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_WG'
     # config.Data.inputDataset = '/WGToLNuG_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_ZG'
     # config.Data.inputDataset = '/ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_TTbarToLFromT'
     # config.Data.inputDataset = '/TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_TTbarToLFromTbar'
     # config.Data.inputDataset = '/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_TTbarToLL'
     # config.Data.inputDataset = '/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_TW'
     # config.Data.inputDataset = '/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_TbarW'
     # config.Data.inputDataset = '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_SingleT_s-channel'
     # config.Data.inputDataset = '/ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_SingleT_t-channel'
     # config.Data.inputDataset = '/ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1/MINIAODSIM'
-    # submit(config)
+    # forkAndSubmit(config)
 
 ##########################################################################
     ## Now do data 
-    from multiprocessing import Process
     config.Data.useParent = False  # This does not yet work; see https://hypernews.cern.ch/HyperNews/CMS/get/computing-tools/1168/1.html  
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_cfg.py' 
     config.Data.splitting = 'LumiBased' 
@@ -166,41 +175,29 @@ if __name__ == '__main__':
 
     # config.General.requestName = 'candidateTrackProducer_MET_2015D_PromptReco_v3'  
     # config.Data.inputDataset = '/MET/Run2015D-PromptReco-v3/AOD' 
-    # p = Process(target=submit, args=(config,))
-    # p.start()
-    # p.join()
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_SingleMuon_2015D_PromptReco_v3'   
     # config.Data.inputDataset = '/SingleMuon/Run2015D-PromptReco-v3/AOD' 
-    # p = Process(target=submit, args=(config,))
-    # p.start()
-    # p.join()
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_SingleElectron_2015D_PromptReco_v3'  
     # config.Data.inputDataset = '/SingleElectron/Run2015D-PromptReco-v3/AOD'  
-    # p = Process(target=submit, args=(config,))
-    # p.start()
-    # p.join()
+    # forkAndSubmit(config)
 
     config.Data.unitsPerJob = 3
     config.Data.outputDatasetTag = 'Run2015D-PromptReco-v4' 
     # config.General.requestName = 'candidateTrackProducer_MET_2015D_PromptReco_v4'  
     # config.Data.inputDataset = '/MET/Run2015D-PromptReco-v4/AOD' 
-    # p = Process(target=submit, args=(config,))
-    # p.start()
-    # p.join()
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_SingleMuon_2015D_PromptReco_v4'   
     # config.Data.inputDataset = '/SingleMuon/Run2015D-PromptReco-v4/AOD' 
-    # p = Process(target=submit, args=(config,))
-    # p.start()
-    # p.join()
+    # forkAndSubmit(config)
 
     # config.General.requestName = 'candidateTrackProducer_SingleElectron_2015D_PromptReco_v4'  
     # config.Data.inputDataset = '/SingleElectron/Run2015D-PromptReco-v4/AOD'  
-    # p = Process(target=submit, args=(config,))
-    # p.start()
-    # p.join()
+    # forkAndSubmit(config)
 
 
 
