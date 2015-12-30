5e6bcd0cee6ef4f1798e30e45c583c7f5c280756
Wells Wulsin <wulsin.1@osu.edu>
2015-12-30 09:22:15 -0500

    Change the InputTag for the track collection, to correspond to the latest version of the data extended MiniAOD samples

--- StandardAnalysis/test/protoConfig_cfg.py
+++ StandardAnalysis/test/protoConfig_cfg.py
@@ -18,6 +18,8 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.source = cms.Source ("PoolSource",
                              fileNames = cms.untracked.vstring (
                                  "/store/user/ahart/AMSB_chargino500GeV_ctau100cm_step4_User.root",  # signal
+                                 # "file:condor/metMinimalSkim/AMSB_chargino_500GeV_100cm/metMinimalSkim/skim_0.root", 
+                                 # "file:condor/candTrkSelection/WJetsToLNu/CandTrkSelection/merged/skim_merged.root",  
                                  # "/store/user/ahart/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-DisappearingTracks-v1/151009_193826/0001/miniAODWithCandidateTracks_1001.root", # bkgd MC
                                  # "/store/user/ahart/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-DisappearingTracks-v1/151009_193736/0000/miniAODWithCandidateTracks_1.root", # bkgd MC
                                  # '/store/user/wulsin/MET/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-DisappearingTracks-v1/151011_155240/0000/miniAODWithCandidateTracks_10.root', # data
@@ -62,7 +64,8 @@ process.maxEvents = cms.untracked.PSet (
 
 from OSUT3Analysis.AnaTools.osuAnalysis_cfi import collectionMap  # miniAOD
 
-collectionMap.tracks = cms.InputTag ('candidateDisappearingTracks')
+# collectionMap.tracks = cms.InputTag ('candidateDisappearingTracks')  # For signal (old version)  
+collectionMap.tracks = cms.InputTag ('candidateTrackProducer')       # For data skim and new samples  
 
 ################################################################################
 ##### Set up weights to be used in plotting and cutflows  ######################
@@ -104,36 +107,65 @@ histSets = cms.VPSet (
     JetHistograms
 )
 
+histSetsMetJet = cms.VPSet (
+    MetHistograms,
+    JetHistograms
+)
+
 histSetsMuon = copy.deepcopy(histSets)
 histSetsMuon.append(MuonHistograms)
 histSetsMuon.append(DiMuonHistograms)
+
+test = cms.PSet(
+    name = cms.string("test"),
+    triggers = cms.vstring(), 
+    cuts = cms.VPSet (
+        cms.PSet (
+            inputCollection = cms.vstring("primaryvertexs"),
+            cutString = cms.string("isValid > 0 && ndof >= 4"),
+            numberRequired = cms.string(">= 1")
+        )
+    )
+)
 
 
 ################################################################################
 ##### Attach the channels and histograms to the process ########################
 ################################################################################
 
-#add_channels (process, [metMinimalSkim],    histSets, weights, collectionMap, variableProducers, True)
-#add_channels (process, [disTrkSelection],    histSets, weights, collectionMap, variableProducers, True)
-#add_channels (process, [disTrkSelectionMatchGenMuon], histSets, weights, collectionMap, variableProducers, True)
+# add_channels (process, [test],    cms.VPSet(), weights, collectionMap, variableProducers, True)
 
-#add_channels (process, [candTrkSelection],    histSets, weights, collectionMap, variableProducers, True)
+# add_channels (process, [metMinimalSkim],    histSetsMetJet, weights, collectionMap, variableProducers, True)
+# add_channels (process, [nonIsoTrkSelection], histSets, weights, collectionMap, variableProducers, False)
+# add_channels (process, [isoTrkSelection],    histSets, weights, collectionMap, variableProducers, False)
+# add_channels (process, [candTrkSelection],    histSets, weights, collectionMap, variableProducers, True)
+# add_channels (process, [disTrkSelection],    histSets, weights, collectionMap, variableProducers, True)
+# add_channels (process, [elecCtrlSelection],  histSets, weights, collectionMap, variableProducers, True)
+# add_channels (process, [muonCtrlSelection],  histSets, weights, collectionMap, variableProducers, True)
+# add_channels (process, [tauCtrlSelection],   histSets, weights, collectionMap, variableProducers, True)
 # add_channels (process, [candTrkEcaloSdband],    histSets, weights, collectionMap, variableProducers, True)
 # add_channels (process, [candTrkNMissOutSdband], histSets, weights, collectionMap, variableProducers, True)
-#add_channels (process, [isoTrkSelection],    histSets, weights, collectionMap, variableProducers, False)
-#add_channels (process, [nonIsoTrkSelection], histSets, weights, collectionMap, variableProducers, False)
-#add_channels (process, [elecCtrlSelection],  histSets, weights, collectionMap, variableProducers, True)
-#add_channels (process, [muonCtrlSelection],  histSets, weights, collectionMap, variableProducers, True)
-#add_channels (process, [tauCtrlSelection],   histSets, weights, collectionMap, variableProducers, True)
-#add_channels (process, [ZtoMuMu],          histSetsMuon, weights, collectionMap, variableProducers, True)
-#add_channels (process, [ZtoMuProbeTrk],  histSetsMuon, weights, collectionMap, variableProducers, False)
-#add_channels (process, [ZtoMuMu],          histSetsMuon, weights, collectionMap, variableProducers, True)
-#add_channels (process, [ZtoMuMuCandTrk],          histSetsMuon, weights, collectionMap, variableProducers, True)
-#add_channels (process, [ZtoMuMuDisTrk],          histSetsMuon, weights, collectionMap, variableProducers, True)
+# add_channels (process, [disTrkSelectionNHits3], histSets, weights, collectionMap, variableProducers, True)
+# add_channels (process, [disTrkSelectionNHits4], histSets, weights, collectionMap, variableProducers, True)
+# add_channels (process, [disTrkSelectionNHits5], histSets, weights, collectionMap, variableProducers, True)
+# add_channels (process, [disTrkSelectionNHits6], histSets, weights, collectionMap, variableProducers, True)
+
+
+# add_channels (process, [ZtoMuMu],          histSetsMuon, weights, collectionMap, variableProducers, True)
+# add_channels (process, [ZtoMuProbeTrk],  histSetsMuon, weights, collectionMap, variableProducers, False)
+# add_channels (process, [ZtoMuMu],          histSetsMuon, weights, collectionMap, variableProducers, True)
+# add_channels (process, [ZtoMuMuCandTrk],          histSetsMuon, weights, collectionMap, variableProducers, True)
+# add_channels (process, [ZtoMuMuDisTrk],          histSetsMuon, weights, collectionMap, variableProducers, True)
 # add_channels (process, [ZtoMuMuCandTrkEcaloSdband],     histSetsMuon, weights, collectionMap, variableProducers, True)
 # add_channels (process, [ZtoMuMuCandTrkNMissOutSdband],  histSetsMuon, weights, collectionMap, variableProducers, True)
+# add_channels (process, [ZtoMuMuDisTrkNHits3],        histSetsMuon, weights, collectionMap, variableProducers, True)
+# add_channels (process, [ZtoMuMuDisTrkNHits4],        histSetsMuon, weights, collectionMap, variableProducers, True)
+# add_channels (process, [ZtoMuMuDisTrkNHits5],        histSetsMuon, weights, collectionMap, variableProducers, True)
+# add_channels (process, [ZtoMuMuDisTrkNHits6],        histSetsMuon, weights, collectionMap, variableProducers, True)
 
 
 # uncomment to produce a full python configuration log file
 #outfile = open('dumpedConfig.py','w'); print >> outfile,process.dumpPython(); outfile.close()
+
+#process.Tracer = cms.Service("Tracer")
 
