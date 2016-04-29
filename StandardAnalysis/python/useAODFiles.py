70aed43bd34a79c0ac3fdd46ec3a7954ff174778
Andrew Hart <ahart@cern.ch>
2016-04-29 14:42:53 -0400

    Added the ability to add the parents of skim files, using the condor output files to trace them down.

--- StandardAnalysis/python/useAODFiles.py
+++ StandardAnalysis/python/useAODFiles.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 import OSUT3Analysis.DBTools.osusub_cfg as osusub
 import das_client
 import re
+import os
 
 def getDASData (query):
     results = []
@@ -22,7 +23,7 @@ def getDASData (query):
 
     return results
 
-def addSecondaryFilesAODSIM (fileName):
+def addSecondaryFileAODSIM (fileName):
     parents = []
     fileName = re.sub (r'^.*/store/', r'/store/', fileName)
     parent = getDASData ('parent file=' + fileName + ' instance=prod/phys03 | grep parent.name')
@@ -32,15 +33,53 @@ def addSecondaryFilesAODSIM (fileName):
 
     return parents
 
-def addSecondaryFilesAOD (fileName):
+def addSecondaryFileAOD (fileName):
     parents = []
     fileName = re.sub (r'^.*/store/', r'/store/', fileName)
     parent = getDASData ('parent file=' + fileName + ' instance=prod/phys03 | grep parent.name')
     for p in parent:
-        sibling = getDASData ('child file=' + p + ' | grep child.name')
-        for s in sibling:
-              if re.search (r'/AOD/16Dec2015', s) and not s in parents:
-                  parents.append (s)
+#        sibling = getDASData ('child file=' + p + ' | grep child.name')
+#        for s in sibling:
+#              if re.search (r'/AOD/16Dec2015', s) and not s in parents:
+#                  parents.append (s)
+        if re.search (r'/RAW/', p) and not p in parents:
+            parents.append (p)
+
+    return parents
+
+def addSecondaryFileFromSkim (fileName):
+    parents = []
+    fileName = re.sub (r'^file:', r'', fileName)
+    jobNumber = re.sub (r'.*/skim_([^/]*)\.root$', r'\1', fileName)
+    parentDir = re.sub (r'(.*)/[^/]*/skim_[^/]*\.root$', r'\1/', fileName)
+    condorErr = open (parentDir + "condor_" + jobNumber + ".err")
+    print "getting secondary files for skim..."
+    for line in condorErr:
+        if re.search (r'Successfully opened file', line):
+            p = re.sub (r'.* Successfully opened file (.*)', r'\1', line)
+            parents.append (p)
+    print "parents for " + fileName + ": "
+    for p in sorted (list (set (parents))):
+        print "  " + p
+
+    return sorted (list (set (parents)))
+
+def addSecondaryFile (fileName):
+    parents = []
+    if re.search (r'/store/', fileName):
+        if re.search (r'Run201', fileName):
+            print "calling addSecondaryFileAOD (" + fileName + ")..."
+            parents = addSecondaryFileAOD (fileName)
+        else:
+            print "calling addSecondaryFileAODSIM (" + fileName + ")..."
+            parents = addSecondaryFileAODSIM (fileName)
+    else:
+        print "calling addSecondaryFileFromSkim (" + fileName + ")..."
+        skimParents = addSecondaryFileFromSkim (fileName)
+        for p in skimParents:
+            p.rstrip ('\n')
+            print "calling addSecondaryFile (" + p + ")..."
+            parents = addSecondaryFile (p)
 
     return parents
 
@@ -50,9 +89,8 @@ def addSecondaryFiles (source):
     if osusub.batchMode:
         fileNames = osusub.runList
     for fileName in fileNames:
-        if re.search (r'Run201', fileName):
-            parents += addSecondaryFilesAOD (fileName)
-        else:
-            parents += addSecondaryFilesAODSIM (fileName)
+        fileName.rstrip ('\n')
+        print "calling addSecondaryFile (" + fileName + ")..."
+        parents += addSecondaryFile (fileName)
 
     source.secondaryFileNames = cms.untracked.vstring (parents)
