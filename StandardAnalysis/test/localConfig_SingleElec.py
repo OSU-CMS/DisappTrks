b871cd6a93a60ab6b95e1f92b1b12b4c0b10eeb9
Wells Wulsin <wulsin.1@osu.edu>
2016-05-23 15:37:39 -0400

    Update lumi for SingleMu_2015D dataset; add local config file for SingleEle_2015D dataset.

---
+++ StandardAnalysis/test/localConfig_SingleElec.py
@@ -0,0 +1,12 @@
+from localConfig_SingleMu import * 
+
+intLumi = 2670.0 # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/670
+
+datasetsData = [
+    'SingleEle_2015D', 
+]
+
+datasets = datasetsBkgd + datasetsData 
+
+
+
--- StandardAnalysis/test/localConfig_SingleMu.py
+++ StandardAnalysis/test/localConfig_SingleMu.py
@@ -1,6 +1,6 @@
 from localConfig import * 
 
-intLumi = 2588.0 # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/595
+intLumi = 2670.0 # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/670
 
 datasetsData = [
     'SingleMu_2015D',  
@@ -10,5 +10,9 @@ datasetsBkgd.remove("WJetsToLNu_HT")
 datasetsBkgd.append("WJetsToLNu")  
 
 datasets = datasetsBkgd + datasetsData 
+
+# datasets = [
+#     'SingleMu_2015D',  
+# ]
 
 
