be6a220cde97d31c6ed5a6d2503e345caf983399
Andrew Hart <ahart@cern.ch>
2016-06-16 11:44:33 -0400

    Added separate objects for WJetsToLNu and TTJets.

--- StandardAnalysis/test/closureTestMu.py
+++ StandardAnalysis/test/closureTestMu.py
@@ -6,42 +6,68 @@ from DisappTrks.StandardAnalysis.getUser import *
 from ROOT import TCanvas, TFile
 import os 
 
-
-dirs = getUser() 
+dirs = getUser()
 
 print "********************************************************************************"
+print "performing muon background closure test with WJetsToLNu..."
+print "--------------------------------------------------------------------------------"
 
-fout = TFile.Open ("muonBkgdClosureTest.root", "recreate")
+fout = TFile.Open ("muonBkgdClosureTest_WJetsToLNu.root", "recreate")
 canvas = TCanvas("c1", "c1",800,800)
 setCanvasStyle(canvas)
 
-muonBkgdClosureTest = LeptonBkgdClosureTest ("muon")
-muonBkgdClosureTest.addTFile (fout)
-muonBkgdClosureTest.addTCanvas (canvas)
-muonBkgdClosureTest.addMetCut (100.0)
-muonBkgdClosureTest.addChannel  ("TagPt35",             "MuonTagPt35",           "WJetsToLNu",    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
-#muonBkgdClosureTest.addChannel  ("TagPt35NoTrig",       "MuonTagPt35NoTrig",     "WJetsToLNu",    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
-muonBkgdClosureTest.addChannel  ("TagPt35MetTrig",      "MuonTagPt35MetTrig",    "WJetsToLNu",    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
-#muonBkgdClosureTest.addChannel  ("TagPt35MetCut",       "MuonTagPt35MetCut",     "WJetsToLNu",    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
-muonBkgdClosureTest.addChannel  ("CandTrkIdPt35",       "CandTrkIdMuPt35",       "WJetsToLNu_HT", dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest_LHS")
-muonBkgdClosureTest.addChannel  ("CandTrkIdPt35NoMet",  "CandTrkIdMuPt35NoMet",  "WJetsToLNu",    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest_LHS")
-
-# sample = "TTJets"
-# muonBkgdClosureTest.addChannel  ("TagPt35",             "MuonTagPt35",           sample,    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
-# #muonBkgdClosureTest.addChannel  ("TagPt35NoTrig",       "MuonTagPt35NoTrig",     sample,    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
-# muonBkgdClosureTest.addChannel  ("TagPt35MetTrig",      "MuonTagPt35MetTrig",    sample,    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
-# #muonBkgdClosureTest.addChannel  ("TagPt35MetCut",       "MuonTagPt35MetCut",     sample,    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
-# muonBkgdClosureTest.addChannel  ("CandTrkIdPt35",       "CandTrkIdMuPt35",       sample,    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest_LHS")
-# muonBkgdClosureTest.addChannel  ("CandTrkIdPt35NoMet",  "CandTrkIdMuPt35NoMet",  sample,    dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest_LHS_LHS")
-# muonBkgdClosureTest.printSingleLeptonTriggerEff ()
+muonBkgdClosureTest_WJetsToLNu = LeptonBkgdClosureTest ("muon")
+muonBkgdClosureTest_WJetsToLNu.addTFile (fout)
+muonBkgdClosureTest_WJetsToLNu.addTCanvas (canvas)
+muonBkgdClosureTest_WJetsToLNu.addMetCut (100.0)
+muonBkgdClosureTest_WJetsToLNu.addChannel  ("TagPt35",             "MuonTagPt35",           "WJetsToLNu",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
+#muonBkgdClosureTest_WJetsToLNu.addChannel  ("TagPt35NoTrig",       "MuonTagPt35NoTrig",     "WJetsToLNu",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
+muonBkgdClosureTest_WJetsToLNu.addChannel  ("TagPt35MetTrig",      "MuonTagPt35MetTrig",    "WJetsToLNu",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
+#muonBkgdClosureTest_WJetsToLNu.addChannel  ("TagPt35MetCut",       "MuonTagPt35MetCut",     "WJetsToLNu",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
+muonBkgdClosureTest_WJetsToLNu.addChannel  ("CandTrkIdPt35",       "CandTrkIdMuPt35",       "WJetsToLNu_HT",  dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest_LHS")
+muonBkgdClosureTest_WJetsToLNu.addChannel  ("CandTrkIdPt35NoMet",  "CandTrkIdMuPt35NoMet",  "WJetsToLNu",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest_LHS")
+muonBkgdClosureTest_WJetsToLNu.printSingleLeptonTriggerEff ()
 
 print "********************************************************************************"
 
-(nEst, nEstError) = muonBkgdClosureTest.printNest ()
+(nEst, nEstError) = muonBkgdClosureTest_WJetsToLNu.printNest ()
 
 print "--------------------------------------------------------------------------------"
 
-(nBack, nBackError) = muonBkgdClosureTest.printNback ()
+(nBack, nBackError) = muonBkgdClosureTest_WJetsToLNu.printNback ()
+print "|N_est - N_back| = " + str (abs (nEst - nBack) / math.hypot (nEstError, nBackError)) + " sigma"
+
+print "********************************************************************************"
+
+fout.Close ()
+
+print "\n\n"
+
+print "********************************************************************************"
+print "performing muon background closure test with TTJets..."
+print "--------------------------------------------------------------------------------"
+
+fout = TFile.Open ("muonBkgdClosureTest_TTJets.root", "recreate")
+
+muonBkgdClosureTest_TTJets = LeptonBkgdClosureTest ("muon")
+muonBkgdClosureTest_TTJets.addTFile (fout)
+muonBkgdClosureTest_TTJets.addTCanvas (canvas)
+muonBkgdClosureTest_TTJets.addMetCut (100.0)
+muonBkgdClosureTest_TTJets.addChannel  ("TagPt35",             "MuonTagPt35",           "TTJets",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
+#muonBkgdClosureTest_TTJets.addChannel  ("TagPt35NoTrig",       "MuonTagPt35NoTrig",     "TTJets",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
+muonBkgdClosureTest_TTJets.addChannel  ("TagPt35MetTrig",      "MuonTagPt35MetTrig",    "TTJets",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
+#muonBkgdClosureTest_TTJets.addChannel  ("TagPt35MetCut",       "MuonTagPt35MetCut",     "TTJets",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest")
+muonBkgdClosureTest_TTJets.addChannel  ("CandTrkIdPt35",       "CandTrkIdMuPt35",       "TTJets",  dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest_LHS")
+muonBkgdClosureTest_TTJets.addChannel  ("CandTrkIdPt35NoMet",  "CandTrkIdMuPt35NoMet",  "TTJets",     dirs['Andrew']+"withFiducialCuts/muonBkgdClosureTest_LHS_LHS")
+muonBkgdClosureTest_TTJets.printSingleLeptonTriggerEff ()
+
+print "********************************************************************************"
+
+(nEst, nEstError) = muonBkgdClosureTest_TTJets.printNest ()
+
+print "--------------------------------------------------------------------------------"
+
+(nBack, nBackError) = muonBkgdClosureTest_TTJets.printNback ()
 print "|N_est - N_back| = " + str (abs (nEst - nBack) / math.hypot (nEstError, nBackError)) + " sigma"
 
 print "********************************************************************************"
