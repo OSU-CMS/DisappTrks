c522791bc22e3050f6982ec9964db95ee65dc406
Andrew Hart <ahart@cern.ch>
2016-06-29 14:44:52 -0400

    Updated background estimates from fully data-driven method.

--- StandardAnalysis/test/amsbLimitConfigBkgds.py
+++ StandardAnalysis/test/amsbLimitConfigBkgds.py
@@ -1,34 +1,37 @@
-# Bkgd configuration file for limit-setting produced with makeANTables.py 
-#!/usr/bin/env python  
+#!/usr/bin/env python
+
+# Bkgd configuration file for limit-setting produced with makeANTables.py
+
 backgrounds = {
-'Elec' : {
-    'N' : '2',
-    'alpha' : '0.1279087414',
+'Elec' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/707
+    'N' : '457275',
+    'alpha' : '1.524554979146028e-5',
         },
-'Muon' : {
-    'N' : '0',
-    'alpha' : '0.134569932947',
+'Muon' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/705
+    'N' : '379',
+    'alpha' : '1.465250659630607e-5',
         },
-'Tau' : {
+'Tau' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/710
     'N' : '0',
-    'alpha' : '0.196531791908',
+    'alpha' : '0.0124190476190749',
         },
 'Fake' : {
     'N' : '0',
     'alpha' : '0.569361757681',
         },
-    }
-background_systematics = { 
+}
+
+background_systematics = {
     'Elec' : {
-    'value' : '2.5',     # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/637 
+    'value' : '1.2046374740621713',
                  },
     'Muon' : {
-    'value' : '2.5',     # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/635 
+    'value' : '1.4641395609543109',
                  },
     'Tau' : {
-    'value' : '1.71',    # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/621 
+    'value' : '1.5115259540529666',
                  },
     'Fake' : {
-    'value' : '1.22',    # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/659 
+    'value' : '1.22',    # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/659
                  },
-    }
+}
