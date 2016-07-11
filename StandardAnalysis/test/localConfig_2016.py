113d42bef93d794b9eb87cbbe47d3aa488855d90
Andrew Hart <ahart@cern.ch>
2016-07-11 09:15:26 -0400

    Updated the luminosity for the MET dataset.

--- StandardAnalysis/test/localConfig_2016.py
+++ StandardAnalysis/test/localConfig_2016.py
@@ -3,7 +3,7 @@ from DisappTrks.StandardAnalysis.miniAOD_80X_Samples import *
 
 config_file = "protoConfig_cfg.py"
 
-intLumi = 4011.084
+intLumi = 4329.2 # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/726
 # Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON.txt
 
 InputCondorArguments = {'request_memory': '2048MB', 'request_cpus': '1'}
