606642ad1a28106d2c408072dd1cf70e3aecc42a
Andrew Hart <ahart@cern.ch>
2016-07-19 14:21:11 -0400

    Updated total luminosity.

--- StandardAnalysis/test/amsbLimitConfig.py
+++ StandardAnalysis/test/amsbLimitConfig.py
@@ -50,7 +50,7 @@ lifetimes = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
              '20', '30', '40', '50', '60', '70', '80', '90', '100',
              '200', '300', '400', '500', '600', '700', '800', '900', '1000']
 
-lumi = 2590.0
+lumi = 6919.2
 
 #condor directory in which to find signal root files
 signal_condor_dir = AndrewDir + 'withFiducialCuts/disTrkSelection_signal/'
