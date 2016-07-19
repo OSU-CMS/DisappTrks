c78f53c2153dca9dc88e1635cd819df739ff2404
Andrew Hart <ahart@cern.ch>
2016-07-19 16:20:17 -0400

    Added electron and muon backgrounds for first 4/fb of 2016.

--- StandardAnalysis/test/amsbLimitConfigBkgds.py
+++ StandardAnalysis/test/amsbLimitConfigBkgds.py
@@ -3,11 +3,11 @@
 # Bkgd configuration file for limit-setting produced with makeANTables.py
 
 backgrounds = {
-'Elec' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/715
+'Elec2015' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/715
     'N' : '44129',
     'alpha' : '6.276016681071405e-5',
         },
-'Muon' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/705
+'Muon2015' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/705
     'N' : '379',
     'alpha' : '1.465250659630607e-5',
         },
@@ -15,23 +15,39 @@ backgrounds = {
     'N' : '0',
     'alpha' : '0.0124190476190749',
         },
-'Fake' : {
+'Fake' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/659
     'N' : '0',
-    'alpha' : '0.569361757681',
+    'alpha' : '0.571428571428571',
+        },
+
+'Elec2016' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/732
+    'N' : '32361',
+    'alpha' : '8.07167905141992e-05',
+        },
+'Muon2016' : { # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/732
+    'N' : '1615',
+    'alpha' : '2.40472787764706e-05',
         },
 }
 
 background_systematics = {
-    'Elec' : {
+    'Elec2015' : { # error on alpha
     'value' : '1.2057287367251016',
                  },
-    'Muon' : {
+    'Muon2015' : { # error on alpha
     'value' : '1.4641395609543109',
                  },
-    'Tau' : {
+    'Tau' : { # error on alpha
     'value' : '1.5115259540529666',
                  },
-    'Fake' : {
-    'value' : '1.22',    # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/659
+    'Fake' : { # error on alpha + systematic error
+    'value' : '1.220005252038150',
+                 },
+
+    'Elec2016' : { # error on alpha
+    'value' : '1.1695386695260912',
+                 },
+    'Muon2016' : { # error on alpha
+    'value' : '1.2508672533825393',
                  },
 }
