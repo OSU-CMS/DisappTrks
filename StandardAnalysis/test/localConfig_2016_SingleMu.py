c7970c2f3797fa0972ef2d0c4ff0c9360237776c
Brian Francis <bfrancis@gmail.com>
2016-08-02 09:54:36 -0400

    Update 2016 single lepton datasets to ICHEP16

--- StandardAnalysis/test/localConfig_2016_SingleMu.py
+++ StandardAnalysis/test/localConfig_2016_SingleMu.py
@@ -1,12 +1,14 @@
 from localConfig_2016 import *
 
-# 5856.536 (B) + 370.901 (C)
-# https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/734
-intLumi = 6227.437
+# ICHEP 2016 dataset
+# 5877.897 (B) + 2645.968 (C) + 4353.449 (D)
+# https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/738
+intLumi = 12877.314
 
 datasetsData = [
     'SingleMu_2016B',
     'SingleMu_2016C',
+    'SingleMu_2016D',
 ]
 
 datasets = datasetsData
