8c0a9f70fec303aa5d39e9159c995464c2ff3023
Andrew Hart <ahart@cern.ch>
2016-08-04 11:46:08 -0400

    Updated luminosities.

--- StandardAnalysis/test/localConfig_2016.py
+++ StandardAnalysis/test/localConfig_2016.py
@@ -3,14 +3,15 @@ from DisappTrks.StandardAnalysis.miniAOD_80X_Samples import *
 
 config_file = "protoConfig_2016_cfg.py"
 
-# https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/735
-intLumi = 6317.0
+# https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/741
+intLumi = 12884.361
 
 InputCondorArguments = {'request_memory': '2048MB', 'request_cpus': '1'}
 
 datasetsData = [
     'MET_2016B',
     'MET_2016C',
+    'MET_2016D',
 ]
 
 datasets = datasetsData
--- StandardAnalysis/test/localConfig_2016_Tau.py
+++ StandardAnalysis/test/localConfig_2016_Tau.py
@@ -1,12 +1,13 @@
 from localConfig_2016 import *
 
-# https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/735
-#intLumi = 6308.9 # unprescaled
-intLumi = 742.02 # luminosity for HLT_LooseIsoPFTau50_Trk30_eta2p1_v* path
+# https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/741
+#intLumi = 12879.779 # unprescaled
+intLumi = 952.6743 # luminosity for HLT_LooseIsoPFTau50_Trk30_eta2p1_v* path
 
 datasetsData = [
     'Tau_2016B',
     'Tau_2016C',
+    'Tau_2016D',
 ]
 
 datasets = datasetsData
