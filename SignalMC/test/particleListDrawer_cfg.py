d26e331d31fa07bf73f39023b4901f4b0c1e2d84
Andrew Hart <ahart@cern.ch>
2016-09-07 16:13:02 -0400

    Put an actual signal file in the source.

--- SignalMC/test/particleListDrawer_cfg.py
+++ SignalMC/test/particleListDrawer_cfg.py
@@ -9,18 +9,12 @@ process.load ('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.source = cms.Source ('PoolSource',
     fileNames = cms.untracked.vstring (
-#        'file:charginoPartGun_GEN_SIM_5nsWithDecayFlagsOn.root', 
-#        'file:AMSB_chargino_test_GEN.root', 
-#    'root://xrootd.ba.infn.it//store/user/wulsin/AMSB_chargino_100GeV_ctau1000cm_FilterSumPt50_8TeV_pythia6_V1/AMSB_chargino_100GeV_ctau1000cm_FilterSumPt50_8TeV_pythia6_V1/d66ce63f45b0919f1bcc6fe69cbbc5b1/AMSB_chargino_RECO_9_1_ELT.root', 
-#    'root://xrootd.ba.infn.it//store/user/wulsin/AMSB_chargino_100GeV_ctau10cm_FilterSumPt50_8TeV_pythia6_V1/AMSB_chargino_100GeV_ctau10cm_FilterSumPt50_8TeV_pythia6_V1/a5bf8a415c75e7e03ed81894f138fdd4/AMSB_chargino_RECO_100_1_eB0.root',  
-#          'file:../HSCP_testPythia8_GEN.root',
-#          'file:HSCPstopToBottom_M_1000_1000mm_Tune4C_8TeV_pythia8_cff_GEN_SIM.root', 
-        'file:charginoPartGun_GEN_SIM.root', 
+        "file:/data/users/hart/condor/AMSB_chargino_M-700_CTau-100_TuneZ2star_13TeV_pythia6_step4/hist_0.root",
     )
 )
 
 process.maxEvents = cms.untracked.PSet (
-    input = cms.untracked.int32 (-1)
+    input = cms.untracked.int32 (10)
 )
 
 ###########################################################
