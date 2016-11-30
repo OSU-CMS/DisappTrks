9e5485565d67e35dec9712a7b1afd9188ec15b43
Andrew Hart <ahart@cern.ch>
2016-11-30 16:39:36 -0500

    Removed second instance of signal datasets.

--- StandardAnalysis/test/localConfig_2016DEFG.py
+++ StandardAnalysis/test/localConfig_2016DEFG.py
@@ -11,7 +11,7 @@ datasetsData = [
     'MET_2016G',
 ]
 
-datasets = datasetsData + datasetsSig + datasetsSig
+datasets = datasetsData + datasetsSig
 
 #setNJobs (datasets, composite_dataset_definitions, nJobs, 500)
 #setDatasetType (datasets, composite_dataset_definitions, types, "bgMC")
