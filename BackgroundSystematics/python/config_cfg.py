99f4ff57778ecf06d4d0a337bdc46a09e5b7fb75
Andrew Hart <ahart@cern.ch>
2016-12-09 14:32:13 -0500

    Switch to versions without fiducial cuts.

--- BackgroundSystematics/python/config_cfg.py
+++ BackgroundSystematics/python/config_cfg.py
@@ -4,10 +4,10 @@ from DisappTrks.StandardAnalysis.protoConfig_cfg import *
 # MET channels
 ################################################################################
 # Fake-track-enriched variations of the search region used for fake track systematic
-#  add_channels  (process,  [disTrkSelectionNHits3],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [disTrkSelectionNHits4],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [disTrkSelectionNHits5],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [disTrkSelectionNHits6],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [disTrkSelectionNHits3NoElectronMuonFiducialCuts],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [disTrkSelectionNHits4NoElectronMuonFiducialCuts],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [disTrkSelectionNHits5NoElectronMuonFiducialCuts],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [disTrkSelectionNHits6NoElectronMuonFiducialCuts],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
 ################################################################################
 
 if os.environ["CMSSW_VERSION"].startswith ("CMSSW_8_0_"):
