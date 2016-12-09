c0d7d19ce65aae4ed53a118405705593ef5c13d4
Andrew Hart <ahart@cern.ch>
2016-12-09 11:25:11 -0500

    Removed unnecessary file.

--- BackgroundSystematics/test/localConfig.py
+++
@@ -1,9 +0,0 @@
-from DisappTrks.StandardAnalysis.localConfig import *
-
-intLumi = 2590.0 # https://cmshead.mps.ohio-state.edu:8080/DisappearingTracks/595
-
-datasetsData = [
-    'MET_2015D',
-]
-
-datasets = datasetsBkgdForMET + datasetsData + datasetsSig
