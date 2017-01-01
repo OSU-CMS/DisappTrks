198fd448d7331978385b5fccc8259dc6853acd95
Andrew Hart <ahart@cern.ch>
2017-01-01 09:57:28 -0500

    First commit of a little script for printing out the luminosities in IntegratedLuminosity_cff.py.

---
+++ StandardAnalysis/scripts/printIntegratedLuminosity.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from DisappTrks.StandardAnalysis.IntegratedLuminosity_cff import *
+import sys
+
+def printValidKeys ():
+    for x in sorted (lumi.keys ()):
+        print "  " + x
+        if isinstance (lumi[x], dict):
+            for y in sorted (lumi[x].keys ()):
+                print "    " + y
+
+if len (sys.argv) < 2:
+    print "Usage: " + sys.argv[0] + " KEY SUB_KEY"
+    print ""
+    print "Valid values for KEY and SUB_KEY are the following:"
+    printValidKeys ()
+    sys.exit (1)
+
+if sys.argv[1] not in lumi or (len (sys.argv) > 2 and sys.argv[2] not in lumi[sys.argv[1]]) or (len (sys.argv) < 3 and isinstance (lumi[sys.argv[1]], dict)):
+    print "Invalid KEY or SUB_KEY. Valid values are the following:"
+    printValidKeys ()
+    sys.exit (1)
+
+print "integrated luminosity for",
+if len (sys.argv) < 3:
+    print sys.argv[1] + ": " + str (lumi[sys.argv[1]]) + "/pb"
+else:
+    print sys.argv[2] + " (" + sys.argv[1] + "): " + str (lumi[sys.argv[1]][sys.argv[2]]) + "/pb"
