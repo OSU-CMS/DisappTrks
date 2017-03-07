d8296a8144cf39814ace4c7722cfedc66518ea7c
Andrew Hart <ahart@cern.ch>
2017-03-07 16:33:44 -0500

    Removed 23Sep2016 samples.

--- StandardAnalysis/python/miniAOD_80X_Samples.py
+++ StandardAnalysis/python/miniAOD_80X_Samples.py
@@ -12,69 +12,34 @@ dataset_names_data = {
     # MiniAOD not stored on T3.
     ############################################################################
 
-    # MET 23Sep2016
-    'MET_2016B'       : "/MET/ahart-Run2016B-23Sep2016-v3-DisappTrks-v1-58d73df75df4f7c009ddc0cc54b0773f/USER",
-    'MET_2016C'       : "/MET/ahart-Run2016C-23Sep2016-v1-DisappTrks-v1-58d73df75df4f7c009ddc0cc54b0773f/USER",
-    'MET_2016D'       : "/MET/ahart-Run2016D-23Sep2016-v1-DisappTrks-v1-58d73df75df4f7c009ddc0cc54b0773f/USER",
-    'MET_2016E'       : "/MET/ahart-Run2016E-23Sep2016-v1-DisappTrks-v1-58d73df75df4f7c009ddc0cc54b0773f/USER",
-    'MET_2016F'       : "/MET/ahart-Run2016F-23Sep2016-v1-DisappTrks-v1-58d73df75df4f7c009ddc0cc54b0773f/USER",
-    'MET_2016G'       : "/MET/ahart-Run2016G-23Sep2016-v1-DisappTrks-v1-58d73df75df4f7c009ddc0cc54b0773f/USER",
-    'MET_2016H'       : ["/MET/ahart-Run2016H-PromptReco-v2-DisappTrks-v1-58d73df75df4f7c009ddc0cc54b0773f/USER", "/MET/ahart-Run2016H-PromptReco-v3-DisappTrks-v1-58d73df75df4f7c009ddc0cc54b0773f/USER"],
-
     # MET 2016 PromptReco
-    #'MET_2016C'       : "/MET/ahart-Run2016C-PromptReco-v2-DisappTrks-v8-58d73df75df4f7c009ddc0cc54b0773f/USER",
-    #'MET_2016F'       : "/MET/ahart-Run2016F-PromptReco-v1-DisappTrks-v8-58d73df75df4f7c009ddc0cc54b0773f/USER",
-    #'MET_2016G'       : "/MET/ahart-Run2016G-PromptReco-v1-DisappTrks-v8-58d73df75df4f7c009ddc0cc54b0773f/USER",
+    'MET_2016C'       : "/MET/ahart-Run2016C-PromptReco-v2-DisappTrks-v8-58d73df75df4f7c009ddc0cc54b0773f/USER",
+    'MET_2016F'       : "/MET/ahart-Run2016F-PromptReco-v1-DisappTrks-v8-58d73df75df4f7c009ddc0cc54b0773f/USER",
+    'MET_2016G'       : "/MET/ahart-Run2016G-PromptReco-v1-DisappTrks-v8-58d73df75df4f7c009ddc0cc54b0773f/USER",
 
     # SingleEle PromptReco
-    #'SingleEle_2016B' : "/SingleElectron/bfrancis-Run2016B-PromptReco-v2-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
-    #'SingleEle_2016C' : "/SingleElectron/bfrancis-Run2016C-PromptReco-v2-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
-    #'SingleEle_2016D' : "/SingleElectron/bfrancis-Run2016D-PromptReco-v2-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
-    #'SingleEle_2016E' : "/SingleElectron/bfrancis-Run2016E-PromptReco-v2-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
-    #'SingleEle_2016F' : "/SingleElectron/bfrancis-Run2016F-PromptReco-v1-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
-    #'SingleEle_2016G' : "/SingleElectron/bfrancis-Run2016G-PromptReco-v1-DisappTrks-v8-v2-addfbe4d6f0cf1056130ceadde75d969/USER",
-    #'SingleEle_2016H' : ["/SingleElectron/bfrancis-Run2016H-PromptReco-v2-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER", "/SingleElectron/bfrancis-Run2016H-PromptReco-v3-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER"],
-
-    # SingleEle 23Sep2016 rereco
-    'SingleEle_2016B' : "/SingleElectron/bfrancis-Run2016B-23Sep2016-v3-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER",
-    'SingleEle_2016C' : "/SingleElectron/bfrancis-Run2016C-23Sep2016-v1-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER",
-    'SingleEle_2016D' : "/SingleElectron/bfrancis-Run2016D-23Sep2016-v1-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER",
-    'SingleEle_2016E' : "/SingleElectron/bfrancis-Run2016E-23Sep2016-v1-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER",
-    'SingleEle_2016F' : "/SingleElectron/bfrancis-Run2016F-23Sep2016-v1-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER",
-    'SingleEle_2016G' : "/SingleElectron/bfrancis-Run2016G-23Sep2016-v1-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER",
+    'SingleEle_2016B' : "/SingleElectron/bfrancis-Run2016B-PromptReco-v2-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
+    'SingleEle_2016C' : "/SingleElectron/bfrancis-Run2016C-PromptReco-v2-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
+    'SingleEle_2016D' : "/SingleElectron/bfrancis-Run2016D-PromptReco-v2-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
+    'SingleEle_2016E' : "/SingleElectron/bfrancis-Run2016E-PromptReco-v2-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
+    'SingleEle_2016F' : "/SingleElectron/bfrancis-Run2016F-PromptReco-v1-DisappTrks-v8-addfbe4d6f0cf1056130ceadde75d969/USER",
+    'SingleEle_2016G' : "/SingleElectron/bfrancis-Run2016G-PromptReco-v1-DisappTrks-v8-v2-addfbe4d6f0cf1056130ceadde75d969/USER",
     'SingleEle_2016H' : ["/SingleElectron/bfrancis-Run2016H-PromptReco-v2-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER", "/SingleElectron/bfrancis-Run2016H-PromptReco-v3-DisappTrks-v1-addfbe4d6f0cf1056130ceadde75d969/USER"],
 
-    # SingleMu 23Sep2016
-    'SingleMu_2016B'  : "/SingleMuon/ahart-Run2016B-23Sep2016-v3-DisappTrks-v1-ba21f67961b05c35438052e716a22b53/USER",
-    'SingleMu_2016C'  : "/SingleMuon/wulsin-Run2016C-23Sep2016-v1-DisappTrks-v2-c0937f7e1b09431ec4046954bdd16494/USER",
-    'SingleMu_2016D'  : "/SingleMuon/wulsin-Run2016D-23Sep2016-v1-DisappTrks-v2-c0937f7e1b09431ec4046954bdd16494/USER",
-    'SingleMu_2016E'  : "/SingleMuon/wulsin-Run2016E-23Sep2016-v1-DisappTrks-v2-c0937f7e1b09431ec4046954bdd16494/USER",
-    'SingleMu_2016F'  : "/SingleMuon/wulsin-Run2016F-23Sep2016-v1-DisappTrks-v2-c0937f7e1b09431ec4046954bdd16494/USER",
-    'SingleMu_2016G'  : "/SingleMuon/wulsin-Run2016G-23Sep2016-v1-DisappTrks-v2-c0937f7e1b09431ec4046954bdd16494/USER",
-    'SingleMu_2016H'  : ["/SingleMuon/ahart-Run2016H-PromptReco-v2-DisappTrks-v2-c0937f7e1b09431ec4046954bdd16494/USER", "/SingleMuon/wulsin-Run2016H-PromptReco-v3-DisappTrks-v2-c0937f7e1b09431ec4046954bdd16494/USER"],
-
-    # SingleMu 2016 PromptReco
-    #'SingleMu_2016C'  : "/SingleMuon/ahart-Run2016C-PromptReco-v2-DisappTrks-v8-c0937f7e1b09431ec4046954bdd16494/USER",
-    #'SingleMu_2016G'  : "/SingleMuon/ahart-Run2016G-PromptReco-v1-DisappTrks-v8-c0937f7e1b09431ec4046954bdd16494/USER",
+    # SingleMu PromptReco
+    'SingleMu_2016C'  : "/SingleMuon/ahart-Run2016C-PromptReco-v2-DisappTrks-v8-c0937f7e1b09431ec4046954bdd16494/USER",
+    'SingleMu_2016G'  : "/SingleMuon/ahart-Run2016G-PromptReco-v1-DisappTrks-v8-c0937f7e1b09431ec4046954bdd16494/USER",
 
     # Tau PromptReco
-    #'Tau_2016B'        : "/Tau/bfrancis-Run2016B-PromptReco-v2-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
-    #'Tau_2016C'        : "/Tau/bfrancis-Run2016C-PromptReco-v2-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
-    #'Tau_2016D'        : "/Tau/bfrancis-Run2016D-PromptReco-v2-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
-    #'Tau_2016E'        : "/Tau/bfrancis-Run2016E-PromptReco-v2-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
-    #'Tau_2016F'        : "/Tau/bfrancis-Run2016F-PromptReco-v1-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
-    #'Tau_2016G'        : "/Tau/bfrancis-Run2016G-PromptReco-v1-DisappTrks-v8-v2-e69bca4a9575e4a5110aaa47eb9af51b/USER",
-    #'Tau_2016H'        :  ["/Tau/ahart-Run2016H-PromptReco-v2-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER", "/Tau/ahart-Run2016H-PromptReco-v3-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER"],
-
-    # Tau 23Sep2016
-    'Tau_2016B'        :  "/Tau/ahart-Run2016B-23Sep2016-v3-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER",
-    'Tau_2016C'        :  "/Tau/ahart-Run2016C-23Sep2016-v1-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER",
-    'Tau_2016D'        :  "/Tau/ahart-Run2016D-23Sep2016-v1-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER",
-    'Tau_2016E'        :  "/Tau/ahart-Run2016E-23Sep2016-v1-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER",
-    'Tau_2016F'        :  "/Tau/ahart-Run2016F-23Sep2016-v1-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER",
-    'Tau_2016G'        :  "/Tau/ahart-Run2016G-23Sep2016-v1-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER",
+    'Tau_2016B'        : "/Tau/bfrancis-Run2016B-PromptReco-v2-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
+    'Tau_2016C'        : "/Tau/bfrancis-Run2016C-PromptReco-v2-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
+    'Tau_2016D'        : "/Tau/bfrancis-Run2016D-PromptReco-v2-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
+    'Tau_2016E'        : "/Tau/bfrancis-Run2016E-PromptReco-v2-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
+    'Tau_2016F'        : "/Tau/bfrancis-Run2016F-PromptReco-v1-DisappTrks-v8-e69bca4a9575e4a5110aaa47eb9af51b/USER",
+    'Tau_2016G'        : "/Tau/bfrancis-Run2016G-PromptReco-v1-DisappTrks-v8-v2-e69bca4a9575e4a5110aaa47eb9af51b/USER",
     'Tau_2016H'        :  ["/Tau/ahart-Run2016H-PromptReco-v2-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER", "/Tau/ahart-Run2016H-PromptReco-v3-DisappTrks-v1-baeccab93c8c5cab5837a97f96996439/USER"],
 
+    # ZeroBias PromptReco
     'ZeroBias_2016C' : "/ZeroBias/ahart-Run2016C-PromptReco-v2-DisappTrks-v8-369b4fc9d4b0224d59848b34d927552e/USER",
     'ZeroBias_2016D' : "/ZeroBias/ahart-Run2016D-PromptReco-v2-DisappTrks-v8-369b4fc9d4b0224d59848b34d927552e/USER",
     'ZeroBias_2016E' : "/ZeroBias/ahart-Run2016E-PromptReco-v2-DisappTrks-v8-369b4fc9d4b0224d59848b34d927552e/USER",
