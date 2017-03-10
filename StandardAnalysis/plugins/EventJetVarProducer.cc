c5a80989e86c59783e676dab4a427ff5ef2dee3d
Andrew Hart <ahart@cern.ch>
2017-03-10 16:20:01 -0500

    Added the number of tracks as an event variable.

--- StandardAnalysis/plugins/EventJetVarProducer.cc
+++ StandardAnalysis/plugins/EventJetVarProducer.cc
@@ -8,7 +8,7 @@
 #include "OSUT3Analysis/AnaTools/interface/ValueLookupTree.h"
 #include "OSUT3Analysis/AnaTools/interface/CommonUtils.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
-
+#include "DataFormats/PatCandidates/interface/PackedCandidate.h"
 
 
 class EventJetVarProducer : public EventVariableProducer {
@@ -18,8 +18,10 @@ public:
 
 private:
   void AddVariables(const edm::Event &);
-  edm::EDGetTokenT<vector<TYPE(jets)> > tokenJets_;
-  edm::EDGetTokenT<vector<TYPE(mets)>  > tokenMets_;
+  edm::EDGetTokenT<vector<TYPE(jets)> >             tokenJets_;
+  edm::EDGetTokenT<vector<TYPE(mets)> >             tokenMets_;
+  edm::EDGetTokenT<vector<pat::PackedCandidate> >   tokenPFCands_;
+  edm::EDGetTokenT<vector<reco::Track> >            tokenLostTracks_;
 
   bool IsValidJet(const TYPE(jets) & jet);
 };
@@ -29,9 +31,10 @@ private:
 EventJetVarProducer::EventJetVarProducer(const edm::ParameterSet &cfg) :
   EventVariableProducer(cfg)
 {
-  tokenJets_      = consumes<vector<TYPE(jets)> >      (collections_.getParameter<edm::InputTag> ("jets"));
-  tokenMets_      = consumes<vector<TYPE(mets)> >      (collections_.getParameter<edm::InputTag> ("mets"));
-
+  tokenJets_        =  consumes<vector<TYPE(jets)> >             (collections_.getParameter<edm::InputTag>  ("jets"));
+  tokenMets_        =  consumes<vector<TYPE(mets)> >             (collections_.getParameter<edm::InputTag>  ("mets"));
+  tokenPFCands_     =  consumes<vector<pat::PackedCandidate> >   (collections_.getParameter<edm::InputTag>  ("pfCandidates"));
+  tokenLostTracks_  =  consumes<vector<reco::Track> >            (collections_.getParameter<edm::InputTag>  ("lostTracks"));
 }
 
 EventJetVarProducer::~EventJetVarProducer() {}
@@ -61,6 +64,15 @@ EventJetVarProducer::AddVariables (const edm::Event &event) {
     clog << "ERROR:  Could not find mets collection." << endl;
     return;
   }
+
+  edm::Handle<vector<pat::PackedCandidate> > pfCands;
+  if (!event.getByToken (tokenPFCands_, pfCands)) {
+    clog << "ERROR:  Could not find pfCandidates collection." << endl;
+    return;
+  }
+
+  edm::Handle<vector<reco::Track> > lostTracks;
+  event.getByToken (tokenLostTracks_, lostTracks);
 
   int nJets = 0;
   double dijetMaxDeltaPhi         = -999.;  // default is large negative value
@@ -97,6 +109,15 @@ EventJetVarProducer::AddVariables (const edm::Event &event) {
     nJets++;
   }
 
+  unsigned nTracks = 0;
+  for (const auto &pfCand : *pfCands)
+    {
+      if (pfCand.charge () && pfCand.numberOfHits () > 0)
+        nTracks++;
+    }
+  if (lostTracks.isValid ())
+    nTracks += lostTracks->size ();
+
   (*eventvariables)["nJets"]            = nJets;
   (*eventvariables)["dijetMaxDeltaPhi"] = dijetMaxDeltaPhi;
   (*eventvariables)["deltaPhiMetJetLeading"]     = deltaPhiMetJetLeading;
@@ -104,6 +125,8 @@ EventJetVarProducer::AddVariables (const edm::Event &event) {
 
   (*eventvariables)["eventUnixTime"]  = event.time ().unixTime ();
   (*eventvariables)["runNumber"]  = event.run ();
+
+  (*eventvariables)["nTracks"]  = nTracks;
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
