8d5ae8874c5138092935922b3df047125b54d44f
Andrew Hart <ahart@cern.ch>
2017-04-17 13:31:21 -0400

    Set default MET filters according to POG recommendations for Moriond 2017.

--- StandardAnalysis/python/METFilters.py
+++ StandardAnalysis/python/METFilters.py
@@ -1,13 +1,19 @@
 import FWCore.ParameterSet.Config as cms
+import OSUT3Analysis.DBTools.osusub_cfg as osusub
+from OSUT3Analysis.Configuration.configurationOptions import *
 import os
 
 # MET filters recommended for Moriond 2017 here:
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2#Moriond_2017
 metFilters = cms.vstring ([
-  #"Flag_goodVertices",
-  #"Flag_globalTightHalo2016Filter",
-  #"Flag_HBHENoiseFilter",
-  #"Flag_HBHENoiseIsoFilter",
-  #"Flag_EcalDeadCellTriggerPrimitiveFilter",
-  #"Flag_eeBadScFilter",
+  "Flag_goodVertices",
+  "Flag_globalTightHalo2016Filter",
+  "Flag_HBHENoiseFilter",
+  "Flag_HBHENoiseIsoFilter",
+  "Flag_EcalDeadCellTriggerPrimitiveFilter",
 ])
+
+if osusub.batchMode and types[osusub.datasetLabel] == "data":
+  metFilters.append (
+    "Flag_eeBadScFilter",
+  )
