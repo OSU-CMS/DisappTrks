524e63e9d38321a7f0e4d48eb97c7dcf6fdd7c9b
Andrew Hart <ahart@cern.ch>
2017-04-18 09:13:09 -0400

    Added the other genParticle collections available in our signal MC.

--- SignalMC/test/particleListDrawer_cfg.py
+++ SignalMC/test/particleListDrawer_cfg.py
@@ -9,7 +9,7 @@ process.load ('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.source = cms.Source ('PoolSource',
     fileNames = cms.untracked.vstring (
-        "file:/data/users/hart/condor/AMSB_chargino_M-700_CTau-100_TuneZ2star_13TeV_pythia6_step4/hist_0.root",
+        "file:/data/users/hart/condor/signalMC/2016/AMSB_chargino_M-700_CTau-100_TuneZ2star_13TeV_pythia6_step4/hist_0.root",
     )
 )
 
@@ -26,10 +26,14 @@ process.particleListDrawer = cms.EDAnalyzer ('ParticleListDrawer',
     maxEventsToPrint = cms.untracked.int32(-1),
     printOnlyHardInteraction = cms.untracked.bool(False),
     printVertex = cms.untracked.bool(True),
-    printFlags = cms.untracked.bool(True),
+    printFlags = cms.untracked.bool(False),
     useMessageLogger = cms.untracked.bool(False),
-#    src = cms.InputTag("genParticles")
-    src = cms.InputTag("genParticlePlusGeant")
+    src = cms.InputTag("genParticles")
+#    src = cms.InputTag("genParticlePlusGeant")
+#    src = cms.InputTag("prunedGenParticles")
+#    src = cms.InputTag("prunedGenParticlePlusGeant")
+#    src = cms.InputTag("packedGenParticles")
+#    src = cms.InputTag("packedGenParticlePlusGeant")
 )
 
 process.myPath = cms.Path (process.particleListDrawer)
