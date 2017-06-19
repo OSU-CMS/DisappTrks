390c36c71c854af53df619073cd483f8cf951f48
Andrew Hart <ahart@cern.ch>
2017-06-19 11:53:48 -0400

    Replaced "D-G" with "D-H".

--- StandardAnalysis/python/localConfig.py
+++ StandardAnalysis/python/localConfig.py
@@ -154,5 +154,5 @@ for attribute in list (locals ()):
             b = re.sub (r"(.*)2016D(.*)", r"\g<1>2016DEFGH\2", a)
             newKeys[b] = copy.deepcopy (locals ()[attribute][a])
             if isinstance (newKeys[b], str) and re.match (r".*2016D.*", newKeys[b]):
-                newKeys[b] = re.sub (r"(.*)2016D(.*)", r"\g<1>2016D-G\2", newKeys[b])
+                newKeys[b] = re.sub (r"(.*)2016D(.*)", r"\g<1>2016D-H\2", newKeys[b])
     locals ()[attribute].update (newKeys)
