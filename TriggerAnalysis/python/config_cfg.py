1db64f7b84b2e1822c10d98d2a62388c0ca917d8
Andrew Hart <ahart@cern.ch>
2017-08-11 10:32:10 -0400

    I just needed to realign.

--- TriggerAnalysis/python/config_cfg.py
+++ TriggerAnalysis/python/config_cfg.py
@@ -7,42 +7,42 @@ variableProducers.append('EventTriggerVarProducer')
 ################################################################################
 
 # HLT_MET75_IsoTrk50 channels
-# add_channels (process, [METLegDenominator],                            histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
-# add_channels (process, METLegNumerator.values(),                       histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
-# add_channels (process, TrackLegDenominatorWithMuons.values(),          histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
-# add_channels (process, TrackLegNumeratorWithMuons.values(),            histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
+#  add_channels  (process,  [METLegDenominator],                    histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  METLegNumerator.values(),               histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  TrackLegDenominatorWithMuons.values(),  histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  TrackLegNumeratorWithMuons.values(),    histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
 
 # The Grand Or
-# add_channels (process, [GrandOrDenominator],                           histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
-# add_channels (process, [GrandORNumerator],                             histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
+#  add_channels  (process,  [GrandOrDenominator],  histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [GrandORNumerator],    histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
 
 # Testing: require a match of any muon to the HLT track rather than just the lead muon
-# add_channels (process, TrackLegNumeratorWithMuonsAnyHLTMatch.values(), histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
+#  add_channels  (process,  TrackLegNumeratorWithMuonsAnyHLTMatch.values(),  histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
 
 ################################################################################
 # Signal MC channels
 ################################################################################
 
 # HLT_MET75_IsoTrk50 channels
-# add_channels (process, [METLegDenominator],                            histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
-# add_channels (process, METLegNumerator.values(),                       histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
-# add_channels (process, TrackLegDenominatorWithTracksNoTrig.values(),   histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
-# add_channels (process, TrackLegNumeratorWithTracksNoTrig.values(),     histSetsTrigger, weightsWithMuonSF, scaleFactorProducersWithMuons, collectionMap, variableProducers, False)
+#  add_channels  (process,  [METLegDenominator],                           histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  METLegNumerator.values(),                      histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  TrackLegDenominatorWithTracksNoTrig.values(),  histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  TrackLegNumeratorWithTracksNoTrig.values(),    histSetsTrigger,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
 
 ################################################################################
 # HLT purity measurement channels -- BasicSelection but only with one HLT path
 ################################################################################
 
-# add_channels (process, [basicSelectionOnlyMET75IsoTrk50],              histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
-# add_channels (process, [basicSelectionOnlyMET90IsoTrk50],              histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [basicSelectionOnlyMET75IsoTrk50],  histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [basicSelectionOnlyMET90IsoTrk50],  histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
 
-# add_channels (process, [basicSelectionOnlyMET75IsoTrk50HltMet105],     histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
-# add_channels (process, [basicSelectionOnlyMET90IsoTrk50HltMet105],     histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [basicSelectionOnlyMET75IsoTrk50HltMet105],  histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [basicSelectionOnlyMET90IsoTrk50HltMet105],  histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
 
 ################################################################################
 
-# add_channels (process, [justMET75IsoTrk50],              histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
-# add_channels (process, [justMET90IsoTrk50],              histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [justMET75IsoTrk50],  histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [justMET90IsoTrk50],  histSetsTrigger,  weights,  [],  collectionMap,  variableProducers,  False)
 
 process.EventJetVarProducer.triggerNames = triggerNamesInclusive
 process.EventTriggerVarProducer.triggerNames = triggerNamesInclusive
