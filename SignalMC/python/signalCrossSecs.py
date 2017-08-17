4d21871fe84caebb68692819b84b7e02405330bb
Andrew Hart <ahart@cern.ch>
2017-08-17 11:26:44 -0400

    Added dictionaries for chargino-neutralino and chargino-chargino production, which are added using the Measurement class.

--- SignalMC/python/signalCrossSecs.py
+++ SignalMC/python/signalCrossSecs.py
@@ -1,35 +1,100 @@
 #!/usr/bin/env python
 
+from OSUT3Analysis.Configuration.Measurement import Measurement
 
 #values and errors taken from https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVx1x1wino and
 # https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVn2x1wino
-signal_cross_sections = { # in pb, in terms of chargino mass
+
+chargino_neutralino_cross_sections = { # in pb, in terms of chargino mass
     '100' : {
-    'value' : '34.282',
-    'error' : '1.032187',
+        'value' : str ( 22670.1 * 1.0e-3 ),
+        'error' : str ( 973.967 * 1.0e-3 ),
     },
     '200' : {
-    'value' : '2.709959',
-    'error' : '1.042320',
+        'value' : str ( 1807.39 * 1.0e-3 ),
+        'error' : str ( 101.316 * 1.0e-3 ),
     },
     '300' : {
-    'value' : '0.577095',
-    'error' : '1.051275',
+        'value' : str ( 386.936 * 1.0e-3 ),
+        'error' : str ( 26.3602 * 1.0e-3 ),
     },
     '400' : {
-    'value' : '0.179644',
-    'error' : '1.059650',
+        'value' : str ( 121.013 * 1.0e-3 ),
+        'error' : str ( 9.61659 * 1.0e-3 ),
     },
     '500' : {
-    'value' : '0.06848',
-    'error' : '1.067146',
+        'value' : str ( 46.3533 * 1.0e-3 ),
+        'error' : str ( 4.16461 * 1.0e-3 ),
     },
     '600' : {
-    'value' : '0.029636',
-    'error' : '1.075539',
+        'value' : str ( 20.1372 * 1.0e-3 ),
+        'error' : str ( 2.04438 * 1.0e-3 ),
     },
     '700' : {
-    'value' : '0.013949',
-    'error' : '1.081502',
+        'value' : str ( 9.51032 * 1.0e-3 ),
+        'error' : str ( 1.04092 * 1.0e-3 ),
+    },
+    '800' : {
+        'value' : str ( 4.75843 * 1.0e-3 ),
+        'error' : str ( 0.564061 * 1.0e-3 ),
+    },
+    '900' : {
+        'value' : str ( 2.49667 * 1.0e-3 ),
+        'error' : str ( 0.314019 * 1.0e-3 ),
     },
 }
+
+chargino_chargino_cross_sections = { # in pb, in terms of chargino mass
+    '100' : {
+        'value' : str ( 11611.9 * 1.0e-3 ),
+        'error' : str ( 518.613 * 1.0e-3 ),
+    },
+    '200' : {
+        'value' : str ( 902.569 * 1.0e-3 ),
+        'error' : str ( 53.7411 * 1.0e-3 ),
+    },
+    '300' : {
+        'value' : str ( 190.159 * 1.0e-3 ),
+        'error' : str ( 13.4438 * 1.0e-3 ),
+    },
+    '400' : {
+        'value' : str ( 58.6311 * 1.0e-3 ),
+        'error' : str ( 4.7276 * 1.0e-3 ),
+    },
+    '500' : {
+        'value' : str ( 22.1265 * 1.0e-3 ),
+        'error' : str ( 1.94904 * 1.0e-3 ),
+    },
+    '600' : {
+        'value' : str ( 9.49913 * 1.0e-3 ),
+        'error' : str ( 0.912324 * 1.0e-3 ),
+    },
+    '700' : {
+        'value' : str ( 4.4387 * 1.0e-3 ),
+        'error' : str ( 0.457123 * 1.0e-3 ),
+    },
+    '800' : {
+        'value' : str ( 2.21197 * 1.0e-3 ),
+        'error' : str ( 0.245196 * 1.0e-3 ),
+    },
+    '900' : {
+        'value' : str ( 1.15301 * 1.0e-3 ),
+        'error' : str ( 0.135822 * 1.0e-3 ),
+    },
+}
+
+signal_cross_sections = {}
+
+for mass in chargino_neutralino_cross_sections:
+    cnX = chargino_neutralino_cross_sections[mass]
+    ccX = chargino_chargino_cross_sections[mass]
+
+    cn = Measurement (cnX['value'], cnX['error'])
+    cc = Measurement (ccX['value'], ccX['error'])
+
+    total = cn + cc
+
+    signal_cross_sections[mass] = {
+        'value' : str (total.centralValue ()),
+        'error' : str (total.uncertainty ()),
+    }
