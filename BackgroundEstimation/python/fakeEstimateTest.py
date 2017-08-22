0a12d22c7752b29e59e78cc1f6e72ccd752903cf
Andrew Hart <ahart@cern.ch>
2017-08-22 04:36:17 -0400

    Migrated to use the Measurement class.

--- BackgroundEstimation/python/fakeEstimateTest.py
+++ BackgroundEstimation/python/fakeEstimateTest.py
@@ -6,6 +6,7 @@ import functools
 
 from ROOT import gROOT, gStyle, TCanvas, TFile, TGraphAsymmErrors, TH1D, TH3D, TMath, TPaveText, TObject
 
+from OSUT3Analysis.Configuration.Measurement import Measurement
 from DisappTrks.StandardAnalysis.plotUtilities import *
 
 setTDRStyle()
@@ -48,108 +49,96 @@ class FakeTrackBkgdEstimate:
 
     def addChannel (self, role, name, sample, condorDir):
         channel = {"name" : name, "sample" : sample, "condorDir" : condorDir}
-        channel["yield"], channel["yieldError"] = getHistIntegral (sample, condorDir, name + "Plotter", "Track-eventvariable Plots/trackd0WRTPVMag", 0.0, self._maxD0 - 0.001)
+        n, nError = getHistIntegral (sample, condorDir, name + "Plotter", "Track-eventvariable Plots/trackd0WRTPVMag", 0.0, self._maxD0 - 0.001)
+        channel["yield"] = Measurement (n, nError)
+        channel["yield"].isPositive ()
         setattr (self, role, channel)
-        print "yield for " + name + ": " + str (channel["yield"]) + " +- " + str (channel["yieldError"])
+        print "yield for " + name + ": " + str (channel["yield"])
 
     def printTransferFactor (self):
         if hasattr (self, "Basic3hits"):
             passes, passesError = getHistIntegral (self.Basic3hits["sample"], self.Basic3hits["condorDir"], self.Basic3hits["name"] + "Plotter", "Track-eventvariable Plots/trackd0WRTPVMag", 0.0, 0.02 - 0.001)
             fails, failsError = getHistIntegral (self.Basic3hits["sample"], self.Basic3hits["condorDir"], self.Basic3hits["name"] + "Plotter", "Track-eventvariable Plots/trackd0WRTPVMag", self._minD0, self._maxD0 - 0.001)
 
+            passes = Measurement (passes, passesError)
+            fails = Measurement (fails, failsError)
+
+            passes.isPositive ()
+            fails.isPositive ()
+
             if fails > 0.0:
                 transferFactor = passes / fails
-                transferFactorError = transferFactor * math.hypot (passesError/passes, failsError/fails)
-                print "Transfer factor: " + str (transferFactor) + " +/- " + str (transferFactorError)
-                return (transferFactor, transferFactorError, passes, passesError, fails, failsError)
+                print "Transfer factor: " + str (transferFactor)
+                return (transferFactor, passes, fails)
             else:
                 print "N(fail d0 cut, 3 hits) = 0, not printing scale factor..."
-                return (float ("nan"), float ("nan"), float ("nan"), float ("nan"), float ("nan"), float ("nan"))
+                return (float ("nan"), float ("nan"), float ("nan"))
 
         else:
             print "Basic3hits is not defined. Not printing transfer factor..."
-            return (float ("nan"), float ("nan"), float ("nan"), float ("nan"), float ("nan"), float ("nan"))
+            return (float ("nan"), float ("nan"), float ("nan"))
 
     def printNctrl (self):
         if hasattr (self, "DisTrkInvertD0"):
             n, nError = getHistIntegral (self.DisTrkInvertD0["sample"], self.DisTrkInvertD0["condorDir"], self.DisTrkInvertD0["name"] + "Plotter", "Track-eventvariable Plots/trackd0WRTPVMag", self._minD0, self._maxD0 - 0.001)
+            n = Measurement (n, (nError if n != 0.0 else 0.5 * TMath.ChisquareQuantile (0.68, 2 * (n + 1))))
+            n.isPositive ()
+
             if self._minHits < 7:
                 n6, n6Error = getHistIntegral (self.DisTrkInvertD0NHits6["sample"], self.DisTrkInvertD0NHits6["condorDir"], self.DisTrkInvertD0NHits6["name"] + "Plotter", "Track-eventvariable Plots/trackd0WRTPVMag", self._minD0, self._maxD0 - 0.001)
+                n6 = Measurement (n6, (n6Error if n6 != 0.0 else 0.5 * TMath.ChisquareQuantile (0.68, 2 * (n6 + 1))))
+                n6.isPositive ()
                 n += n6
-                nError = math.hypot (nError, n6Error)
             if self._minHits < 6:
                 n5, n5Error = getHistIntegral (self.DisTrkInvertD0NHits5["sample"], self.DisTrkInvertD0NHits5["condorDir"], self.DisTrkInvertD0NHits5["name"] + "Plotter", "Track-eventvariable Plots/trackd0WRTPVMag", self._minD0, self._maxD0 - 0.001)
+                n5 = Measurement (n5, (n5Error if n5 != 0.0 else 0.5 * TMath.ChisquareQuantile (0.68, 2 * (n5 + 1))))
+                n5.isPositive ()
                 n += n5
-                nError = math.hypot (nError, n5Error)
             if self._minHits < 5:
                 n4, n4Error = getHistIntegral (self.DisTrkInvertD0NHits4["sample"], self.DisTrkInvertD0NHits4["condorDir"], self.DisTrkInvertD0NHits4["name"] + "Plotter", "Track-eventvariable Plots/trackd0WRTPVMag", self._minD0, self._maxD0 - 0.001)
+                n4 = Measurement (n4, (n4Error if n4 != 0.0 else 0.5 * TMath.ChisquareQuantile (0.68, 2 * (n4 + 1))))
+                n4.isPositive ()
                 n += n4
-                nError = math.hypot (nError, n4Error)
             if self._minHits < 4:
                 n3, n3Error = getHistIntegral (self.DisTrkInvertD0NHits3["sample"], self.DisTrkInvertD0NHits3["condorDir"], self.DisTrkInvertD0NHits3["name"] + "Plotter", "Track-eventvariable Plots/trackd0WRTPVMag", self._minD0, self._maxD0 - 0.001)
+                n3 = Measurement (n3, (n3Error if n3 != 0.0 else 0.5 * TMath.ChisquareQuantile (0.68, 2 * (n3 + 1))))
+                n3.isPositive ()
                 n += n3
-                nError = math.hypot (nError, n3Error)
-            if n == 0.0:
-                nError = 0.5 * TMath.ChisquareQuantile (0.68, 2 * (n + 1))
 
             pFake = pFakeError = float ("nan")
 
             # For ZtoMuMu control regions, need to normalize to BasicSelection
             if hasattr (self, "ZtoLL") and hasattr (self, "Basic"):
                 norm = self.Basic["yield"] / self.ZtoLL["yield"]
-                normError = norm * math.hypot (self.Basic["yieldError"] / self.Basic["yield"], self.ZtoLL["yieldError"] / self.ZtoLL["yield"])
-
                 pFake = n / self.ZtoLL["yield"]
-                pFakeError = math.hypot (n * self.ZtoLL["yieldError"], nError * self.ZtoLL["yield"]) / (self.ZtoLL["yield"] * self.ZtoLL["yield"])
-
                 n = n * norm
-                nError = math.hypot (n * normError, nError * norm)
             elif hasattr (self, "Basic"):
                 pFake = n / self.Basic["yield"]
-                pFakeError = math.hypot (n * self.Basic["yieldError"], nError * self.Basic["yield"]) / (self.Basic["yield"] * self.Basic["yield"])
 
             n *= self._prescale
-            nError *= self._prescale
 
-            if n > 0.0:
-                print "N_ctrl: " + str (n) + " +- " + str (nError) + " (" + str (n / self._luminosityInInvFb) + " +- " + str (nError / self._luminosityInInvFb) + " fb)"
-            else:
-                print "N_ctrl: " + str (n) + " - 0.0 + " + str (nError) + " (" + str (n / self._luminosityInInvFb) + " - 0 + " + str (nError / self._luminosityInInvFb) + " fb)"
-            return (n, nError, pFake, pFakeError)
+            print "N_ctrl: " + str (n) + " (" + str (n / self._luminosityInInvFb) + " fb)"
+            return (n, pFake)
         else:
             print "DisTrkInvertD0 is not defined. Not printing N_ctrl..."
-            return (float ("nan"), float ("nan"), float ("nan"), float ("nan"))
+            return (float ("nan"), float ("nan"))
 
     def printNest (self):
-        xi, xiError, xiPass, xiPassError, xiFail, xiFailError = self.printTransferFactor ()
-        nCtrl, nCtrlError, pFake, pFakeError = self.printNctrl ()
+        xi, xiPass, xiFail = self.printTransferFactor ()
+        nCtrl, pFake = self.printNctrl ()
 
         pFake *= xi
-        pFakeError = math.hypot (xi * pFakeError, xiError * pFake)
 
         N = nCtrl
         alpha = (xiPass / xiFail)
-        alphaError = math.hypot (xiFailError * xiPass, xiPassError * xiFail) / (xiFail * xiFail)
 
         nEst = xi * nCtrl
-        nEstError = math.hypot (xiError * nCtrl, nCtrlError * xi)
 
-        if pFake > 0.0:
-            print "P_fake: " + str (pFake) + " +- " + str (pFakeError)
-        else:
-            print "P_fake: " + str (pFake) + " - 0 + " + str (pFakeError)
-
+        alpha.printLongFormat ()
+        print "P_fake: " + str (pFake)
         print "N: " + str (N)
-        if not (alpha == 0):
-            print "alpha: " + str (alpha) + " +- " + str (alphaError)
-            print "error on alpha: " + str (1.0 + (alphaError / alpha))
-        else:
-            print "alpha: " + str (alpha) + " - 0 + " + str (alphaError)
-            print "error on alpha: nan"
+        print "alpha: " + str (alpha)
+        print "error on alpha: " + str ((1.0 + (alpha.uncertainty () / alpha.centralValue ())) if alpha != 0.0 else float ("nan"))
+        print "N_est: " + str (nEst) + " (" + str (nEst / self._luminosityInInvFb) + " fb)"
 
-        if nEst > 0.0:
-            print "N_est: " + str (nEst) + " +- " + str (nEstError) + " (" + str (nEst / self._luminosityInInvFb) + " +- " + str (nEstError / self._luminosityInInvFb) + " fb)"
-        else:
-            print "N_est: " + str (nEst) + " + " + str (nEstError) + " - 0.0 (" + str (nEst / self._luminosityInInvFb) + " + " + str (nEstError / self._luminosityInInvFb) + " - 0.0 fb)"
-
-        return (nEst, nEstError)
+        return (nEst.centralValue (), nEst.uncertainty ())
