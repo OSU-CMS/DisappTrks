373fa74d2c130081fa57fbbe352440159e4ed716
Andrew Hart <ahart@cern.ch>
2017-08-28 15:05:56 -0400

    Updated for one final round of ntuple making.

--- CandidateTrackProducer/test/crab_MiniAODExt_2016.py
+++ CandidateTrackProducer/test/crab_MiniAODExt_2016.py
@@ -18,15 +18,18 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_MC2016_cfg.py'  # For MC only
 config.JobType.allowUndistributedCMSSW = True
 
+config.JobType.numCores = 4
 config.Data.inputDataset = ''
 config.Data.useParent = False
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased' # for both MC and data
-config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+#config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/group/lpclonglived/DisappTrks/'
 config.Data.publication = True
 config.Data.outputDatasetTag = 'RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-DisappTrks-v1'
 
-config.Site.storageSite = 'T3_US_OSU'
+#config.Site.storageSite = 'T2_US_Purdue'
+config.Site.storageSite = 'T3_US_FNALLPC'
 
 if __name__ == '__main__':
 
@@ -56,8 +59,6 @@ if __name__ == '__main__':
 ##########################################################################
     ## Do the MC first
 
-    config.Site.storageSite = 'T2_US_Purdue'
-
     config.Data.unitsPerJob = 75
     config.General.requestName = 'candidateTrackProducer_DYToLL'
     config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16reHLT80-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext1-v1/AODSIM'
@@ -68,167 +69,166 @@ if __name__ == '__main__':
     config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16reHLT80-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext1-v1/AODSIM'
     #forkAndSubmit(config)
 
-    config.Data.unitsPerJob = 2
-    config.General.requestName = 'candidateTrackProducer_TT_RecoDebug'
-    config.Data.inputDataset = '/TT_TuneCUETP8M1_13TeV-amcatnlo-pythia8/RunIISpring16DR80-PUSpring16_RECODEBUG_80X_mcRun2_asymptotic_2016_v3_ext1-v1/GEN-SIM-RECODEBUG'
-    #forkAndSubmit(config)
-
 ##########################################################################
     ## Now do data
-    config.JobType.numCores = 4
+    config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/Final/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt'
 
-    config.Data.unitsPerJob = 10
-    config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+    # Run2016B-v2 (v1 exists but is disjoint from the Golden Prompt JSON)
 
-    config.Site.storageSite = 'T2_US_Purdue'
+    config.Data.outputDatasetTag = 'Run2016B-PromptReco-v2-DisappTrks-v9'
+    config.Data.unitsPerJob = 64 # 2016B has ~64083 lumis
 
-    # Run2016B-v3
-
-    config.Data.outputDatasetTag = 'Run2016B-23Sep2016-v3-DisappTrks-v2'
-
-    config.General.requestName = 'candidateTrackProducer_MET_2016B-23Sep2016-v3'
-    config.Data.inputDataset = '/MET/Run2016B-23Sep2016-v3/AOD'
+    config.General.requestName = 'candidateTrackProducer_MET_2016B-PromptReco-v2'
+    config.Data.inputDataset = '/MET/Run2016B-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_METSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_Tau_2016B-23Sep2016-v3'
-    config.Data.inputDataset = '/Tau/Run2016B-23Sep2016-v3/AOD'
+    config.General.requestName = 'candidateTrackProducer_Tau_2016B-PromptReco-v2'
+    config.Data.inputDataset = '/Tau/Run2016B-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_TauSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016B-23Sep2016-v3'
-    config.Data.inputDataset = '/SingleMuon/Run2016B-23Sep2016-v3/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016B-PromptReco-v2'
+    config.Data.inputDataset = '/SingleMuon/Run2016B-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_MuonSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016B-23Sep2016-v3'
-    config.Data.inputDataset = '/SingleElectron/Run2016B-23Sep2016-v3/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016B-PromptReco-v2'
+    config.Data.inputDataset = '/SingleElectron/Run2016B-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_ElectronSkim_cfg.py'
     #forkAndSubmit(config)
 
     # Run2016C
 
-    config.Data.outputDatasetTag = 'Run2016C-23Sep2016-v1-DisappTrks-v2'
+    config.Data.outputDatasetTag = 'Run2016C-PromptReco-v2-DisappTrks-v9'
+    config.Data.unitsPerJob = 22 # 2016C has ~21892 lumis
 
-    config.General.requestName = 'candidateTrackProducer_MET_2016C-23Sep2016-v1'
-    config.Data.inputDataset = '/MET/Run2016C-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_MET_2016C-PromptReco-v2'
+    config.Data.inputDataset = '/MET/Run2016C-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_METSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_Tau_2016C-23Sep2016-v1'
-    config.Data.inputDataset = '/Tau/Run2016C-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_Tau_2016C-PromptReco-v2'
+    config.Data.inputDataset = '/Tau/Run2016C-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_TauSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016C-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleMuon/Run2016C-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016C-PromptReco-v2'
+    config.Data.inputDataset = '/SingleMuon/Run2016C-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_MuonSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016C-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleElectron/Run2016C-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016C-PromptReco-v2'
+    config.Data.inputDataset = '/SingleElectron/Run2016C-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_ElectronSkim_cfg.py'
     #forkAndSubmit(config)
 
     # Run2016D
 
-    config.Data.outputDatasetTag = 'Run2016D-23Sep2016-v1-DisappTrks-v2'
+    config.Data.outputDatasetTag = 'Run2016D-PromptReco-v2-DisappTrks-v9'
+    config.Data.unitsPerJob = 31 # 2016D has ~31021 lumis
 
-    config.General.requestName = 'candidateTrackProducer_MET_2016D-23Sep2016-v1'
-    config.Data.inputDataset = '/MET/Run2016D-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_MET_2016D-PromptReco-v2'
+    config.Data.inputDataset = '/MET/Run2016D-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_METSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_Tau_2016D-23Sep2016-v1'
-    config.Data.inputDataset = '/Tau/Run2016D-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_Tau_2016D-PromptReco-v2'
+    config.Data.inputDataset = '/Tau/Run2016D-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_TauSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016D-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleMuon/Run2016D-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016D-PromptReco-v2'
+    config.Data.inputDataset = '/SingleMuon/Run2016D-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_MuonSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016D-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleElectron/Run2016D-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016D-PromptReco-v2'
+    config.Data.inputDataset = '/SingleElectron/Run2016D-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_ElectronSkim_cfg.py'
     #forkAndSubmit(config)
 
     # Run2016E
 
-    config.Data.outputDatasetTag = 'Run2016E-23Sep2016-v1-DisappTrks-v2'
+    config.Data.outputDatasetTag = 'Run2016E-PromptReco-v2-DisappTrks-v9'
+    config.Data.unitsPerJob = 28 # 2016E has ~27807 lumis
 
-    config.General.requestName = 'candidateTrackProducer_MET_2016E-23Sep2016-v1'
-    config.Data.inputDataset = '/MET/Run2016E-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_MET_2016E-PromptReco-v2'
+    config.Data.inputDataset = '/MET/Run2016E-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_METSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_Tau_2016E-23Sep2016-v1'
-    config.Data.inputDataset = '/Tau/Run2016E-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_Tau_2016E-PromptReco-v2'
+    config.Data.inputDataset = '/Tau/Run2016E-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_TauSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016E-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleMuon/Run2016E-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016E-PromptReco-v2'
+    config.Data.inputDataset = '/SingleMuon/Run2016E-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_MuonSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016E-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleElectron/Run2016E-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016E-PromptReco-v2'
+    config.Data.inputDataset = '/SingleElectron/Run2016E-PromptReco-v2/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_ElectronSkim_cfg.py'
     #forkAndSubmit(config)
 
     # Run2016F
+    # NOTE: this indeed is PromptReco-v1, there is no v2 is DAS for these datasets
 
-    config.Data.outputDatasetTag = 'Run2016F-23Sep2016-v1-DisappTrks-v2'
+    config.Data.outputDatasetTag = 'Run2016F-PromptReco-v1-DisappTrks-v9'
+    config.Data.unitsPerJob = 21 # 2016F has ~21220 lumis
 
-    config.General.requestName = 'candidateTrackProducer_MET_2016F-23Sep2016-v1'
-    config.Data.inputDataset = '/MET/Run2016F-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_MET_2016F-PromptReco-v1'
+    config.Data.inputDataset = '/MET/Run2016F-PromptReco-v1/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_METSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_Tau_2016F-23Sep2016-v1'
-    config.Data.inputDataset = '/Tau/Run2016F-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_Tau_2016F-PromptReco-v1'
+    config.Data.inputDataset = '/Tau/Run2016F-PromptReco-v1/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_TauSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016F-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleMuon/Run2016F-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016F-PromptReco-v1'
+    config.Data.inputDataset = '/SingleMuon/Run2016F-PromptReco-v1/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_MuonSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016F-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleElectron/Run2016F-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016F-PromptReco-v1'
+    config.Data.inputDataset = '/SingleElectron/Run2016F-PromptReco-v1/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_ElectronSkim_cfg.py'
     #forkAndSubmit(config)
 
     # Run2016G
+    # NOTE: this indeed is PromptReco-v1, there is no v2 is DAS for these datasets
 
-    config.Data.outputDatasetTag = 'Run2016G-23Sep2016-v1-DisappTrks-v2'
+    config.Data.outputDatasetTag = 'Run2016G-PromptReco-v1-DisappTrks-v9'
+    config.Data.unitsPerJob = 49 # 2016G has ~49295 lumis
 
-    config.General.requestName = 'candidateTrackProducer_MET_2016G-23Sep2016-v1'
-    config.Data.inputDataset = '/MET/Run2016G-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_MET_2016G-PromptReco-v1'
+    config.Data.inputDataset = '/MET/Run2016G-PromptReco-v1/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_METSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_Tau_2016G-23Sep2016-v1'
-    config.Data.inputDataset = '/Tau/Run2016G-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_Tau_2016G-PromptReco-v1'
+    config.Data.inputDataset = '/Tau/Run2016G-PromptReco-v1/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_TauSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016G-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleMuon/Run2016G-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleMuon_2016G-PromptReco-v1'
+    config.Data.inputDataset = '/SingleMuon/Run2016G-PromptReco-v1/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_MuonSkim_cfg.py'
     #forkAndSubmit(config)
 
-    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016G-23Sep2016-v1'
-    config.Data.inputDataset = '/SingleElectron/Run2016G-23Sep2016-v1/AOD'
+    config.General.requestName = 'candidateTrackProducer_SingleElectron_2016G-PromptReco-v1'
+    config.Data.inputDataset = '/SingleElectron/Run2016G-PromptReco-v1/AOD'
     config.JobType.psetName = 'candidateTrackProducer_RunMiniAOD_Data2016_ElectronSkim_cfg.py'
     #forkAndSubmit(config)
 
     # Run2016H-v2 (no lumi sections from the Golden JSON are in PromptReco-v1)
 
-    config.Data.outputDatasetTag = 'Run2016H-PromptReco-v2-DisappTrks-v2'
+    config.Data.outputDatasetTag = 'Run2016H-PromptReco-v2-DisappTrks-v9'
+    config.Data.unitsPerJob = 61 # 2016H-v2 has ~60515 lumis
 
     config.General.requestName = 'candidateTrackProducer_MET_2016H-PromptReco-v2'
     config.Data.inputDataset = '/MET/Run2016H-PromptReco-v2/AOD'
@@ -252,7 +252,8 @@ if __name__ == '__main__':
 
     # Run2016H-v3
 
-    config.Data.outputDatasetTag = 'Run2016H-PromptReco-v3-DisappTrks-v2'
+    config.Data.outputDatasetTag = 'Run2016H-PromptReco-v3-DisappTrks-v9'
+    config.Data.unitsPerJob = 1 # 2016H-v3 has ~1373 lumis
 
     config.General.requestName = 'candidateTrackProducer_MET_2016H-PromptReco-v3'
     config.Data.inputDataset = '/MET/Run2016H-PromptReco-v3/AOD'
