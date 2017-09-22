0bdc10518894424c9630f6cc45186f317311ad46
Andrew Hart <ahart@cern.ch>
2017-09-22 15:16:15 -0400

    Add configuration information for WZToLNuNuNu_2016MC.

--- StandardAnalysis/python/localConfig.py
+++ StandardAnalysis/python/localConfig.py
@@ -187,7 +187,7 @@ for attribute in list (locals ()):
         continue
     newKeys = {}
     for a in locals ()[attribute]:
-        if re.match (r"DYJetsToLL_50", a) or re.match (r"WZ", a):
+        if re.match (r"DYJetsToLL_50", a) or re.match (r"WZ", a) or re.match (r"WZToLNuNuNu", a):
             b = re.sub (r"(.*)", r"\1_2016MC", a)
             newKeys[b] = copy.deepcopy (locals ()[attribute][a])
             if isinstance (newKeys[b], str) and attribute == "labels":
