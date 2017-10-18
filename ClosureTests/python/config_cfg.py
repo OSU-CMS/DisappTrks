ed860ba0dd715266bb0330561c1aca68199dcd5f
Brian Francis <brian.patrick.francis@cern.ch>
2017-10-18 14:03:07 -0400

    Now add commit ahead of master for collMap

--- ClosureTests/python/config_cfg.py
+++ ClosureTests/python/config_cfg.py
@@ -3,39 +3,39 @@ from DisappTrks.StandardAnalysis.protoConfig_cfg import *
 ################################################################################
 # MET channels
 ################################################################################
-#  add_channels  (process,  [candTrkIdElecPt35],       histSets,  weights,  [],  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [candTrkIdElecPt35NoMet],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [candTrkIdElecPt35],       histSets,  weights,  [],  collMap,  variableProducers,  False)
+#  add_channels  (process,  [candTrkIdElecPt35NoMet],  histSets,  weights,  [],  collMap,  variableProducers,  False)
 
-#  add_channels  (process,  [candTrkIdMuPt35],       histSets,  weights,  [],  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [candTrkIdMuPt35NoMet],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [candTrkIdMuPt35],       histSets,  weights,  [],  collMap,  variableProducers,  False)
+#  add_channels  (process,  [candTrkIdMuPt35NoMet],  histSets,  weights,  [],  collMap,  variableProducers,  False)
 
-#  add_channels  (process,  [candTrkIdTauPt55],       histSets,  weights,  [],  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [candTrkIdTauPt55NoMet],  histSets,  weights,  [],  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [candTrkIdTauPt55],       histSets,  weights,  [],  collMap,  variableProducers,  False)
+#  add_channels  (process,  [candTrkIdTauPt55NoMet],  histSets,  weights,  [],  collMap,  variableProducers,  False)
 
 ################################################################################
 # SingleElectron channels
 ################################################################################
-#  add_channels  (process,  [ZtoEleProbeTrkWithZCuts],          histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [ZtoEleDisTrkNoNMissOutCut],        histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [ElectronTagPt35NoJetCuts],         histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [ElectronTagPt35NoJetCutsMetTrig],  histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [ZtoEleProbeTrkWithZCuts],          histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collMap,  variableProducers,  False)
+#  add_channels  (process,  [ZtoEleDisTrkNoNMissOutCut],        histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collMap,  variableProducers,  False)
+#  add_channels  (process,  [ElectronTagPt35NoJetCuts],         histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collMap,  variableProducers,  False)
+#  add_channels  (process,  [ElectronTagPt35NoJetCutsMetTrig],  histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collMap,  variableProducers,  False)
 
-#  add_channels  (process,  [ZtoTauToEleProbeTrkWithZCuts],    histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [ZtoTauToEleDisTrkNoNMissOutCut],  histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [ZtoTauToEleProbeTrkWithZCuts],    histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collMap,  variableProducers,  False)
+#  add_channels  (process,  [ZtoTauToEleDisTrkNoNMissOutCut],  histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collMap,  variableProducers,  False)
 
 ################################################################################
 # SingleMuon channels
 ################################################################################
-#  add_channels  (process,  [ZtoMuProbeTrkWithZCuts],       histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [ZtoMuDisTrkNoNMissOutCut],     histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [MuonTagPt35NoJetCuts],         histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [MuonTagPt35NoJetCutsMetTrig],  histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [ZtoMuProbeTrkWithZCuts],       histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collMap,  variableProducers,  False)
+#  add_channels  (process,  [ZtoMuDisTrkNoNMissOutCut],     histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collMap,  variableProducers,  False)
+#  add_channels  (process,  [MuonTagPt35NoJetCuts],         histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collMap,  variableProducers,  False)
+#  add_channels  (process,  [MuonTagPt35NoJetCutsMetTrig],  histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collMap,  variableProducers,  False)
 
-#  add_channels  (process,  [ZtoTauToMuProbeTrkWithZCuts],    histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [ZtoTauToMuDisTrkNoNMissOutCut],  histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [ZtoTauToMuProbeTrkWithZCuts],    histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collMap,  variableProducers,  False)
+#  add_channels  (process,  [ZtoTauToMuDisTrkNoNMissOutCut],  histSetsMuon,  weightsWithMuonSF,  scaleFactorProducersWithMuons,  collMap,  variableProducers,  False)
 
 ################################################################################
 # Tau channels
 ################################################################################
-#  add_channels  (process,  [TauTagPt55NoJetCuts],         histSetsTau,  weights,  scaleFactorProducers,  collectionMap,  variableProducers,  False)
-#  add_channels  (process,  [TauTagPt55NoJetCutsMetTrig],  histSetsTau,  weights,  scaleFactorProducers,  collectionMap,  variableProducers,  False)
+#  add_channels  (process,  [TauTagPt55NoJetCuts],         histSetsTau,  weights,  scaleFactorProducers,  collMap,  variableProducers,  False)
+#  add_channels  (process,  [TauTagPt55NoJetCutsMetTrig],  histSetsTau,  weights,  scaleFactorProducers,  collMap,  variableProducers,  False)
