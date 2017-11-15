cf134206d8159ec53dee487492640697fd91b69f
Andrew Hart <ahart@cern.ch>
2017-11-15 16:11:47 -0500

    First commit of an EDAnalyzer for printing out L1 trigger information. Debug statements included for now.

---
+++ TriggerAnalysis/plugins/L1TriggerInfoPrinter.cc
@@ -0,0 +1,64 @@
+#include "DisappTrks/TriggerAnalysis/plugins/L1TriggerInfoPrinter.h"
+
+L1TriggerInfoPrinter::L1TriggerInfoPrinter (const edm::ParameterSet &cfg) :
+  l1GtReadoutRecord_ (cfg.getParameter<edm::InputTag> ("l1GtReadoutRecord"))
+{
+  tok_gtRec_ = consumes<L1GlobalTriggerReadoutRecord> (l1GtReadoutRecord_);
+}
+
+L1TriggerInfoPrinter::~L1TriggerInfoPrinter ()
+{
+}
+
+void
+L1TriggerInfoPrinter::analyze (const edm::Event &iEvent, const edm::EventSetup &iSetup)
+{
+  stringstream ss;
+  ss.str ("");
+
+  edm::ESHandle<L1GtTriggerMenu> menuRcd;
+  iSetup.get<L1GtTriggerMenuRcd>().get(menuRcd) ;
+  const L1GtTriggerMenu* menu = menuRcd.product();
+  const AlgorithmMap& bitMap = menu->gtAlgorithmMap();
+  ss<<" Number of algorithms in L1 GT menu: "<<bitMap.size()<<endl;
+
+  edm::Handle<L1GlobalTriggerReadoutRecord> gtRecord;
+  iEvent.getByToken(tok_gtRec_, gtRecord);
+
+  ss<<"================================================================================"<<endl;
+  ss<<" Printing L1GlobalTriggerReadoutRecord..."<<endl;
+  ss<<"--------------------------------------------------------------------------------"<<endl;
+  ss<<(*gtRecord)<<endl;
+  ss<<"--------------------------------------------------------------------------------"<<endl;
+  gtRecord->printGtDecision(ss);
+  ss<<"--------------------------------------------------------------------------------"<<endl;
+  gtRecord->printTechnicalTrigger(ss);
+  ss<<"--------------------------------------------------------------------------------"<<endl;
+  gtRecord->print(ss);
+  ss<<"================================================================================"<<endl;
+
+  if (!gtRecord.isValid()) {
+    ss<<" No L1 trigger record "<<endl;
+  } else {
+
+    const DecisionWord dWord = gtRecord->decisionWord();
+    ss<<" Size of decision word from readout record: "<<dWord.size()<<endl;
+
+    /*if (dWord.empty()) {
+      ss<<" No decision word from readout record "<<endl;
+    } else {*/
+      for (CItAlgo itAlgo = bitMap.begin(); itAlgo != bitMap.end(); itAlgo++) {
+        ss<<" Testing algorithm \""<<itAlgo->first<<"\""<<endl;
+        if (dWord.empty()) continue;
+        bool decision=menu->gtAlgorithmResult(itAlgo->first,dWord);
+        /*if(decision == 1)*/ ss<<" Trigger "<<itAlgo->first<<" "<<decision<<endl;
+      }
+
+    }
+  //}
+
+  edm::LogInfo("L1TriggerInfoPrinter")<<ss.str ();
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(L1TriggerInfoPrinter);
---
+++ TriggerAnalysis/plugins/L1TriggerInfoPrinter.h
@@ -0,0 +1,38 @@
+#ifndef L1_TRIGGER_INFO_PRINTER
+#define L1_TRIGGER_INFO_PRINTER
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "CondFormats/L1TObjects/interface/L1GtTriggerMenu.h"
+#include "CondFormats/DataRecord/interface/L1GtTriggerMenuRcd.h"
+
+using namespace std;
+
+class L1TriggerInfoPrinter : public edm::EDAnalyzer
+{
+  public:
+    L1TriggerInfoPrinter (const edm::ParameterSet &);
+    ~L1TriggerInfoPrinter ();
+
+    void analyze (const edm::Event &, const edm::EventSetup &);
+
+  private:
+    edm::InputTag l1GtReadoutRecord_;
+
+    edm::EDGetTokenT<L1GlobalTriggerReadoutRecord> tok_gtRec_;
+};
+
+#endif
