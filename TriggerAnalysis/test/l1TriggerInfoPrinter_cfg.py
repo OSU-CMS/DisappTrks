aac62cfed9babf4c2181d583d1069ebc4f34834b
Andrew Hart <ahart@cern.ch>
2017-11-17 11:11:50 -0500

    Few cosmetic changes.

--- TriggerAnalysis/test/l1TriggerInfoPrinter_cfg.py
+++ TriggerAnalysis/test/l1TriggerInfoPrinter_cfg.py
@@ -1,24 +1,30 @@
 import FWCore.ParameterSet.Config as cms
 import sys
 
+globalTag = '76X_dataRun2_v15'
+if os.environ["CMSSW_VERSION"].startswith ("CMSSW_8_0_"):
+    globalTag = '80X_dataRun2_2016SeptRepro_v6'
+if os.environ["CMSSW_VERSION"].startswith ("CMSSW_9_2_"):
+    globalTag = '92X_dataRun2_Prompt_v8'
+
 ###########################################################
 ##### Set up process #####
 ###########################################################
 
 process = cms.Process ('L1TINFOPRINTER')
 process.load ('FWCore.MessageService.MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.MessageLogger.categories.append ("L1GlobalTriggerReadoutRecord")
 process.MessageLogger.categories.append ("L1TriggerInfoPrinter")
 process.MessageLogger.cerr.threshold = cms.untracked.string ("DEBUG")
 process.MessageLogger.debugModules = cms.untracked.vstring ("*")
 
 process.maxEvents = cms.untracked.PSet (
-    input = cms.untracked.int32 (10)
+    input = cms.untracked.int32 (-1)
 )
 process.source = cms.Source ("PoolSource",
     fileNames = cms.untracked.vstring (
-        "file:local2015_numEvent5.root",
+        "file:pickevents.root",
     ),
 )
 
@@ -29,7 +35,7 @@ process.source = cms.Source ("PoolSource",
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '80X_dataRun2_2016SeptRepro_v6', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, globalTag, '')
 
 ###########################################################
 ##### Set up the analyzer #####
