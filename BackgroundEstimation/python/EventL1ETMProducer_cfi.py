8779ee3a5dcb21b7051548625bacf5f0b82f9d62
Andrew Hart <ahart@cern.ch>
2017-11-20 16:29:05 -0500

    Minor alterations.

--- BackgroundEstimation/python/EventL1ETMProducer_cfi.py
+++ BackgroundEstimation/python/EventL1ETMProducer_cfi.py
@@ -3,13 +3,13 @@
 import os
 import FWCore.ParameterSet.Config as cms
 
-def customizeForMETTriggerProducer (producer):
+def customizeForL1ETMProducer (producer):
     producer.filterCategories = cms.vstring (
         "etm",
     )
 
     if os.environ["CMSSW_VERSION"].startswith ("CMSSW_7_6_"):
-        print "# Using 2015 MET trigger filters in EventMETTriggerProducer_cfi.py..."
+        print "# Using 2015 MET trigger filters in EventL1ETMProducer_cfi.py..."
 
         # The items in each vector correspond to the following triggers, in this order:
         # [HLT_MET250_v*, HLT_MET75_IsoTrk50_v*, HLT_PFMET120_PFMHT120_IDTight_v*, HLT_PFMET170_HBHECleaned_v*, HLT_PFMET170_JetIdCleaned_v*, HLT_PFMET170_NoiseCleaned_v*, HLT_PFMET170_v*, HLT_PFMET90_PFMHT90_IDTight_v*, HLT_PFMETNoMu120_JetIdCleaned_PFMHTNoMu120_IDTight_v*, HLT_PFMETNoMu90_JetIdCleaned_PFMHTNoMu90_IDTight_v*]
@@ -22,13 +22,16 @@ def customizeForMETTriggerProducer (producer):
 
         if producer.type_ () == "EventElectronL1ETMProducer":
             producer.tagCollection = cms.string ("hltL1extraParticles:Isolated:HLT")
-        if producer.type_ () == "EventMuonL1ETMProducer":
+        elif producer.type_ () == "EventMuonL1ETMProducer":
             producer.tagCollection = cms.string ("hltL1extraParticles::HLT")
-        if producer.type_ () == "EventTauL1ETMProducer":
+        elif producer.type_ () == "EventTauL1ETMProducer":
             producer.tagCollection = cms.string ("hltL1extraParticles:Tau:HLT")
+        else:
+            print "# Unknown producer type! Cannot set collection for tag lepton."
+            exit (1)
 
     elif os.environ["CMSSW_VERSION"].startswith ("CMSSW_8_0_"):
-        print "# Using 2016 MET trigger filters in EventMETTriggerProducer_cfi.py..."
+        print "# Using 2016 MET trigger filters in EventL1ETMProducer_cfi.py..."
 
         # The items in each vector correspond to the following triggers, in this order:
         # [HLT_MET200_v*,  HLT_MET75_IsoTrk50_v*,  HLT_PFMET100_PFMHT100_IDTight_BeamHaloCleaned_v*,  HLT_PFMET120_PFMHT120_IDTight_v*,  HLT_PFMET170_HBHECleaned_v*,  HLT_PFMET300_v*,  HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v*]
@@ -41,13 +44,16 @@ def customizeForMETTriggerProducer (producer):
 
         if producer.type_ () == "EventElectronL1ETMProducer":
             producer.tagCollection = cms.string ("hltCaloStage2Digis:EGamma:HLT")
-        if producer.type_ () == "EventMuonL1ETMProducer":
+        elif producer.type_ () == "EventMuonL1ETMProducer":
             producer.tagCollection = cms.string ("hltGmtStage2Digis:Muon:HLT")
-        if producer.type_ () == "EventTauL1ETMProducer":
+        elif producer.type_ () == "EventTauL1ETMProducer":
             producer.tagCollection = cms.string ("hltCaloStage2Digis:Tau:HLT")
+        else:
+            print "# Unknown producer type! Cannot set collection for tag lepton."
+            exit (1)
 
     else:
-        print "EventMETTriggerProducer_cfg.py does not know which MET trigger filters to apply!"
+        print "EventL1ETMProducer_cfg.py does not know which MET trigger filters to apply!"
         exit (1)
 
     return producer
