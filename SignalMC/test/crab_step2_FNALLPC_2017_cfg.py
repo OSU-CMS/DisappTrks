8228bc04589ca713fd3dafd359a3cb6703d4e8f7
bpf6qc <bfrancis@gmail.com>
2018-04-10 15:19:41 -0400

    Update crab step2 for 2017 test samples

--- SignalMC/test/crab_step2_FNALLPC_2017_cfg.py
+++ SignalMC/test/crab_step2_FNALLPC_2017_cfg.py
@@ -19,9 +19,11 @@ config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 1
 config.Data.outLFNDirBase = '/store/group/lpclonglived/DisappTrks/'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v1_step2'
+config.Data.outputDatasetTag = 'RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v3_step2'
 
 config.Site.storageSite = 'T3_US_FNALLPC'
+
+config.JobType.maxMemoryMB = 8000
 
 if __name__ == '__main__':
 
@@ -48,7 +50,14 @@ if __name__ == '__main__':
     ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
     #############################################################################################
 
-    config.General.requestName = 'AMSB_chargino900GeV_ctau10cm_step2'
-    config.Data.inputDataset = '/AMSB_chargino_M-900_CTau-10000_TuneZ2star_13TeV_pythia6/bfrancis-RunIISummer15GS-MCRUN2_71_V1-v2-9be42cee2e08d751ff195ac87d1d151d/USER'
+    config.General.requestName = 'AMSB_chargino700GeV_ctau100cm_step2'
+    config.Data.inputDataset = '/AMSB_chargino_M-700_CTau-100_TuneCUEP8M1_13TeV_pythia8/bfrancis-RunIIFall17DRPremix-94X_mc2017_realistic_v10-v3-8f3a8657071e90c6a17a2fb14d07aeb5/USER'
     #forkAndSubmit(config)
 
+    config.General.requestName = 'AMSB_chargino700GeV_ctau1000cm_step2'
+    config.Data.inputDataset = '/AMSB_chargino_M-700_CTau-1000_TuneCUEP8M1_13TeV_pythia8/bfrancis-RunIIFall17DRPremix-94X_mc2017_realistic_v10-v3-15a26dfbf2810ea023e4b06ebe64c27e/USER'
+    #forkAndSubmit(config)
+
+    config.General.requestName = 'AMSB_chargino300GeV_ctau100cm_step2'
+    config.Data.inputDataset = '/AMSB_chargino_M-300_CTau-100_TuneCUEP8M1_13TeV_pythia8/bfrancis-RunIIFall17DRPremix-94X_mc2017_realistic_v10-v3-508e13cba294da224a49b3f7ba92121a/USER'
+    #forkAndSubmit(config)
