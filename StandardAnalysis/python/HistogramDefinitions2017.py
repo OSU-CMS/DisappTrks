630eb9144749b2e7ce404df34ceaead02415b011
zacharypollock <38051843+zacharypollock@users.noreply.github.com>
2018-06-29 10:01:47 -0400

    fixed typo with deltaRToClosestPFElectron histograms

--- StandardAnalysis/python/HistogramDefinitions2017.py
+++ StandardAnalysis/python/HistogramDefinitions2017.py
@@ -253,9 +253,9 @@ IsolatedTrackCandidateTrackHistograms = cms.PSet(
         cms.PSet (
             name = cms.string("candTrackDeltaRToClosestElectronVsIsoTrackDeltaRToClosestPFElectron"),
             title = cms.string(";#DeltaR_{min}(track, PF muon);#DeltaR_{min}(track, PF charged hadron)"),
-            binsX = cms.untracked.vdouble(100, 0, 0.001),
-            binsY = cms.untracked.vdouble(100, 0, 0.001),
-            inputVariables = cms.vstring("deltaRToClosestPFelectron", "matchedCandidateTrack.deltaRToClosestElectron"),
+            binsX = cms.untracked.vdouble(1000, 0, 1),
+            binsY = cms.untracked.vdouble(1000, 0, 1,
+            inputVariables = cms.vstring("deltaRToClosestPFElectron", "matchedCandidateTrack.deltaRToClosestElectron"),
         ),
     )
 )
