928fa1135fd4f2d22c77f3e3779efefcd82eb2d9
bpf6qc <bfrancis@gmail.com>
2018-07-09 15:28:14 -0400

    Clarify behavior in 2017 data format

--- StandardAnalysis/interface/CustomDataFormat.h
+++ StandardAnalysis/interface/CustomDataFormat.h
@@ -5,12 +5,26 @@
 
   #define DISAPP_TRKS
 
+#if DATA_FORMAT_IS_2017
+
+  #include "OSUT3Analysis/AnaTools/interface/DataFormatMiniAOD2017.h"
+
+  #undef  tracks_TYPE
+  #undef  secondaryTracks_TYPE
+
+  #define tracks_TYPE  CandidateTrack
+  #define secondaryTracks_TYPE  CandidateTrack
+
+#else
+
   #include "OSUT3Analysis/AnaTools/interface/DataFormatMiniAOD.h"
 
   #undef  tracks_TYPE
   #undef  tracks_INVALID
 
   #define tracks_TYPE  CandidateTrack
+
+#endif
 
   #include "DisappTrks/CandidateTrackProducer/interface/CandidateTrack.h"
 
