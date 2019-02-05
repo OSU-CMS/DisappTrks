f291f18232afbb7dcb0e4e356b9688f1269f32ca
Brian Francis <brian.patrick.francis@cern.ch>
2019-02-05 11:57:16 -0500

    Clarify corrections to be applied to MC when calculating a new correction

--- SignalSystematics/test/config_2017_cfg.py
+++ SignalSystematics/test/config_2017_cfg.py
@@ -6,3 +6,8 @@ if not os.environ["CMSSW_VERSION"].startswith ("CMSSW_9_4_"):
     sys.exit (0)
 
 process = customize (process, "2017", applyPUReweighting = True, applyTriggerReweighting = True, applyMissingHitsCorrections = True, runMETFilters = False)
+
+# When using these channels for calculating a new weight in MC, use the following customization instead:
+# ZtoMuMuISRStudy, ZtoMuMuISRStudyJet30
+# hitsSystematicsCtrlSelection, muonCtrlSelection
+# process = customize (process, "2017", applyPUReweighting = True, applyISRReweighting = False, applyTriggerReweighting = False, applyMissingHitsCorrections = False, runMETFilters = False)
