e3ce01f47bccc660a12dcd49505b5fa778f9119c
Andrew Hart <ahart@cern.ch>
2019-02-13 15:24:30 -0600

    Scripts for calculating 2017 closure tests.

---
+++ ClosureTests/test/closureTestElec_2017.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import math, os, sys
+from DisappTrks.BackgroundEstimation.bkgdEstimate import LeptonBkgdEstimate
+from DisappTrks.StandardAnalysis.plotUtilities import *
+from DisappTrks.StandardAnalysis.IntegratedLuminosity_cff import *
+from ROOT import gROOT, TCanvas, TFile, TGraphErrors
+
+gROOT.SetBatch () # I too am Groot.
+
+if len (sys.argv) < 2:
+  print "Usage: " + os.path.basename (sys.argv[0]) + " NLAYERS"
+  sys.exit (1)
+nLayers = sys.argv[1]
+
+dirs = getUser()
+canvas = TCanvas("c1", "c1",800,800)
+setCanvasStyle(canvas)
+
+print "********************************************************************************"
+print "performing electron closure test with just TTbar (2017)"
+print "--------------------------------------------------------------------------------"
+
+fout = TFile.Open ("electronClosureTest_TTbar.root", "recreate")
+
+electronBkgdEstimate_TTJets_SemiLeptonic = LeptonBkgdEstimate ("electron")
+electronBkgdEstimate_TTJets_SemiLeptonic.addTFile (fout)
+electronBkgdEstimate_TTJets_SemiLeptonic.addTCanvas (canvas)
+electronBkgdEstimate_TTJets_SemiLeptonic.addLuminosityInInvPb (lumi["MET_2017"])
+electronBkgdEstimate_TTJets_SemiLeptonic.addLuminosityLabel (str (round (lumi["MET_2017"] / 1000.0, 2)) + " fb^{-1} (13 TeV)")
+electronBkgdEstimate_TTJets_SemiLeptonic.addMetCut (120.0)
+electronBkgdEstimate_TTJets_SemiLeptonic.addPhiCut (-1.0)
+electronBkgdEstimate_TTJets_SemiLeptonic.addECaloCut (10.0)
+electronBkgdEstimate_TTJets_SemiLeptonic.addUseHistogramsForPpassMetTriggers (False) # temporary measure
+electronBkgdEstimate_TTJets_SemiLeptonic.addChannel ("TagPt35",            "ElectronTagPt35NoJetCuts",        "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest_background/" + nLayers + "layers/electron")
+electronBkgdEstimate_TTJets_SemiLeptonic.addChannel ("TagPt35MetTrig",     "ElectronTagPt35NoJetCutsMetTrig", "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest_background/" + nLayers + "layers/electron_metTrig")
+electronBkgdEstimate_TTJets_SemiLeptonic.addChannel ("CandTrkIdPt35",      "CandTrkIdElecPt35",               "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest/" + nLayers + "layers/electron")
+electronBkgdEstimate_TTJets_SemiLeptonic.addChannel ("CandTrkIdPt35NoMet", "CandTrkIdElecPt35NoMet",          "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest/" + nLayers + "layers/electron_noMet")
+if int (nLayers) < 6:
+  electronBkgdEstimate_TTJets_SemiLeptonic.addChannel ("TrigEffDenom",            "ElectronTagPt35NoJetCuts",        "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest_background/6layers/electron")
+  electronBkgdEstimate_TTJets_SemiLeptonic.addChannel ("TrigEffNumer",     "ElectronTagPt35NoJetCutsMetTrig", "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest_background/6layers/electron_metTrig")
+if nLayers == "6":
+  nLayers = "6plus"
+electronBkgdEstimate_TTJets_SemiLeptonic.addChannel   ("TagProbe",       "ZtoEleProbeTrkNLayers"+nLayers,             "DYJetsToLL_50", dirs['Andrew']+"2017/closureTest_tagAndProbe/electron")
+electronBkgdEstimate_TTJets_SemiLeptonic.addChannel   ("TagProbePass",   "ZtoEleProbeTrkNLayers"+nLayers,   "DYJetsToLL_50", dirs['Andrew']+"2017/closureTest_tagAndProbe/electron")
+
+print "********************************************************************************"
+
+est = electronBkgdEstimate_TTJets_SemiLeptonic.printNest ()
+
+print "--------------------------------------------------------------------------------"
+
+obs = electronBkgdEstimate_TTJets_SemiLeptonic.printNback ()
+
+print ""
+
+diff = est - obs
+
+print "N_est - N_obs: " + str (diff.centralValue () / diff.maxUncertainty ()) + " sigma"
+
+print "********************************************************************************"
+
+fout.Close ()
---
+++ ClosureTests/test/closureTestTau_2017.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import math, os, sys
+from DisappTrks.BackgroundEstimation.bkgdEstimate import LeptonBkgdEstimate
+from DisappTrks.StandardAnalysis.plotUtilities import *
+from DisappTrks.StandardAnalysis.IntegratedLuminosity_cff import *
+from ROOT import gROOT, TCanvas, TFile, TGraphErrors
+
+gROOT.SetBatch () # I too am Groot.
+
+if len (sys.argv) < 2:
+  print "Usage: " + os.path.basename (sys.argv[0]) + " NLAYERS"
+  sys.exit (1)
+nLayers = sys.argv[1]
+
+dirs = getUser()
+canvas = TCanvas("c1", "c1",800,800)
+setCanvasStyle(canvas)
+
+print "********************************************************************************"
+print "performing tau closure test with just TTbar (2017)"
+print "--------------------------------------------------------------------------------"
+
+fout = TFile.Open ("tauClosureTest_TTbar.root", "recreate")
+
+tauBkgdEstimate_TTJets_SemiLeptonic = LeptonBkgdEstimate ("tau")
+tauBkgdEstimate_TTJets_SemiLeptonic.addTFile (fout)
+tauBkgdEstimate_TTJets_SemiLeptonic.addTCanvas (canvas)
+tauBkgdEstimate_TTJets_SemiLeptonic.addLuminosityInInvPb (lumi["MET_2017"])
+tauBkgdEstimate_TTJets_SemiLeptonic.addLuminosityLabel (str (round (lumi["MET_2017"] / 1000.0, 2)) + " fb^{-1} (13 TeV)")
+tauBkgdEstimate_TTJets_SemiLeptonic.addMetCut (120.0)
+tauBkgdEstimate_TTJets_SemiLeptonic.addPhiCut (-1.0)
+tauBkgdEstimate_TTJets_SemiLeptonic.addECaloCut (10.0)
+tauBkgdEstimate_TTJets_SemiLeptonic.addUseHistogramsForPpassMetTriggers (False) # temporary measure
+tauBkgdEstimate_TTJets_SemiLeptonic.addChannel ("TagPt35",            "TauTagPt55NoJetCuts",        "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest_background/" + nLayers + "layers/tau")
+tauBkgdEstimate_TTJets_SemiLeptonic.addChannel ("TagPt35MetTrig",     "TauTagPt55NoJetCutsMetTrig", "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest_background/" + nLayers + "layers/tau_metTrig")
+tauBkgdEstimate_TTJets_SemiLeptonic.addChannel ("CandTrkIdPt35",      "CandTrkIdTauPt55",               "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest/" + nLayers + "layers/tau")
+tauBkgdEstimate_TTJets_SemiLeptonic.addChannel ("CandTrkIdPt35NoMet", "CandTrkIdTauPt55NoMet",          "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest/" + nLayers + "layers/tau_noMet")
+if int (nLayers) < 6:
+  tauBkgdEstimate_TTJets_SemiLeptonic.addChannel ("TrigEffDenom",            "TauTagPt55NoJetCuts",        "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest_background/6layers/tau")
+  tauBkgdEstimate_TTJets_SemiLeptonic.addChannel ("TrigEffNumer",     "TauTagPt55NoJetCutsMetTrig", "TTJets_SemiLeptonic", dirs['Andrew']+"2017/closureTest_background/6layers/tau_metTrig")
+if nLayers == "6":
+  nLayers = "6plus"
+tauBkgdEstimate_TTJets_SemiLeptonic.addChannel   ("TagProbe",       "ZtoTauToEleProbeTrkNLayers"+nLayers,             "DYJetsToLL_50", dirs['Andrew']+"2017/closureTest_tagAndProbe/tauToElectron")
+tauBkgdEstimate_TTJets_SemiLeptonic.addChannel   ("TagProbePass",   "ZtoTauToEleProbeTrkNLayers"+nLayers,   "DYJetsToLL_50", dirs['Andrew']+"2017/closureTest_tagAndProbe/tauToElectron")
+tauBkgdEstimate_TTJets_SemiLeptonic.addChannel   ("TagProbe1",       "ZtoTauToMuProbeTrkNLayers"+nLayers,             "DYJetsToLL_50", dirs['Andrew']+"2017/closureTest_tagAndProbe/tauToMuon")
+tauBkgdEstimate_TTJets_SemiLeptonic.addChannel   ("TagProbePass1",   "ZtoTauToMuProbeTrkNLayers"+nLayers,   "DYJetsToLL_50", dirs['Andrew']+"2017/closureTest_tagAndProbe/tauToMuon")
+
+print "********************************************************************************"
+
+est = tauBkgdEstimate_TTJets_SemiLeptonic.printNest ()
+
+print "--------------------------------------------------------------------------------"
+
+obs = tauBkgdEstimate_TTJets_SemiLeptonic.printNback ()
+
+print ""
+
+diff = est - obs
+
+print "N_est - N_obs: " + str (diff.centralValue () / diff.maxUncertainty ()) + " sigma"
+
+print "********************************************************************************"
+
+fout.Close ()
