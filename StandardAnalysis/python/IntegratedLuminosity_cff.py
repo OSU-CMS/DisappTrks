19e0b3e464f7e51e9eae2d5d33bef904e01c3c03
czkaiweb <czkaiweb@gmail.com>
2019-06-24 22:10:07 -0400

    update EGamma_2018C lumi

--- StandardAnalysis/python/IntegratedLuminosity_cff.py
+++ StandardAnalysis/python/IntegratedLuminosity_cff.py
@@ -353,7 +353,7 @@ lumi_2018_ntuples = {
     # --hltpath "HLT_Ele32_WPTight_Gsf_v*"
     #"EGamma_2018A" : 14002.865,
     #"EGamma_2018B" : 7097.149,
-    "EGamma_2018C" : 6615.617770,
+    "EGamma_2018C" : 6688.523784,
     #"EGamma_2018D" : 31929.065,
 
     # --hltpath "HLT_IsoMu24_v*"
