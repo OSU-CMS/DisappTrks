7a41a76875f7a44b4628612799f7577c7fcf59f3
czkaiweb <czkaiweb@gmail.com>
2019-06-30 20:16:04 -0400

    crab seen updated

--- StandardAnalysis/python/miniAOD_102X_Ntuples.py
+++ StandardAnalysis/python/miniAOD_102X_Ntuples.py
@@ -113,17 +113,17 @@ bkgd_crabSeen = { # run framework job with CandidateTrackProducer/test/analyzeSk
 
     'SingleTop_s_channel'         : 20000000.0,
     'SingleTop_t_channel_antitop' : 3326400.0,
-    'SingleTop_t_channel_top'     : -1,
+    'SingleTop_t_channel_top'     : 1599600.0,
     'SingleTop_tW'                : 7740170.0,
     'SingleTop_tbarW'             : 5813561.0,
 
     'WJetsToLNu_HT70to100'    : 28159421.0,
-    'WJetsToLNu_HT100to200'   : -1,
-    'WJetsToLNu_HT200to400'   : -1,
+    'WJetsToLNu_HT100to200'   : 29552325.0,
+    'WJetsToLNu_HT200to400'   : 24936269.0,
     'WJetsToLNu_HT400to600'   : 5932701.0,
     'WJetsToLNu_HT600to800'   : 20079356.0,
     'WJetsToLNu_HT800to1200'  : 8441662.0,
-    'WJetsToLNu_HT1200to2500' : -1,
+    'WJetsToLNu_HT1200to2500' : 7623501.0,
     'WJetsToLNu_HT2500toInf'  : 3281971.0,
 
     'WJetsToLNu' : 71122242.0,
@@ -136,7 +136,7 @@ bkgd_crabSeen = { # run framework job with CandidateTrackProducer/test/analyzeSk
     'ZJetsToNuNu_HT200to400'   : 23760670.0,
     'ZJetsToNuNu_HT400to600'   : 9527919.0,
     'ZJetsToNuNu_HT600to800'   : 5761094.0,
-    'ZJetsToNuNu_HT800to1200'  : -1,
+    'ZJetsToNuNu_HT800to1200'  : 1819062.0,
     'ZJetsToNuNu_HT1200to2500' : 342781.0,
     'ZJetsToNuNu_HT2500toInf'  : 359639.0,
 
