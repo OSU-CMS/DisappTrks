e92dc5e1139daac042cf1b242c531316ea60c38a
Kai Wei <czkaiweb@Kais-MacBook-Air.local>
2020-01-09 10:25:29 -0500

    mela for 2018 samples added

---
+++ MELA/test/mela_2018.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import math, os, sys
+from OSUT3Analysis.Configuration.Measurement import Measurement
+from DisappTrks.MELA.mela import LikelihoodEstimator
+from DisappTrks.StandardAnalysis.plotUtilities import *
+from DisappTrks.StandardAnalysis.IntegratedLuminosity_cff import *
+from ROOT import gROOT, TCanvas, TFile, TGraphErrors
+
+gROOT.SetBatch()
+
+dirs = getUser()
+canvas = TCanvas("c1", "c1", 800, 800)
+setCanvasStyle(canvas)
+
+nLayersWords = ["NLayers4", "NLayers5", "NLayers6plus"]
+if len(sys.argv) > 2:
+    nLayersWords = [sys.argv[2]]
+
+variables = [
+        'track_p',
+        'track_matchedIsolatedTrack.dEdxPixel',
+        'track_jet_trackJetDR',
+        'track_met_trackMetPhi',
+]
+
+for nLayersWord in nLayersWords:
+
+        print "********************************************************************************"
+        print "constructing signal PDFs in sample: AMSB_chargino_700GeV_100cm_94X -- (", nLayersWord, ")"
+        print "--------------------------------------------------------------------------------"
+
+        estimator = LikelihoodEstimator(nLayersWord)
+        estimator.addSignal('AMSB_chargino_700GeV_100cm_102X', 'disTrkSelectionSmearedJets' + nLayersWord, dirs['Kai'] + '2018/signalAcceptance_full_dEdx', 700, 100)
+
+        estimator.addData  ('observation', 'MET_2018ABCD_obs',   'DisTrkSelection' + nLayersWord, dirs['Kai'] + '2018/miniUnblind')
+        estimator.addData  ('fake',        'SingleMu_2018ABCD', 'ZtoMuMuDisTrkNoD0Cut' + nLayersWord, dirs['Kai'] + '2018/fake_dEdx')
+
+        estimator.setPrintObservedTracks(True)
+
+        estimator.constructSignalPdfs()
+        estimator.constructDataPdf('observation')
+        estimator.constructDataPdf('fake')
