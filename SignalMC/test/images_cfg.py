9fc6db3e256705d1a11d7fb397e5f8d0c71ee277
Brian Francis <brian.patrick.francis@cern.ch>
2020-05-13 11:07:25 -0400

    Update image producer

--- SignalMC/test/images_cfg.py
+++ SignalMC/test/images_cfg.py
@@ -5,17 +5,11 @@ import glob, sys, os
 from Configuration.StandardSequences.GeometryRecoDB_cff import *
 from Configuration.StandardSequences.MagneticField_38T_cff import *
 
-dirName = sys.argv[2]
-fileName = sys.argv[3]
-
-print "processing \"" + dirName + "/*.root\"..."
-print "writing histograms to \"" + fileName + "\""
-
 ###########################################################
 ##### Set up process #####
 ###########################################################
 
-process = cms.Process ('CHARGINO')
+process = cms.Process ('IMAGES')
 process.load ('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
@@ -31,31 +25,38 @@ process.maxEvents = cms.untracked.PSet (
 )
 process.source = cms.Source ("PoolSource",
     fileNames = cms.untracked.vstring (
-        map (lambda a : "file:" + a, [f for f in glob.glob (dirName + "/*.root") if os.path.getsize(f) != 0])
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-5to50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/PU2017_94X_mc2017_realistic_v11-v3/280000/0002944B-672B-E911-8E3C-0025905B85CC.root',
     ),
 )
 process.TFileService = cms.Service ('TFileService',
-    fileName = cms.string (fileName)
+    fileName = cms.string ('images.root')
 )
 
 ###########################################################
 ##### Set up the producer and the end path            #####
 ###########################################################
 
-process.charginoImageAnalyzer = cms.EDAnalyzer ("CharginoImageAnalyzer",
-    genParticles = cms.InputTag ("genParticles", ""),
-    EBRecHits          =  cms.InputTag  ("reducedEcalRecHitsEB"),
-    EERecHits          =  cms.InputTag  ("reducedEcalRecHitsEE"),
-    HBHERecHits        =  cms.InputTag  ("reducedHcalRecHits", "hbhereco"),
-    matchingID = cms.int32(1000022),
-    matchingMotherID = cms.int32(1000024), # use 0 for no mother ID requirement
-    matchingStatus = cms.int32(1), # use 0 for no gen status requirement
-    x_lo = cms.double (-0.5),
-    x_hi = cms.double (0.5),
-    n_x = cms.int32 (50),
-    y_lo = cms.double (-0.5),
-    y_hi = cms.double (0.5),
-    n_y = cms.int32 (50),
+process.trackImageAnalyzer = cms.EDAnalyzer ("TrackImageAnalyzer",
+    tracks       = cms.InputTag   ("generalTracks"),
+    genParticles = cms.InputTag   ("genParticles", ""),
+    electrons    = cms.InputTag   ("gedGsfElectrons", ""),
+    muons        = cms.InputTag   ("muons", ""),
+    taus         = cms.InputTag   ("hpsPFTauProducer", ""),
+    pfCandidates = cms.InputTag   ("particleFlow", ""),
+    EBRecHits    =  cms.InputTag  ("reducedEcalRecHitsEB"),
+    EERecHits    =  cms.InputTag  ("reducedEcalRecHitsEE"),
+    HBHERecHits  =  cms.InputTag  ("reducedHcalRecHits", "hbhereco"),
+
+    tauDecayModeFinding      = cms.InputTag ("hpsPFTauDiscriminationByDecayModeFinding"),
+    tauElectronDiscriminator = cms.InputTag ("hpsPFTauDiscriminationByMVA6LooseElectronRejection"),
+    tauMuonDiscriminator     = cms.InputTag ("hpsPFTauDiscriminationByLooseMuonRejection3"),
+
+    minGenParticlePt = cms.double (10.0),
+    minTrackPt       = cms.double (35.0),
+    maxRelTrackIso   = cms.double (0.05),
+
+    maxDEtaTrackRecHit = cms.double (0.5),
+    maxDPhiTrackRecHit = cms.double (0.5),
 )
 
-process.myPath = cms.Path (process.charginoImageAnalyzer)
+process.myPath = cms.Path (process.trackImageAnalyzer)
