740ce63c5d4353550d86b3da06414458838c3b10
Michael Carrigan <carriganm95@gmail.com>
2021-04-30 11:41:13 -0400

    files for running step2 neutrino gun on OSU t3

---
+++ ToyModels/test/osusub_step2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+osusub.py -n 999999 -e 4000 -t Dataset -f process.PREMIXRAWoutput.fileName -d /NeutrinoGun_TuneCP5_13TeV_pythia8/micarrig-RunIIFall17DRPremix-93X_mc2017_realistic_v3-v2-3b5907885936b211e7ccb76119d1fc02/USER -A -P -c NeutrinoGun_step2.py -w NeutrinoGun/step2/ -g
