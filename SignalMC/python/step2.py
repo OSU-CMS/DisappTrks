b2f91f648dc92a12a5083a03ac1f1b55a6e99a24
czkaiweb <czkaiweb@gmail.com>
2021-08-24 17:32:49 +0200

    step2s for trigger study

---
+++ SignalMC/python/step2.py
@@ -0,0 +1,7 @@
+amsbRun3TriggerStudy  =  {
+  (700,1)     :  '/AMSB_chargino_M-700GeV_CTau-1cm_TuneCP5_PSweights_14TeV_pythia8/kwei-Run3Winter21DRMiniAOD-FlatPU20to70_112X_mcRun3_2021_realistic_v16-e4f2835349a27b64f20249edf52e8228/USER',
+  (700,10)    :  '/AMSB_chargino_M-700GeV_CTau-10cm_TuneCP5_PSweights_14TeV_pythia8/kwei-Run3Winter21DRMiniAOD-FlatPU20to70_112X_mcRun3_2021_realistic_v16-e4f2835349a27b64f20249edf52e8228/USER',
+  (700,100)   :  '/AMSB_chargino_M-700GeV_CTau-100cm_TuneCP5_PSweights_14TeV_pythia8/kwei-Run3Winter21DRMiniAOD-FlatPU20to70_112X_mcRun3_2021_realistic_v16-e4f2835349a27b64f20249edf52e8228/USER',
+  (700,1000)   :  '/AMSB_chargino_M-700GeV_CTau-1000cm_TuneCP5_PSweights_14TeV_pythia8/kwei-Run3Winter21DRMiniAOD-FlatPU20to70_112X_mcRun3_2021_realistic_v16-e4f2835349a27b64f20249edf52e8228/USER',
+  (700,10000)   :  '/AMSB_chargino_M-700GeV_CTau-10000cm_TuneCP5_PSweights_14TeV_pythia8/kwei-Run3Winter21DRMiniAOD-FlatPU20to70_112X_mcRun3_2021_realistic_v16-e4f2835349a27b64f20249edf52e8228/USER',
+}
