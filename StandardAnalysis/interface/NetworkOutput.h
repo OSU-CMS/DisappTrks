ea0e2f4d051b9599fb92f933660fff094a29840a
Michael Carrigan <carriganm95@gmail.com>
2022-08-18 10:37:06 -0400

    fixes to network output

--- StandardAnalysis/interface/NetworkOutput.h
+++ StandardAnalysis/interface/NetworkOutput.h
@@ -11,32 +11,27 @@ class NetworkOutput
   public:
     
     NetworkOutput () {};
-    //NetworkOutput (const reco::Track &);
-    //NetworkOutput (const reco::Track &, const vector<reco::Track> &);
     ~NetworkOutput () {};
 
-    /*const float getOutput(int ID) const{
+    const float getOutput(int ID) const{
       return networkOutput_.at(ID);
-    }*/
+    }
 
-    //const std::vector<float> scores() const { return this->networkOutput_; };
-    const float networkOutput () const { return this->networkOutput_; };
+    const std::vector<float> scores() const { return this->networkOutput_; };
 
 
-    /*void setOutput(int ID, const float networkOutput){
+    void setOutput(int ID, const float networkOutput){
       networkOutput_[ID] = std::move(networkOutput);
-    }*/
-    void set_networkOutput (double value) { networkOutput_ = value; };
+    }
 
-    /*void addOutput(const float networkOutput){
+    void addOutput(const float networkOutput){
       networkOutput_.push_back(networkOutput);
-    }*/
+    }
 
     
 
   private:
-    //std::vector<float> networkOutput_;
-    float networkOutput_;
+    std::vector<float> networkOutput_;
 
 };
 
--- StandardAnalysis/plugins/FakeTrackVarProducer.cc
+++ StandardAnalysis/plugins/FakeTrackVarProducer.cc
@@ -95,11 +95,8 @@ FakeTrackVarProducer::FakeTrackVarProducer(const edm::ParameterSet &cfg, const C
   signalTriggerNames = cfg.getParameter<vector<string> >("signalTriggerNames");
   metFilterNames = cfg.getParameter<vector<string> >("metFilterNames");
 
-  //putTokenNetworkScores_ = produces<vector<float> >("networkScores");
-  //produces<std::vector<std::vector<tensorflow::Tensor> > > ("networkScores");
-  produces<vector<NetworkOutput> >();
-  //produces<std::vector<std::vector<CandidateTrack> > >("networkScores");
-
+  produces<NetworkOutput>("networkScores");
+  
   //networkScores_.clear();
   trackInfos_.clear();
   recHitInfos_.clear();
@@ -341,17 +338,14 @@ FakeTrackVarProducer::produce(edm::Event &event, const edm::EventSetup &setup)
   //std::sort(recHitInfos_.begin(),recHitInfos_.end(),hitInfoOrder());
 
   tensorflow::Tensor input(tensorflow::DT_FLOAT, {1,176});
-  //auto networkScores_ = std::make_unique<NetworkOutput>(); //(new std::vector<float> ());
-  unique_ptr<vector<NetworkOutput> > networkScores (new vector<NetworkOutput> ());
+  auto networkScores_ = std::make_unique<NetworkOutput>(); //(new std::vector<float> ());
   std::vector<float> v_networkScores_;
-  //std::unique_ptr<std::vector<std::vector<tensorflow::Tensor> > > networkScores_(new std::vector<std::vector<tensorflow::Tensor> >());
   
   int track_num = 0;
 
   for(auto &track : trackInfos_) 
   {
 
-    NetworkOutput networkOutput;
     std::vector<std::vector<double>> hitMap = getHitMap(track.dEdxInfo);   
     std::pair<double, double> maxHits = getMaxHits(track.dEdxInfo);
     unsigned long encodedLayers = encodeLayers(hitMap);
@@ -406,41 +400,22 @@ FakeTrackVarProducer::produce(edm::Event &event, const edm::EventSetup &setup)
 
     std::cout << " -> " << outputs[0].matrix<float>()(0, 0) << std::endl << std::endl;
 
-    networkOutput.set_networkOutput((float)outputs[0].matrix<float>()(0, 0));
 
-    //std::cout << "Number of outputs: " << outputs.size() << std::endl;
-
-    //auto output_tensor = outputs[0].flat<float>();
-
-    //std::cout << "output tensor: " << output_tensor(0) << std::endl;
-
-    //float score = outputs[0].matrix<float>()(0,0);
-
-    //networkScores_->push_back(outputs);
-    //for(unsigned int out=0; out < sizeof(output_tensor)/sizeof(output_tensor(0)); out++){
-	//float score = output_tensor(out);
-        //v_networkScores_.push_back(score);
-    //}
     float score = outputs[0].matrix<float>()(0,0);
     v_networkScores_.push_back(score);
 
     track_num++;
 
-    networkScores->push_back(networkOutput);
   }// end of track loop
 
   int counter = 0;
   //std::cout << "About to place score into product" << std::endl;
   for(std::vector<float>::iterator it = v_networkScores_.begin(); it != v_networkScores_.end(); it++){
-    //std::cout << "Trying to place output: " << *it << std::endl;
-    //networkScores_->setOutput(counter, *it);
-    ///networkScores_->addOutput(*it);
-    //networkScores.set_networkOutput(*it);
-    //std::cout << "Added output to product: " << counter << std::endl;
+    networkScores_->addOutput(*it);
     counter++;
   }
 
-  event.put(std::move(networkScores));
+  event.put(std::move(networkScores_), "networkScores");
   
 
 }
