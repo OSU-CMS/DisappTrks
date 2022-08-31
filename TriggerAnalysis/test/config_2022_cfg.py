3da2b4aafa35b17e412af7b5a7edb69130c43591
czkaiweb <czkaiweb@gmail.com>
2022-08-31 15:34:09 -0500

    trigger analysis script for 2022 created, need to be fixed

---
+++ TriggerAnalysis/test/config_2022_cfg.py
@@ -0,0 +1,8 @@
+from DisappTrks.TriggerAnalysis.config_cfg import *
+from DisappTrks.StandardAnalysis.customize import *
+
+if not os.environ["CMSSW_VERSION"].startswith ("CMSSW_12_4_"):
+    print("Please use a CMSSW_12_4_X release...")
+    sys.exit (0)
+
+process = customize (process, "2022", applyPUReweighting = False, applyISRReweighting = False, applyTriggerReweighting = False, applyMissingHitsCorrections = False, runMETFilters = False)
---
+++ TriggerAnalysis/test/localConfig_2022_SingleMu.py
@@ -0,0 +1,17 @@
+from DisappTrks.StandardAnalysis.localConfig import *
+
+config_file = "config_2022_cfg.py"
+
+intLumi = lumi["MET_2022A"]
+
+datasetsData = [
+    'MET_2022A',
+]
+
+datasets = datasetsBkgd + datasetsData + datasetsSig
+
+datasets = datasetsData
+
+#setNJobs (datasets, composite_dataset_definitions, nJobs, 500)
+#setDatasetType (datasets, composite_dataset_definitions, types, "bgMC")
+#InputCondorArguments["hold"] = "True"
