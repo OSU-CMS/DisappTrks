908a360aaacb0830553bc95efece42daf378c22c
czkaiweb <czkaiweb@gmail.com>
2022-09-16 15:18:49 -0500

    fix previous typo, get the proper METFilters for 2022 data

--- TriggerAnalysis/python/AllTriggers.py
+++ TriggerAnalysis/python/AllTriggers.py
@@ -13,6 +13,8 @@ elif os.environ["CMSSW_VERSION"].startswith ("CMSSW_9_4_"):
     print("# Trigger filters: 2017")
 elif os.environ["CMSSW_VERSION"].startswith ("CMSSW_10_2_"):
     print("# Trigger filters: 2018")
+elif os.environ["CMSSW_VERSION"].startswith ("CMSSW_12_4_"):
+    print("# Trigger filters: 2022")
 else:
     print("# Trigger filters: 2015")
     
@@ -125,7 +127,7 @@ if os.environ["CMSSW_VERSION"].startswith ("CMSSW_10_2_"):
     triggerFiltersMuon = ("hltIterL3MuonCandidates::HLT", "hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07")
     triggerFiltersTau = ("hltSelectedPFTausTrackPt30MediumAbsOrRelIsolation1Prong::HLT", "hltPFTau50TrackPt30MediumAbsOrRelIso1Prong")
 
-if os.environ["CMSSW_VERSION"].startswith ("cmssw_12_4_"):
+if os.environ["CMSSW_VERSION"].startswith ("CMSSW_12_4_"):
     triggerFiltersMet = {
         'HLT_MET105_IsoTrk50_v'                                 : ['hltMET105', 'hltMETClean65'],
         'HLT_MET120_IsoTrk50_v'                                 : ['hltMET120', 'hltMETClean65'],
