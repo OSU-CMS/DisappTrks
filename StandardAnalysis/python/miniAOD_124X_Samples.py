77487bc9d0bdba91a7ce3001293d51432f88271a
Michael Carrigan <carriganm95@gmail.com>
2023-03-10 15:47:20 -0500

    update json files

--- StandardAnalysis/python/miniAOD_124X_Samples.py
+++ StandardAnalysis/python/miniAOD_124X_Samples.py
@@ -16,18 +16,33 @@ dataset_names_data = {
     'Tau_2022F' : '/Tau/Run2022F-EXODisappTrk-PromptReco-v1/AOD',
 
     #MET
-    'MET_2022G' : '/JetMET/Run2022G-EXODisappTrk-PromptReco-v1/AOD',
+    #'MET_2022A' : '/MET/lpclonglived-Skim_EXODisappTrks_MET_Run2022A-485942db884bb77e20d203394ccaa614/USER',
+    #'MET_2022B' : '/MET/lpclonglived-Skim_EXODisappTrks_MET_Run2022B-485942db884bb77e20d203394ccaa614/USER',
+    #'MET_2022C' : '/MET/lpclonglived-Skim_EXODisappTrks_MET_Run2022C-485942db884bb77e20d203394ccaa614/USER',
+    #'MET_2022D' : '',
+    #'MET_2022E' : '/JetMET/Run2022E-EXODisappTrk-PromptReco-v1/AOD',
+    #'MET_2022F' : '/JetMET/Run2022F-EXODisappTrk-PromptReco-v1/AOD',
+    #'MET_2022G' : '/JetMET/Run2022G-EXODisappTrk-PromptReco-v1/AOD',
 
+    #temporary
+    'MET_2022A' : '/MET/Run2022A-10Dec2022-v2/MINIAOD',
+    'MET_2022B' : '/MET/Run2022B-PromptReco-v1/MINIAOD',
+    'MET_2022C' : '/MET/Run2022C-10Dec2022-v2/MINIAOD',
+    'MET_2022D' : '/JetMET/Run2022D-10Dec2022-v1/MINIAOD',
+    'MET_2022E' : '/JetMET/Run2022E-PromptReco-v1/MINIAOD',
+    'MET_2022F' : '/JetMET/Run2022F-PromptReco-v1/MINIAOD',
+    'MET_2022G' : '/JetMET/Run2022G-PromptReco-v1/MINIAOD',
 }
 
 run3_skim_sibling_datasets = { # Fixme -> added those that exist as of Nov 14, 2022
     # MET
-    'MET_2022A' : '/MET/Run2022A-PromptReco-v1/MINIAOD',
-    'MET_2022B' : '/MET/Run2022B-PromptReco-v1/MINIAOD',
-    'MET_2022C' : '/MET/Run2022C-PromptReco-v1/MINIAOD',
-    #'MET_2022D' : '/MET/Run2022D-PromptReco-v1/MINIAOD',
-    #'MET_2022E' : '/MET/Run2022E-PromptReco-v1/MINIAOD',
-    'MET_2022G' : '/JetMET/Run2022G-PromptReco-v1/MINIAOD',
+    #'MET_2022A' : '/MET/Run2022A-PromptReco-v1/MINIAOD',
+    #'MET_2022B' : '/MET/Run2022B-PromptReco-v1/MINIAOD',
+    #'MET_2022C' : '/MET/Run2022C-PromptReco-v1/MINIAOD',
+    #'MET_2022D' : '/JetMET/Run2022D-10Dec2022-v1/MINIAOD',
+    #'MET_2022E' : '/JetMET/Run2022E-PromptReco-v1/MINIAOD',
+    #'MET_2022F' : '/JetMET/Run2022F-PromptReco-v1/MINIAOD',
+    #'MET_2022G' : '/JetMET/Run2022G-PromptReco-v1/MINIAOD',
 
     #SingleMuon
     'SingleMu_2022A' : '/SingleMuon/Run2022A-PromptReco-v1/MINIAOD',
