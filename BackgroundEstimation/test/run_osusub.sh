86712c9b41ef5d7ff67691c2d041527ad4f49c64
ryansantos1174 <ryansantos1174@gmail.com>
2023-08-01 14:11:18 -0400

    ignore

--- BackgroundEstimation/test/run_osusub.sh
+++ BackgroundEstimation/test/run_osusub.sh
@@ -2,11 +2,11 @@
 
 version=1
 
-until [ ! -d "condor/2022/EGammaFiducialMap_$version" ]
+until [ ! -d "condor/test_$version" ]
 do
   echo folder: $version
   ((version=version+1))
 done
 
 #osusub.py -n 9999 -m -1 -t Dataset -A -P -l localConfig_2022.py -w run2022_test_v$version/ --redirector FNAL -g -R "request_memory=3000MB request_cpus=4" -J P_Golden22
-osusub.py -l localConfig_2022.py -t Dataset -w 2022/EGammaFiducialMap_$version/ -A -P -J P_Golden22 -R "request_memory=3000MB request_cpus=4 request_disk=1000MB"
+osusub.py -n 1 -m -1 -l localConfig_2022.py -t Dataset -w test_$version -A -P -J P_Golden22 -R "request_memory=3000MB request_cpus=4 request_disk=1000MB"
--- StandardAnalysis/test/run_osusub.sh
+++ StandardAnalysis/test/run_osusub.sh
@@ -9,4 +9,4 @@ do
 done
 
 #osusub.py -n 9999 -m -1 -t Dataset -A -P -l localConfig_2022.py -w run2022_test_v$version/ --redirector FNAL -g -R "request_memory=3000MB request_cpus=4" -J P_Golden22
-osusub.py -n 1 -m -1 -l localConfig_2022.py -t Dataset -w test_$version -A -P -J P_Golden22 -R "request_memory=3000MB request_cpus=4 request_disk=1000MB"
+osusub.py -n 100 -m -1 -l localConfig_2022.py -t Dataset -w test_$version -A -P -J P_Golden22 -R "request_memory=3000MB request_cpus=4 request_disk=1000MB"
