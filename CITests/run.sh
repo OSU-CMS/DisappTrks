1d5b94ba20e2820f3ce3b361cd17830145c9a979
Michael Carrigan <carriganm95@gmail.com>
2024-04-05 14:57:44 -0400

    add directory to run CI tests

---
+++ CITests/run.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+python3 /share/scratch0/mcarrigan/disTracksML/CMSSW_12_4_11_patch3/src/OSUT3Analysis/DBTools/python/getSiblings.py -f EGamma_2022F -s /EGamma/Run2022F-PromptReco-v1/AOD -t 10 -j 1 -m 1000
+cmsRun config_cfg.py True 100 10 /QCD_PT-2400to3200_TuneCP5_13p6TeV_pythia8/Run3Summer22DRPremix-124X_mcRun3_2022_realistic_v12-v2/AODSIM QCD_PT2400to3200_TEST eventList_10.txt
