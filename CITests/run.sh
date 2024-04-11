3ac979431324dc196c277cfb948fe9e3ecb18de1
CollinKa <colin.ka9@gmail.com>
2024-04-11 13:59:08 -0400

    update

--- CITests/run.sh
+++ CITests/run.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
-python3 OSUT3Analysis/DBTools/python/getSiblings.py -f EGamma_2022F -s /EGamma/Run2022F-PromptReco-v1/AOD -t 10 -j 1 -m 100
-cmsRun DisappTrks/CITests/config_cfg.py True 100 10 /EGamma/Run2022F-EXODisappTrk-PromptReco-v1/AOD EGamma_2022F eventList_10.txt
+python3 ../../OSUT3Analysis/DBTools/python/getSiblings.py -f EGamma_2022F -s /EGamma/Run2022F-PromptReco-v1/AOD -t 10 -j 1 -m 100
+cmsRun config_cfg.py True 100 10 /EGamma/Run2022F-EXODisappTrk-PromptReco-v1/AOD EGamma_2022F eventList_1.txt
