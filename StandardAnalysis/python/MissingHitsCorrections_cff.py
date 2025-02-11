ecb15db91229cdee31a176b7613adaa25332f592
Michael Carrigan <carriganm95@gmail.com>
2025-02-11 10:19:13 -0500

    update missing hits for all of 2022EFG

--- StandardAnalysis/python/MissingHitsCorrections_cff.py
+++ StandardAnalysis/python/MissingHitsCorrections_cff.py
@@ -43,8 +43,8 @@ MissingHitsCorrections["2018"] = {
 }
 
 MissingHitsCorrections["2022EFG"] = {
-    "dropTOBProbability"          :  0.0005744529, # PRE_TOB Y()
-    "preTOBDropHitInefficiency"   :  0.0055458020, # PRE_TOB X()
-    "postTOBDropHitInefficiency"  :  0.6082950476, # POST_TOB X()
-    "hitInefficiency"             :  0.0036159936, # middle X()
+    "dropTOBProbability"          :  0.0007151755, # PRE_TOB Y()
+    "preTOBDropHitInefficiency"   :  0.0037699605, # PRE_TOB X()
+    "postTOBDropHitInefficiency"  :  0.6114408725, # POST_TOB X()
+    "hitInefficiency"             :  0.0055455684, # middle X()
 }
