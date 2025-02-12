835e260ccf5dd0c90dd79470bb359324958455b2
Michael Carrigan <carriganm95@gmail.com>
2025-02-12 15:12:37 -0500

    fixes for running missing hits corrections with variable lumis, set era-by-era to false, start fake track validation

--- BackgroundSystematics/test/fakeTrackEstimateVsD0.py
+++ BackgroundSystematics/test/fakeTrackEstimateVsD0.py
@@ -9,23 +9,23 @@ from ROOT import gROOT, TFile, TGraphAsymmErrors
 gROOT.SetBatch () # I am Groot.
 
 if len (sys.argv) < 2:
-    print "Usage: " + os.path.basename (sys.argv[0]) + " NLAYERS"
+    print("Usage: " + os.path.basename (sys.argv[0]) + " NLAYERS")
     sys.exit (1)
 nLayers = sys.argv[1]
-if int (nLayers) > 5:
-    nLayers += "plus"
+#if int (nLayers) > 5:
+#    nLayers += "plus"
 
 dirs = getUser()
 
-runPeriods = ['']
+runPeriods = ['AB']
 
 stdout = sys.stdout
 nullout = open ("/dev/null", "w")
 sys.stdout = nullout
 
-N = 5
+N = 9
 A = 0.05
-B = 0.1
+B = 0.5
 D = (B - A) / N
 
 try:
@@ -46,18 +46,36 @@ for runPeriod in runPeriods:
         minD0 = A + i * D
 
         sys.stdout = stdout
-        print "minimum |d0|: " + str (minD0) + " cm"
+        print("minimum |d0|: " + str (minD0) + " cm")
+        print("maximum |d0|: " + str (minD0+0.05) + " cm")
+
         sys.stdout = nullout
 
+        fout = TFile.Open("fakeTrackBkgdEstimate_zToMuMu_2018" + runPeriod + "_" + nLayers + "Test.root", "recreate")
+        txtFile = "fakeTrackBkgdEstimate_zToMuMu_2018" + runPeriod + "_" + nLayers + 'Test.txt'
+        f = open(txtFile, 'w+')
         fakeTrackBkgdEstimate = FakeTrackBkgdEstimate ()
-        fakeTrackBkgdEstimate.addLuminosityInInvPb (lumi["MET_2017" + runPeriod])
+        fakeTrackBkgdEstimate.addLuminosityInInvPb (lumi["MET_2018" + runPeriod])
         fakeTrackBkgdEstimate.addMinD0 (minD0)
-        fakeTrackBkgdEstimate.addChannel  ("Basic3hits",      "ZtoMuMuDisTrkNoD0Cut3Layers",          "SingleMu_2017"  +  runPeriod,  dirs['Andrew']+"2017/fakeTrackBackground")
-        fakeTrackBkgdEstimate.addChannel  ("DisTrkInvertD0",  "ZtoMuMuDisTrkNoD0CutNLayers"+nLayers,  "SingleMu_2017"  +  runPeriod,  dirs['Andrew']+"2017/fakeTrackBackground_noD0")
-        fakeTrackBkgdEstimate.addChannel  ("Basic",           "BasicSelection",                       "MET_2017"       +  runPeriod,  dirs['Andrew']+"2017/basicSelection")
-        fakeTrackBkgdEstimate.addChannel  ("ZtoLL",           "ZtoMuMu",                              "SingleMu_2017"  +  runPeriod,  dirs['Andrew']+"2017/zToMuMu")
+        fakeTrackBkgdEstimate.addMaxD0 (minD0+0.05)        
+        fakeTrackBkgdEstimate.addTFile (fout)
+        fakeTrackBkgdEstimate.addTxtFile(txtFile)
 
-        nEst = fakeTrackBkgdEstimate.printNest ()
+        #fakeTrackBkgdEstimate.addChannel  ("Basic3hits",      "ZtoMuMuDisTrkNoD0Cut3Layers",          "SingleMu_2017"  +  runPeriod,  dirs['Andrew']+"2017/fakeTrackBackground")
+        #fakeTrackBkgdEstimate.addChannel  ("DisTrkInvertD0",  "ZtoMuMuDisTrkNoD0CutNLayers"+nLayers,  "SingleMu_2017"  +  runPeriod,  dirs['Andrew']+"2017/fakeTrackBackground_noD0")
+        #fakeTrackBkgdEstimate.addChannel  ("Basic",           "BasicSelection",                       "MET_2017"       +  runPeriod,  dirs['Andrew']+"2017/basicSelection")
+        #fakeTrackBkgdEstimate.addChannel  ("ZtoLL",           "ZtoMuMu",                              "SingleMu_2017"  +  runPeriod,  dirs['Andrew']+"2017/zToMuMu")
+        fakeTrackBkgdEstimate.addChannel("Basic3hits",     "ZtoMuMuDisTrkNoD0CutNLayers4",       "SingleMu_2018" + runPeriod, dirs['Mike'] + "bfrancisStore/2018/fromLPC/fakeBackground")
+        fakeTrackBkgdEstimate.addChannel("DisTrkInvertD0", "ZtoMuMuDisTrkNoD0Cut" + nLayers, "SingleMu_2018" + runPeriod, dirs['Mike'] + "bfrancisStore/2018/fromLPC/fakeBackground")
+        fakeTrackBkgdEstimate.addChannel("Basic",          "BasicSelection",                     "MET_2018"      + runPeriod, dirs['Mike'] + "bfrancisStore/2018/fromLPC/basicSelection_v2")
+        fakeTrackBkgdEstimate.addChannel("ZtoLL",          "ZtoMuMu",                            "SingleMu_2018" + runPeriod, dirs['Mike'] + "bfrancisStore/2018/fromLPC/zToMuMu")
+
+        nEst = fakeTrackBkgdEstimate.printNest ()[0]
+
+
+        print("debugging!!!!")
+
+        print("nest", nEst, type(nEst))
 
         g0.SetPoint (i, minD0, nEst.centralValue ())
         g0.SetPointError (i, D / 2.0, D / 2.0, min (nEst.maxUncertainty (), nEst.centralValue ()), nEst.maxUncertainty ())
@@ -71,7 +89,7 @@ for runPeriod in runPeriods:
             maxFluctuationUp = max (maxFluctuationUp, nEst.centralValue () - nominal)
 
     sys.stdout = stdout
-    print "[2017" + runPeriod + "] systematic uncertainty: - " + str (maxFluctuationDown) + " + " + str (maxFluctuationUp) + " (- " + str ((maxFluctuationDown / nominal) * 100.0) + " + " + str ((maxFluctuationUp / nominal) * 100.0) + ")%"
+    print ("[2017" + runPeriod + "] systematic uncertainty: - " + str (maxFluctuationDown) + " + " + str (maxFluctuationUp) + " (- " + str ((maxFluctuationDown / nominal) * 100.0) + " + " + str ((maxFluctuationUp / nominal) * 100.0) + ")%")
     sys.stdout = nullout
 
     fout = TFile ("fakeTrackEstimateVsD0.root", "update")
